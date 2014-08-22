d89147a0d4dce9d14be5434818d9d28783a54103
Marek Vavruša <marek.vavrusa@nic.cz>
2014-08-22 16:02:35 +0200

    lib: implemented resolver query plan

---
+++ lib/rplan.c
@@ -0,0 +1,73 @@
+#include <libknot/descriptor.h>
+
+#include "lib/rplan.h"
+
+static struct kr_query *query_create(mm_ctx_t *pool, const knot_dname_t *name)
+{
+	struct kr_query *qry = mm_alloc(pool, sizeof(struct kr_query));
+	if (qry == NULL) {
+		return NULL;
+	}
+
+	memset(qry, 0, sizeof(struct kr_query));
+	qry->sname = knot_dname_copy(name, pool);
+	if (qry->sname == NULL) {
+		mm_free(pool, qry);
+		return NULL;
+	}
+
+	return qry;
+}
+
+static void query_free(mm_ctx_t *pool, struct kr_query *qry)
+{
+	mm_free(pool, qry->sname);
+	mm_free(pool, qry);
+}
+
+void kr_rplan_init(struct kr_rplan *rplan, mm_ctx_t *pool)
+{
+	rplan->pool = pool;
+	init_list(&rplan->q);
+}
+
+void kr_rplan_clear(struct kr_rplan *rplan)
+{
+	struct kr_query *qry = NULL, *next = NULL;
+	WALK_LIST_DELSAFE(qry, next, rplan->q) {
+		query_free(rplan->pool, qry);
+	}
+
+	kr_rplan_init(rplan, rplan->pool);
+}
+
+struct kr_query *kr_rplan_push(struct kr_rplan *rplan, const knot_dname_t *name,
+                               uint16_t type, uint16_t cls)
+{
+	struct kr_query *qry =  query_create(rplan->pool, name);
+	if (qry == NULL) {
+		return NULL;
+	}
+
+	qry->sclass = cls;
+	qry->stype = type;
+	qry->flags = RESOLVE_QUERY;
+
+	add_head(&rplan->q, &qry->node);
+	return qry;
+}
+
+int kr_rplan_pop(struct kr_rplan *rplan, struct kr_query *qry)
+{
+	rem_node(&qry->node);
+	query_free(rplan->pool, qry);
+	return 0;
+}
+
+struct kr_query *kr_rplan_next(struct kr_rplan *rplan)
+{
+	if (EMPTY_LIST(rplan->q)) {
+		return NULL;
+	}
+	return HEAD(rplan->q);
+}
---
+++ lib/rplan.h
@@ -0,0 +1,47 @@
+/* Copyright 2014 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+#pragma once
+
+#include <libknot/dname.h>
+#include <common/lists.h>
+
+enum {
+	RESOLVE_QUERY = 0 << 0,
+	RESOLVE_DELEG = 1 << 0,
+};
+
+struct kr_query {
+	node_t node;
+	knot_dname_t *sname;
+	uint16_t stype;
+	uint16_t sclass;
+	uint16_t flags;
+	void *ext;
+};
+
+struct kr_rplan {
+	list_t q;
+	mm_ctx_t *pool;
+};
+
+void kr_rplan_init(struct kr_rplan *rplan, mm_ctx_t *pool);
+void kr_rplan_clear(struct kr_rplan *rplan);
+
+struct kr_query *kr_rplan_push(struct kr_rplan *rplan, const knot_dname_t *name,
+                               uint16_t type, uint16_t cls);
+int kr_rplan_pop(struct kr_rplan *rplan, struct kr_query *qry);
+struct kr_query *kr_rplan_next(struct kr_rplan *rplan);
+
