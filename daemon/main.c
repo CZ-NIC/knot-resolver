4bab5dadc1d9a3498b48f34c25d8f4bf25bab594
Marek Vavruša <marek.vavrusa@nic.cz>
2014-12-30 13:16:17 +0100

    relicensed under GPLv3

--- daemon/main.c
+++ daemon/main.c
@@ -1,3 +1,19 @@
+/*  Copyright (C) 2014 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <stdlib.h>
 #include <string.h>
 #include <getopt.h>
@@ -5,6 +21,8 @@
 #include <uv.h>
 
 #include <libknot/internal/sockaddr.h>
+#include <libknot/errcode.h>
+
 #include "lib/resolve.h"
 #include "worker.h"
 
@@ -12,6 +30,7 @@ void signal_handler(uv_signal_t *handle, int signum)
 {
 	uv_stop(uv_default_loop());
 	uv_signal_stop(handle);
+	exit(1);
 }
 
 static void help(void)
