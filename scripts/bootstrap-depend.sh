76757db9733b504be29db800a4db89cab41eb063
Marek Vavruša <marek.vavrusa@nic.cz>
2015-01-28 17:31:08 +0100

    travis: setup build env

---
+++ scripts/bootstrap-depend.sh
@@ -0,0 +1,57 @@
+#!/bin/sh  
+set -e
+
+PKG_CONFIG_PATH="${1}/lib/pkgconfig"
+
+if [ -z ${1} ]; then
+	echo "$0 <fakeroot>"
+	exit 1
+fi
+
+install -d ${1}
+
+# lmdb
+if [ ! -e ${1}/include/lmdb.h ]; then
+	git clone https://gitorious.org/mdb/mdb.git || true
+	cd mdb/libraries/liblmdb
+	make
+	install -d ${1}/lib ${1}/include
+	install -t ${1}/lib liblmdb.so
+	install -t ${1}/include lmdb.h
+	cd ../../..
+fi
+
+# liburcu
+if [ ! -e ${1}/include/urcu.h ]; then
+	git clone git://git.urcu.so/userspace-rcu.git || true
+	cd userspace-rcu
+	./bootstrap
+	./configure --prefix=${1}
+	make
+	make install
+	cd ..
+fi
+
+# libknot
+if [ ! -e ${1}/include/libknot ]; then
+	git clone https://github.com/CZNIC-Labs/knot.git || true
+	cd knot
+	git checkout resolver_improvements
+	autoreconf -i
+	./configure --prefix=${1}
+	make
+	make install
+	cd ..
+fi
+
+# libuv
+if [ ! -e ${1}/include/uv.h ]; then
+	git clone https://github.com/libuv/libuv.git || true
+	cd libuv
+	sh autogen.sh
+	./configure --prefix=${1}
+	make 
+	make install
+fi
+
+ldconfig || true
