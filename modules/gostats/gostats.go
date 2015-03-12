cfd3e89515df448b4acf98b8489e871e0bacf6ec
Marek Vavruša <marek.vavrusa@nic.cz>
2015-03-12 10:49:39 +0100

    modules: updated documentation + gostats

--- modules/gostats/gostats.go
+++ modules/gostats/gostats.go
@@ -3,19 +3,20 @@ package main
 /*
 #include "lib/layer.h"
 #include "lib/module.h"
-extern int begin(knot_layer_t *, void *) __asm__ ("main.Begin");
-extern int finish(knot_layer_t *) __asm__ ("main.Finish");
-static inline const knot_layer_api_t *_gostats_layer(void)
+extern int Begin(knot_layer_t *, void *) __asm__ ("main.Begin");
+extern int Finish(knot_layer_t *) __asm__ ("main.Finish");
+static inline const knot_layer_api_t *_layer(void)
 {
-	static const knot_layer_api_t _module = {
-		.begin = &begin,
-		.finish = &finish
+	static const knot_layer_api_t api = {
+		.begin = &Begin,
+		.finish = &Finish
 	};
-	return &_module;
+	return &api;
 }
 */
 import "C"
 import "unsafe"
+import "fmt"
 
 func Api() C.uint32_t {
 	return C.KR_MODULE_API
@@ -30,13 +31,16 @@ func Deinit(module *C.struct_kr_module) C.int {
 }
 
 func Begin(ctx *C.knot_layer_t, param unsafe.Pointer) C.int {
+	ctx->data = param
 	return 0
 }
 
 func Finish(ctx *C.knot_layer_t) C.int {
+	var param *C.struct_kr_layer_param = (*C.struct_kr_layer_param)(ctx.data)
+	fmt.Printf("[gostats] resolved %d queries", C.list_size(&param.rplan.resolved))
 	return 0
 }
 
 func Layer() *C.knot_layer_api_t {
-	return C._gostats_layer()
+	return C._layer()
 }
