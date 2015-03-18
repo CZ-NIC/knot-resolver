83780e5634a2e5dfc870bcd559eb7615251e1772
Marek Vavruša <marek.vavrusa@nic.cz>
2015-03-18 16:24:49 +0100

    doc: update

--- doc/conf.py
+++ doc/conf.py
@@ -68,7 +68,7 @@ html_theme = 'sphinx_rtd_theme'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+#html_static_path = ['_static']
 
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
@@ -108,7 +108,7 @@ html_use_smartypants = True
 #html_file_suffix = None
 
 # Output file base name for HTML help builder.
-htmlhelp_basename = 'formatdoc'
+htmlhelp_basename = 'apidoc'
 
 
 # -- Options for LaTeX output --------------------------------------------------
