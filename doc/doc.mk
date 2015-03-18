0e4d21fd23575ee0c8530483c2ba660fac153ea7
Marek Vavruša <marek.vavrusa@nic.cz>
2015-03-18 16:52:47 +0100

    doc: fixed rtd build

--- doc/doc.mk
+++ doc/doc.mk
@@ -3,7 +3,7 @@ doc-doxygen:
 doc-html: doc-doxygen
 	@cd doc && $(sphinx-build_BIN) -b html . html
 doc-clean:
-	@rm -rf doc/doxyxml doc/*.db doc/html
+	rm -rf doc/doxyxml doc/*.db doc/html
 
 .PHONY: doc-doxygen doc-html doc-clean
 
