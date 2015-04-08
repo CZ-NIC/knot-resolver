c84060730ca8234731d2a9a8761212aa6b94e71b
Marek Vavruša <marek.vavrusa@nic.cz>
2015-04-08 17:45:53 +0200

    daemon/lua: interpreter is able to pretty-print expressions and results

--- daemon/lua/sandbox.lua
+++ daemon/lua/sandbox.lua
@@ -35,3 +35,40 @@ if setfenv then -- Lua 5.1 and less
 else -- Lua 5.2+
 	_SANDBOX = make_sandbox(_ENV)
 end
+
+-- Pretty printing
+function table_print (tt, indent, done)
+	done = done or {}
+	indent = indent or 0
+	if type(tt) == "table" then
+		for key, value in pairs (tt) do
+			io.write(string.rep (" ", indent))
+			if type (value) == "table" and not done [value] then
+				done [value] = true
+				io.write(string.format("[%s] => {\n", tostring (key)));
+				table_print (value, indent + 4, done)
+				io.write(string.rep (" ", indent))
+				io.write("}\n");
+			else
+				io.write(string.format("[%s] => %s\n",
+				         tostring (key), tostring(value)))
+			end
+		end
+	else
+		io.write(tostring(tt) .. "\n")
+	end
+end
+
+-- Interactive command evaluation
+function eval_cmd(line)
+	local chunk, err = loadstring('table_print('..line..')')
+	if err then
+		chunk, err = loadstring(line)
+	end
+	if not err then
+		status, err = pcall(chunk)
+	end
+	if err then
+		print(err)
+	end
+end
