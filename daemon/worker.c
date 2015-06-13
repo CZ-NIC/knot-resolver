274cfa12fed6100af5d1928ecfbe6230cae9b535
Marek Vavruša <marek.vavrusa@nic.cz>
2015-06-13 15:22:27 +0200

    docker: fixed broken musl libc + stack protector

--- daemon/worker.c
+++ daemon/worker.c
@@ -14,7 +14,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#if !defined(__APPLE__) && defined(_GNU_SOURCE)
+#if defined(__GLIBC__) && defined(_GNU_SOURCE)
 #include <malloc.h>
 #endif
 #include <uv.h>
@@ -140,7 +140,7 @@ static void qr_task_free(uv_handle_t *handle)
 		array_push(worker->pools, mp_context);
 	} else {
 		mp_delete(mp_context);
-#if !defined(__APPLE__) && defined(_GNU_SOURCE)
+#if defined(__GLIBC__) && defined(_GNU_SOURCE)
 		/* Decommit memory every once in a while */
 		static int mp_delete_count = 0;
 		if (++mp_delete_count == 1000) {
