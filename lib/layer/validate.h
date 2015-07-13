debb317c36c65653de9730a6547fda455d366362
Karel Slany <karel.slany@nic.cz>
2015-07-13 18:56:08 +0200

    liv/validate: added missing header file

---
+++ lib/layer/validate.h
@@ -0,0 +1,30 @@
+/*  Copyright (C) 2015 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include <libknot/rrset.h>
+
+//#define ROOT_TA ". IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"
+#define ROOT_TA ". IN DS 19036 RSASHA256 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"
+
+/**
+ * Parses the supplied trust anchor string and creates a new RRSet.
+ *
+ * @param rr     created resource record
+ * @param ds_str DS in presentation format
+ * @param pool
+ * @return       0 or an error code
+ */
+int kr_ta_parse(knot_rrset_t **rr, const char *ds_str, mm_ctx_t *pool);
