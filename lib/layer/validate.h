5920f8acd9a6f0e1c7a0151ddd63a2d7703a1fea
Karel Slany <karel.slany@nic.cz>
2015-07-14 15:32:06 +0200

    lib/validate: added missing #pragma once

--- lib/layer/validate.h
+++ lib/layer/validate.h
@@ -14,6 +14,8 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
+#pragma once
+
 #include <libknot/rrset.h>
 
 //#define ROOT_TA ". IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"
