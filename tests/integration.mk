fdcf9ddda648e637666e0059c482a1cf36295671
Grigorii Demidov <grigorii.demidov@nic.cz>
2015-07-31 11:36:18 +0200

    tests: libfaketime search

--- tests/integration.mk
+++ tests/integration.mk
@@ -6,9 +6,14 @@ CWRAP_PATH := $(socket_wrapper_LIBS)
 FAKETIME_PATH := $(libfaketime_LIBS)
 
 # Targets
-preload_syms := LD_PRELOAD="$(FAKETIME_PATH):$(CWRAP_PATH)"
 
-check-integration: $(libmock_calls) $(_test_integration)
+ifeq ($(PLATFORM),Darwin)
+	preload_syms := DYLD_FORCE_FLAT_NAMESPACE=1 DYLD_INSERT_LIBRARIES="$(FAKETIME_PATH):$(CWRAP_PATH)"
+else
+	preload_syms := LD_PRELOAD="$(FAKETIME_PATH):$(CWRAP_PATH)"
+endif
+
+check-integration:
 	$(call preload_LIBS) $(preload_syms) tests/test_integration.py tests/testdata
 
 .PHONY: check-integration
