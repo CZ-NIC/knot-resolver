002543f8f02a49251e9dc3342e399216cf05f1ce
Marek Vavruša <marek.vavrusa@nic.cz>
2015-10-13 15:05:19 +0200

    build: fixed lua embedding on NetBSD
    thanks to johan ihren

--- scripts/embed.sh
+++ scripts/embed.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 set -e
 alias strip="sed -e 's/^[    ]*//g; s/[ ][ ]*/ /g; /^--/d; /^$/d'"
-if hash xxd 2>/dev/null; then
+if command -v xxd > /dev/null 2>&1; then
 	xxd -i - < $1 | strip
 else
 	hexdump -v -e '/1 "0x%02X, " " "' < $1 | strip
 fi
+exit $?
