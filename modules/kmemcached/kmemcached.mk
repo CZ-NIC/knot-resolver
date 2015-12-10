67fa76e167bed82a0eee99c05cd31f80471d30e7
Marek Vavruša <marek.vavrusa@nic.cz>
2015-12-10 18:33:13 +0100

    build: fixed modules depends
    fixes #19

--- modules/kmemcached/kmemcached.mk
+++ modules/kmemcached/kmemcached.mk
@@ -1,4 +1,5 @@
 kmemcached_CFLAGS := -fvisibility=hidden -fPIC
 kmemcached_SOURCES := modules/kmemcached/kmemcached.c modules/kmemcached/namedb_memcached.c
+kmemcached_DEPEND := $(libkres)
 kmemcached_LIBS := $(libkres_TARGET) $(libkres_LIBS) $(libmemcached_LIBS)
 $(call make_c_module,kmemcached)
