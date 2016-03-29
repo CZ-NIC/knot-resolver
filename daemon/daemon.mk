81e40c226b9e0aa756f4ab06445d172ee5d1a845
Ondřej Surý <ondrej@sury.org>
2016-03-29 11:57:30 +0200

    Add fix missing -m in the install command

--- daemon/daemon.mk
+++ daemon/daemon.mk
@@ -44,7 +44,7 @@ daemon: $(kresd)
 daemon-install: kresd-install bindings-install
 ifneq ($(SED),)
 	$(SED) -e "s/@VERSION@/$(MAJOR).$(MINOR).$(PATCH)/" -e "s/@DATE@/$(date)/" doc/kresd.8.in > doc/kresd.8
-	$(INSTALL) -d 0755 $(DESTDIR)$(PREFIX)/share/man/man8/
+	$(INSTALL) -d -m 0755 $(DESTDIR)$(PREFIX)/share/man/man8/
 	$(INSTALL) -m 0644 doc/kresd.8 $(DESTDIR)$(PREFIX)/share/man/man8/
 endif
 daemon-clean: kresd-clean
