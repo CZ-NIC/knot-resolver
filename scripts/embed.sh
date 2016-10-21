86ef92ea068a9c49d8d0638737b56b606362c260
Vladimír Čunát <vladimir.cunat@nic.cz>
2016-10-21 13:38:06 +0200

    make: strip *.lua as intended since dcd89700
    This saves a few kilobytes in the executable.
    Also, the name XXD seemed no longer suitable,
    as it does lua-specific cleaning.

--- scripts/embed.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -e
-alias strip="sed -e 's/^[    ]*//g; s/[ ][ ]*/ /g; /^--/d; /^$/d'"
-if command -v xxd > /dev/null 2>&1; then
-	xxd -i - < $1 | strip
-else
-	hexdump -v -e '/1 "0x%02X, " " "' < $1 | strip
-fi
-exit $?
