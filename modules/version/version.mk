8835a56eee9489f4882d9e9235b7e42e415b5020
Ondřej Surý <ondrej@sury.org>
2016-10-26 12:05:21 +0200

    Improve version.mk a bit to use $(call quiet,,)

--- modules/version/version.mk
+++ modules/version/version.mk
@@ -1,6 +1,11 @@
 version_SOURCES := version.lua
 
 modules/version/version.lua: modules/version/version.lua.in
-	$(SED) -e "s/@VERSION@/$(VERSION)/" < "$<" > "$@"
+	@$(call quiet,SED,$<) -e "s/@VERSION@/$(VERSION)/" $< > $@
 
 $(call make_lua_module,version)
+
+version-clean:
+	@$(call quiet,RM,modules/version/version.lua) modules/version/version.lua
+
+.PHONY: version-clean
