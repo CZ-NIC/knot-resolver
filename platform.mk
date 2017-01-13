ed9032b580f7bc2be75d1d9b3ceea955bcfadb6a
Petr Špaček <petr.spacek@nic.cz>
2017-01-13 09:35:27 +0100

    build docs: detect sphinx and python-breathe presence in make info

--- platform.mk
+++ platform.mk
@@ -183,6 +183,11 @@ define find_gopkg
 	HAS_$(1) := $(shell go list $(2) > /dev/null 2>&1 && echo yes || echo no)
 endef
 
+# Find Python package
+define find_pythonpkg
+	HAS_$(1) := $(shell python -c "import $(1)" > /dev/null 2>&1 && echo yes || echo no)
+endef
+
 define find_soname
 
 # N/A on Windows
