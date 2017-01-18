a6fcdc0d039c1f04bf9afd448d26116f34f9a407
Vladimír Čunát <vladimir.cunat@nic.cz>
2017-01-18 18:17:21 +0100

    QUERY_TRACE: disable with -DNDEBUG
    At least for now.  It didn't compile.

--- lib/layer/iterate.c
+++ lib/layer/iterate.c
@@ -745,12 +745,12 @@ static int resolve(kr_layer_t *ctx, knot_pkt_t *pkt)
 	if (!query) {
 		return ctx->state;
 	}
-
+#ifndef NDEBUG
 	if (query->flags & QUERY_TRACE) {
 		VERBOSE_MSG("<= answer received:\n");
 		kr_pkt_print(pkt);
 	}
-
+#endif
 	if (query->flags & (QUERY_RESOLVED|QUERY_BADCOOKIE_AGAIN)) {
 		return ctx->state;
 	}
