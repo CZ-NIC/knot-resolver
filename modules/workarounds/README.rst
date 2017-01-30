40c68f7dd7c62c53954452faa63c9cc50af96fe2
Vladimír Čunát <vladimir.cunat@nic.cz>
2017-01-30 17:38:27 +0100

    workarounds: prototype of a new module
    including code to deal with #128 and #140.

---
+++ modules/workarounds/README.rst
@@ -0,0 +1,4 @@
+.. _mod-workarounds:
+
+FIXME
+
---
+++ modules/workarounds/workarounds.lua
@@ -0,0 +1,20 @@
+-- Load dependent module
+if not policy then modules.load('policy') end
+
+local M = {} -- the module
+
+function M.config()
+	policy.add(policy.suffix(policy.FLAGS(kres.query.NO_0X20), {
+	--  https://github.com/DNS-OARC/dns-violations/blob/master/2017/DVE-2017-0003.md
+		todname('avqs.mcafee.com'),
+
+	--  https://github.com/DNS-OARC/dns-violations/blob/master/2017/DVE-2017-0006.md
+	--  Obtained via a reverse search on {ns1,ns3}.panthercdn.com.
+		todname('cdnga.com'), todname('cdngc.com'), todname('cdngd.com'),
+		todname('cdngl.com'), todname('cdngm.com'),
+		todname('cdngc.net'), todname('panthercdn.com'),
+	}))
+end
+
+return M
+
