911f179f1c8018b363b1eba5bbcd8d2e7df169d2
Vladimír Čunát <vladimir.cunat@nic.cz>
2017-07-31 18:51:14 +0200

    policy aho-corasick: makefile tweaks
    - install the library with executable bit
    - use native library extension (i.e. .dylib on Macs)
    - kill their fancy CFLAGS to get better portability (e.g. -msse4.1)
    - gitlab-ci: this submodule is needed before building already
    - actually remove aho-corasick.lua

--- modules/policy/policy.mk
+++ modules/policy/policy.mk
@@ -1,9 +1,16 @@
-policy_SOURCES := policy.lua lua-aho-corasick/ahocorasick.so
-policy_DEPEND := modules/policy/lua-aho-corasick/ahocorasick.so
+AHOCORASICK_DIR = modules/policy/lua-aho-corasick/
+
+policy_SOURCES := policy.lua
+policy_DEPEND := $(AHOCORASICK_DIR)ahocorasick$(LIBEXT)
 $(call make_lua_module,policy)
 
-AHOCORASICK_DIR = modules/policy/lua-aho-corasick/
 policy-clean:
 	$(MAKE) -C $(AHOCORASICK_DIR) clean
-$(AHOCORASICK_DIR)ahocorasick.so: $(AHOCORASICK_DIR)Makefile
-	$(MAKE) -C $(AHOCORASICK_DIR) MY_CFLAGS=$(lua_CFLAGS) SO_EXT=so
+$(AHOCORASICK_DIR)ahocorasick$(LIBEXT): $(AHOCORASICK_DIR)Makefile
+	$(MAKE) -C $(AHOCORASICK_DIR) ahocorasick$(LIBEXT) CFLAGS="$(lua_CFLAGS) -O2 $(CFLAGS)"
+	@# CFLAGS overridden to get rid of -msse4.1 etc.
+
+policy-install: ahocorasick-install
+ahocorasick-install: $(AHOCORASICK_DIR)ahocorasick$(LIBEXT) $(DESTDIR)$(MODULEDIR)
+	$(INSTALL) -m 755 $(AHOCORASICK_DIR)ahocorasick$(LIBEXT) $(DESTDIR)$(MODULEDIR)
+
