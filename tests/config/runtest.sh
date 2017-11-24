a4fbf6f7f4ba8b1812f4eab1924f2fdb599b8f5e
Petr Špaček <petr.spacek@nic.cz>
2017-11-24 15:15:25 +0100

    CI: run installcheck tests with and without Valgrind

--- tests/config/runtest.sh
+++ tests/config/runtest.sh
@@ -1,11 +1,11 @@
 #!/bin/sh -e
-export TMP_RUNDIR=`mktemp -d`
+export TMP_RUNDIR="$(mktemp -d)"
 function finish {
-  rm -rf ${TMP_RUNDIR}
+  rm -rf "${TMP_RUNDIR}"
 }
 trap finish EXIT
 
 echo "config-test: ${2}"
-cp tests/config/${2}/* ${TMP_RUNDIR}/
-cp tests/config/test_utils.lua ${TMP_RUNDIR}/
-KRESD_NO_LISTEN=1 ${1} -f 1 -c test.cfg ${TMP_RUNDIR}
+cp "tests/config/${2}/"* "${TMP_RUNDIR}/"
+cp tests/config/test_utils.lua "${TMP_RUNDIR}/"
+KRESD_NO_LISTEN=1 ${DEBUGGER} ${1} -f 1 -c test.cfg "${TMP_RUNDIR}"
