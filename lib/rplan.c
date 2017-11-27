26adb0b0643d6cc98ac8474f89cc7f93b7385c7f
Marek Vavruša <mvavrusa@cloudflare.com>
2017-11-27 14:59:55 -0800

    lib/rplan: remember request context in each query
    This doesn't affect any of the objects lifetime, just provides a
    convenience for logging as some subsystems take only pointer of queries
    not the request object.

--- lib/rplan.c
+++ lib/rplan.c
@@ -172,6 +172,7 @@ static struct kr_query *kr_rplan_push_query(struct kr_rplan *rplan,
 	/* Class and type must be set outside this function. */
 	qry->flags = rplan->request->options;
 	qry->parent = parent;
+	qry->request = rplan->request;
 	qry->ns.ctx = rplan->request->ctx;
 	qry->ns.addr[0].ip.sa_family = AF_UNSPEC;
 	gettimeofday(&qry->timestamp, NULL);
