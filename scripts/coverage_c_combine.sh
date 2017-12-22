b2497b13e7d13d6430c53dc537183f47e0d6ded7
Petr Špaček <petr.spacek@nic.cz>
2017-12-22 13:21:24 +0100

    CI coverage: merge test coverage data from parallel runs
    We run tests in paralell so have to make sure that coverage tools
    do not overwrite results from each run.
    
    This is hacky because lcov tool insists on having gcno and gcda files
    in the same place as original source, so we have to copy files
    to workaround this.

---
+++ scripts/coverage_c_combine.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+# $1 = top source directory
+# $2 = coverage data directory path
+# $3 = output directory for *.info files
+
+set -o errexit -o nounset
+shopt -s nullglob
+IFS=$'\n'
+
+TOPSRCDIR="$1"
+DATAROOT="$2"
+OUTDIR="$3"
+
+cd "${TOPSRCDIR}"
+for COVNAME in $(find "${DATAROOT}" -name .topdir_kresd_coverage)
+do
+	find "${DATAROOT}" -name '*.gcda' -not -path "${DATAROOT}/*" -delete
+	COVDIR="$(dirname "${COVNAME}")"
+	COVDATA_FILENAMES=("${COVDIR}"/*)  # filenames in BASH array
+	(( ${#COVDATA_FILENAMES[*]} )) || continue  # skip empty dirs
+
+	cp -r -t ${TOPSRCDIR} "${COVDIR}"/*
+	${LCOV} -q --no-external --capture -d lib -d daemon -d modules -o "$(mktemp -p "${OUTDIR}" -t XXXXXXXX.c.info)" > /dev/null
+done
