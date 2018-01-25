a11dbf2b6402a4bba570cf2e476e3816a0eb1451
Petr Špaček <petr.spacek@nic.cz>
2018-01-25 13:53:17 +0100

    CI: test doc build

--- doc/doc.mk
+++ doc/doc.mk
@@ -2,7 +2,7 @@ ifeq ($(HAS_doxygen)|$(HAS_sphinx-build), yes|yes)
 doc-doxygen:
 	@cd doc && $(doxygen_BIN)
 doc-html: doc-doxygen
-	@cd doc && $(sphinx-build_BIN) -b html . html
+	@cd doc && $(sphinx-build_BIN) $(SPHINXFLAGS) -b html . html
 else
 doc-html:
 	$(error doxygen and sphinx must be installed)
