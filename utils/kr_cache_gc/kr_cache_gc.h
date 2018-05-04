55a7670f716ca933e92eef5a5bada9b04478287d
Libor Peltan <libor.peltan@nic.cz>
2018-05-04 15:27:16 +0200

    kr_cache_gc: implemented categories, code split and cleanup
    categories: different level of what should be deleted based on DB usage

--- utils/kr_cache_gc/kr_cache_gc.h
+++ utils/kr_cache_gc/kr_cache_gc.h
@@ -1,6 +1,15 @@
 #pragma once
 
 #include <stddef.h>
+#include <stdint.h>
+
+typedef struct {
+        int64_t expires_in;         // < 0 => already expired
+        uint16_t rrtype;
+        uint8_t no_labels;          // 0 == ., 1 == root zone member, 2 == TLD member ...
+        uint8_t rank;
+        size_t entry_size;          // amount of bytes occupied in cache by this record
+} gc_record_info_t;
 
 typedef struct {
         const char *cache_path;          // path to the LMDB with resolver cache
@@ -16,4 +25,4 @@ typedef struct {
 
 int kr_cache_gc(kr_cache_gc_cfg_t *cfg);
 
-#define KR_CACHE_GC_VERSION "0.1"
+#define KR_CACHE_GC_VERSION "0.2"
