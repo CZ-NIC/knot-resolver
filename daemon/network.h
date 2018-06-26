d9ca7e639c4be519376438f34fcfe1f415e17ebb
Grigorii Demidov <grigorii.demidov@nic.cz>
2018-06-26 10:49:23 +0200

    daemon: make idle timeout for incoming connection configurable

--- daemon/network.h
+++ daemon/network.h
@@ -42,6 +42,10 @@ struct endpoint {
 typedef array_t(struct endpoint*) endpoint_array_t;
 /* @endcond */
 
+struct net_tcp_param {
+	uint64_t in_idle_timeout;
+};
+
 struct tls_session_ticket_ctx;
 struct network {
 	uv_loop_t *loop;
@@ -49,6 +53,7 @@ struct network {
 	struct tls_credentials *tls_credentials;
 	map_t tls_client_params;
 	struct tls_session_ticket_ctx *tls_session_ticket_ctx;
+	struct net_tcp_param tcp;
 };
 
 void network_init(struct network *net, uv_loop_t *loop);
