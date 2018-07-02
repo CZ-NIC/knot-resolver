ce08ba2d67095a91bab745e0097ae7081389332f
Vladimír Čunát <vladimir.cunat@nic.cz>
2018-07-02 14:13:19 +0200

    lib/utils: naive refactoring of kr_dname_lf()
    It's not optimal; we can improve performance later.

--- lib/utils.h
+++ lib/utils.h
@@ -420,12 +420,14 @@ int knot_dname_lf2wire(knot_dname_t *dst, uint8_t len, const uint8_t *lf);
  */
 static inline int kr_dname_lf(uint8_t *dst, const knot_dname_t *src, bool add_wildcard)
 {
-	int ret = knot_dname_lf(dst, src, NULL);
-	if (ret)
-		return ret;
-	int len = dst[0];
-	if (len == 1)
-		len = 0;
+	knot_dname_storage_t right_aligned_dst;
+	uint8_t *right_aligned_dname_start = knot_dname_lf(src, right_aligned_dst);
+	if (!right_aligned_dname_start) {
+		return kr_error(EINVAL);
+	}
+	int len = right_aligned_dname_start[0];
+	assert(right_aligned_dname_start + 1 + len - KNOT_DNAME_MAXLEN == right_aligned_dst);
+	memcpy(dst + 1, right_aligned_dname_start + 1, len);
 	if (add_wildcard) {
 		if (len + 2 > KNOT_DNAME_MAXLEN)
 			return kr_error(ENOSPC);
