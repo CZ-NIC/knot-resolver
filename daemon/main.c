7cc8f5b08f860deba7ead99cb6a90a97fdcb8f7b
Vladimír Čunát <vladimir.cunat@nic.cz>
2018-09-12 14:59:46 +0200

    cache: improve out-of-disk condition
    When suspect SIGBUS happens, print helpful error and try to remove
    the cache, so that the service might work again if auto-restarted.
    Theoretically we could longjmp() out of the SIGBUS handler,
    but that would be rather messy, so let the process die.

--- daemon/main.c
+++ daemon/main.c
@@ -15,21 +15,23 @@
  */
 
 #include <arpa/inet.h>
+#include <assert.h>
+#include <getopt.h>
+#include <libgen.h>
 #include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <getopt.h>
-#include <libgen.h>
+#include <unistd.h>
+
 #include <uv.h>
-#include <assert.h>
-#include <contrib/cleanup.h>
-#include <contrib/ucw/mempool.h>
-#include <contrib/ccan/asprintf/asprintf.h>
-#include <libknot/error.h>
 #ifdef HAS_SYSTEMD
 #include <systemd/sd-daemon.h>
 #endif
+#include <libknot/error.h>
 
+#include <contrib/cleanup.h>
+#include <contrib/ucw/mempool.h>
+#include <contrib/ccan/asprintf/asprintf.h>
 #include "lib/defines.h"
 #include "lib/resolve.h"
 #include "lib/dnssec.h"
@@ -298,6 +300,30 @@ static void signal_handler(uv_signal_t *handle, int signum)
 {
 	uv_stop(uv_default_loop());
 	uv_signal_stop(handle);
+}
+
+/** SIGBUS -> attempt to remove the overflowing cache file and abort. */
+static void sigbus_handler(int sig, siginfo_t *siginfo, void *ptr)
+{
+	const char msg_typical[] =
+		"SIGBUS received; this is most likely due to filling up the filesystem where cache resides.\n",
+		msg_unknown[] = "SIGBUS received, cause unknown.\n",
+		msg_deleted[] = "Cache file deleted.\n",
+		msg_del_fail[] = "Cache file deletion failed.\n";
+	/* We can't safely assume that printf-like functions work, but write() is OK. */
+	if (siginfo->si_code != BUS_ADRERR) {
+		write(STDERR_FILENO, msg_unknown, sizeof(msg_unknown));
+		goto end;
+	}
+	write(STDERR_FILENO, msg_typical, sizeof(msg_typical));
+	if (!kr_cache_emergency_file_to_remove) goto end;
+	if (unlink(kr_cache_emergency_file_to_remove)) {
+		write(STDERR_FILENO, msg_del_fail, sizeof(msg_del_fail));
+	} else {
+		write(STDERR_FILENO, msg_deleted, sizeof(msg_deleted));
+	}
+end:
+	exit(128 - sig); /*< regular return from SIGBUS handler can't work */
 }
 
 /** Split away port from the address. */
@@ -700,28 +726,36 @@ int main(int argc, char **argv)
 		goto cleanup;
 	}
 
-	/* Workaround for https://github.com/libuv/libuv/issues/45
-	 * (Write after ECONNRESET crash.) */
-	if (ret == 0 && signal(SIGPIPE, SIG_IGN) == SIG_ERR) {
-		kr_log_error("[system] can't block SIGPIPE signal: %s\n",
-				strerror(errno));
-		ret = EXIT_FAILURE;
-	}
+	/* Catch some signals. */
 
-	if (ret != 0) {
+	loop = uv_default_loop();
+	uv_signal_t sigint, sigterm;
+	if (true) ret = uv_signal_init(loop, &sigint);
+	if (!ret) ret = uv_signal_init(loop, &sigterm);
+	if (!ret) ret = uv_signal_start(&sigint, signal_handler, SIGINT);
+	if (!ret) ret = uv_signal_start(&sigterm, signal_handler, SIGTERM);
+	/* Block SIGPIPE; see https://github.com/libuv/libuv/issues/45 */
+	if (!ret && signal(SIGPIPE, SIG_IGN) == SIG_ERR) ret = errno;
+	if (!ret) {
+		/* Catching SIGBUS via uv_signal_* doesn't work, apparently,
+		 * so let's use the vanilla POSIX way. */
+		struct sigaction sa;
+		memset(&sa, 0, sizeof(sa));
+		sa.sa_sigaction = sigbus_handler;
+		sa.sa_flags = SA_SIGINFO;
+		if (sigaction(SIGBUS, &sa, NULL)) {
+			ret = errno;
+		}
+	}
+	if (ret) {
+		kr_log_error("[system] failed to set up signal handlers: %s\n",
+				strerror(abs(errno)));
+		ret = EXIT_FAILURE;
 		goto cleanup;
 	}
 
-	engine_set_moduledir(&engine, args.moduledir);
-	
-	/* Block signals. */
-	loop = uv_default_loop();
-	uv_signal_t sigint, sigterm;
-	uv_signal_init(loop, &sigint);
-	uv_signal_init(loop, &sigterm);
-	uv_signal_start(&sigint, signal_handler, SIGINT);
-	uv_signal_start(&sigterm, signal_handler, SIGTERM);
 	/* Start the scripting engine */
+	engine_set_moduledir(&engine, args.moduledir);
 	worker->loop = loop;
 	loop->data = worker;
 
