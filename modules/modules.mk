3b3b00225e500a1ed838dcf0e107911661ca475d
Vladimír Čunát <vladimir.cunat@nic.cz>
2018-11-12 15:41:02 +0100

    modules/edns_keepalive: new module, loaded by default

--- modules/modules.mk
+++ modules/modules.mk
@@ -31,7 +31,8 @@ modules_TARGETS += bogus_log \
                    serve_stale \
                    detect_time_skew \
                    detect_time_jump \
-                   prefill
+                   prefill \
+                   edns_keepalive
 endif
 
 # Make C module
