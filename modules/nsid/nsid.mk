6fd85375c498c0c61b68878188baea9059cf23f3
Vladimír Čunát <vladimir.cunat@nic.cz>
2018-11-14 14:40:39 +0100

    modules/nsid: fix linkage on some systems
    This is my mistake.  I was convinced this line wasn't needed
    and I deleted it before merging the module.
    Apparently we still have some deficiencies in the build system,
    but let's defer that to migration (to meson probably).

--- modules/nsid/nsid.mk
+++ modules/nsid/nsid.mk
@@ -1,4 +1,5 @@
 nsid_CFLAGS := -fPIC
+nsid_LDFLAGS := -Wl,-undefined -Wl,dynamic_lookup
 nsid_SOURCES := modules/nsid/nsid.c
 nsid_DEPEND := $(libkres)
 nsid_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS)
