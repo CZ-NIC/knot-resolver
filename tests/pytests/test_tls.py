ff337331a88e7b9c40c96f9e474f908089711d4c
Tomas Krizek <tomas.krizek@nic.cz>
2018-11-16 10:28:56 +0100

    pytests: test_tls move + doc

--- tests/pytests/test_tls.py
+++ tests/pytests/test_tls.py
@@ -1,4 +1,4 @@
-"""Tests with TLS certificates"""
+"""TLS-specific tests"""
 
 import ssl
 
@@ -8,6 +8,7 @@ import utils
 
 
 def test_tls_no_cert(kresd, sock_family):
+    """Use TLS without certificates."""
     sock, dest = kresd.stream_socket(sock_family, tls=True)
     ctx = utils.make_ssl_context(insecure=True)
     ssock = ctx.wrap_socket(sock)
@@ -17,6 +18,7 @@ def test_tls_no_cert(kresd, sock_family):
 
 
 def test_tls_selfsigned_cert(kresd_tt, sock_family):
+    """Use TLS with a self signed certificate."""
     sock, dest = kresd_tt.stream_socket(sock_family, tls=True)
     ctx = utils.make_ssl_context(verify_location=kresd_tt.tls_cert_path)
     ssock = ctx.wrap_socket(sock, server_hostname='transport-test-server.com')
@@ -26,6 +28,7 @@ def test_tls_selfsigned_cert(kresd_tt, sock_family):
 
 
 def test_tls_cert_hostname_mismatch(kresd_tt, sock_family):
+    """Attempt to use self signed certificate and incorrect hostname."""
     sock, dest = kresd_tt.stream_socket(sock_family, tls=True)
     ctx = utils.make_ssl_context(verify_location=kresd_tt.tls_cert_path)
     ssock = ctx.wrap_socket(sock, server_hostname='wrong-host-name')
@@ -35,6 +38,7 @@ def test_tls_cert_hostname_mismatch(kresd_tt, sock_family):
 
 
 def test_tls_cert_expired(kresd_tt_expired, sock_family):
+    """Attempt to use expired certificate."""
     sock, dest = kresd_tt_expired.stream_socket(sock_family, tls=True)
     ctx = utils.make_ssl_context(verify_location=kresd_tt_expired.tls_cert_path)
     ssock = ctx.wrap_socket(sock, server_hostname='transport-test-server.com')
