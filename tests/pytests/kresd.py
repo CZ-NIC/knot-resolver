4199c3a2bbb744b1dd0559b81d662b7c20781316
Tomas Krizek <tomas.krizek@nic.cz>
2018-11-27 15:54:12 +0100

    pytests: add rehandshake test

--- tests/pytests/kresd.py
+++ tests/pytests/kresd.py
@@ -223,9 +223,11 @@ KRESD_LOG_IO_CLOSE = re.compile(r'^\[io\].*closed by peer.*')
 
 
 @contextmanager
-def make_kresd(workdir, certname=None, ip='127.0.0.1', ip6='::1', forward=None, hints=None):
-    port = make_port(ip, ip6)
-    tls_port = make_port(ip, ip6)
+def make_kresd(
+        workdir, certname=None, ip='127.0.0.1', ip6='::1', forward=None, hints=None,
+        port=None, tls_port=None):
+    port = make_port(ip, ip6) if port is None else port
+    tls_port = make_port(ip, ip6) if tls_port is None else tls_port
     with Kresd(workdir, port, tls_port, ip, ip6, certname, forward=forward, hints=hints) as kresd:
         yield kresd
         with open(kresd.logfile_path) as log:  # display partial log for debugging
---
+++ tests/pytests/test_rehandshake.py
@@ -0,0 +1,63 @@
+"""TLS rehandshake test
+
+Test utilizes rehandshake/tls-proxy, which forwards queries to configured
+resolver, but when it sends the response back to the query source, it
+performs a rehandshake after every byte sent.
+
+It is expected the answer will be received by the source kresd instance
+and sent back to the client (this test).
+
+Make sure to run `make all` in `rehandshake/` to compile the proxy.
+"""
+
+import os
+import subprocess
+
+import pytest
+
+from kresd import Forward, make_kresd, PYTESTS_DIR
+import utils
+
+
+REHANDSHAKE_PROXY = os.path.join(PYTESTS_DIR, 'rehandshake', 'tlsproxy')
+
+
+@pytest.mark.skipif(not os.path.exists(REHANDSHAKE_PROXY),
+                    reason="tlsproxy not found (did you compile it?)")
+def test_rehandshake(tmpdir):
+    def resolve_hint(sock, qname):
+        buff, msgid = utils.get_msgbuff(qname)
+        sock.sendall(buff)
+        answer = utils.receive_parse_answer(sock)
+        assert answer.id == msgid
+        assert answer.answer[0][0].address == '127.0.0.1'
+
+    hints = {
+        '0.foo': '127.0.0.1',
+        '1.foo': '127.0.0.1',
+        '2.foo': '127.0.0.1',
+    }
+    # run forward target instance
+    workdir = os.path.join(str(tmpdir), 'kresd_fwd_target')
+    os.makedirs(workdir)
+
+    with make_kresd(workdir, 'tt', hints=hints, tls_port=53000) as kresd_fwd_target:
+        sock = kresd_fwd_target.ip_tls_socket()
+        resolve_hint(sock, '0.foo')
+
+        # run proxy
+        cwd, cmd = os.path.split(REHANDSHAKE_PROXY)
+        cmd = './' + cmd
+        try:
+            proxy = subprocess.Popen([cmd], cwd=cwd)
+
+            # run test kresd instance
+            workdir2 = os.path.join(str(tmpdir), 'kresd')
+            os.makedirs(workdir2)
+            forward = Forward(proto='tls', ip='127.0.0.1', port=54000)
+            with make_kresd(workdir2, forward=forward) as kresd:
+                sock2 = kresd.ip_tls_socket()
+                for hint in hints:
+                    resolve_hint(sock2, hint)
+        finally:
+            proxy.terminate()
