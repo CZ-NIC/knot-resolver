0597e7bef4e8998ee7d72e0ca39b4e0188bbe1e0
Tomas Krizek <tomas.krizek@nic.cz>
2018-12-12 17:07:26 +0100

    fixup! WIP debug

--- tests/pytests/conn_flood.py
+++ tests/pytests/conn_flood.py
@@ -17,7 +17,7 @@ from kresd import Kresd
 import utils
 
 
-MAX_SOCKETS = 10000  # upper bound of how many connections to open
+MAX_SOCKETS = 20000  # upper bound of how many connections to open
 MAX_ITERATIONS = 20  # number of iterations to run the test
 
 # we can't use softlimit ifself since kresd already has open sockets,
@@ -32,22 +32,22 @@ RESERVED_NOFILE = 40  # 40 is empirical value
     'ip6_tls_socket',
 ])
 def test_conn_flood(tmpdir, sock_func_name):
-    def create_sockets(make_sock, nsockets):
+    def create_sockets(make_sock, nsockets, max_iters=3):
         sockets = []
-        next_ping = time.time() + 5  # less than tcp idle timeout
-        while True:
+        next_ping = time.time() + 3  # less than tcp idle timeout
+        for _ in range(max_iters):
             while time.time() < next_ping:
                 nsock_to_init = min(100, nsockets - len(sockets))
                 if not nsock_to_init:
                     return sockets
                 sockets.extend([make_sock() for _ in range(nsock_to_init)])
-            print(len(sockets))
 
             # large number of connections can take a lot of time to open
             # send some valid data to avoid TCP idle timeout for already open sockets
-            next_ping = time.time() + 5
+            next_ping = time.time() + 3
             for s in sockets:
                 utils.ping_alive(s)
+        return sockets
 
     max_num_of_open_files = resource.getrlimit(resource.RLIMIT_NOFILE)[0] - RESERVED_NOFILE
     nsockets = min(max_num_of_open_files, MAX_SOCKETS)
@@ -56,9 +56,9 @@ def test_conn_flood(tmpdir, sock_func_name):
     ip = '127.0.0.1'
     ip6 = '::1'
     with Kresd(tmpdir, ip=ip, ip6=ip6, verbose=False) as kresd:
-        print("\nEstablishing {} connections".format(nsockets))
         make_sock = getattr(kresd, sock_func_name)  # function for creating sockets
         sockets = create_sockets(make_sock, nsockets)
+        print("\nEstablished {} connections".format(len(sockets)))
 
         print("Start sending data")
         for i in range(MAX_ITERATIONS):
