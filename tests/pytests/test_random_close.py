0b2a8c5484057cd9503e757eb6dca908b671c3d4
Tomas Krizek <tomas.krizek@nic.cz>
2019-01-09 16:42:31 +0100

    pytest/test_random_close: use new proxy

--- tests/pytests/test_random_close.py
+++ tests/pytests/test_random_close.py
@@ -1,26 +1,18 @@
 """TLS test when forward target closes connection after one second
 
-Test utilizes random_close/tls-proxy, which forwards queries to configured
+Test utilizes TLS proxy, which forwards queries to configured
 resolver, but closes the connection 1s after establishing.
 
 Kresd must stay alive and be able to answer queries.
-
-Make sure to run `make all` in `random_close/` to compile the proxy.
 """
 
-import os
 import random
 import string
 import time
 
-import pytest
-
-from kresd import Forward, make_kresd, PYTESTS_DIR
-import proxyutils
+from proxy import HINTS, kresd_tls_client, resolve_hint, TLSProxy
 import utils
 
-
-PROXY_PATH = os.path.join(PYTESTS_DIR, 'random_close', 'tlsproxy')
 
 QPS = 500
 
@@ -41,29 +33,13 @@ def rsa_cannon(sock, duration, domain='test.', qps=QPS):
             time.sleep(time_left)
 
 
-@pytest.mark.skipif(not os.path.exists(PROXY_PATH),
-                    reason="{} not found (did you compile it?)".format(PROXY_PATH))
 def test_proxy_random_close(tmpdir):
-    # run forward target instance
-    workdir = os.path.join(str(tmpdir), 'kresd_fwd_target')
-    os.makedirs(workdir)
+    proxy = TLSProxy(close=1)
 
-    with make_kresd(workdir, hints=proxyutils.HINTS, port=54010,
-                    verbose=False) as kresd_fwd_target:
-        sock = kresd_fwd_target.ip_tls_socket()
-        proxyutils.resolve_hint(sock, list(proxyutils.HINTS.keys())[0])
-
-        with proxyutils.proxy(PROXY_PATH):
-            # run test kresd instance
-            workdir2 = os.path.join(str(tmpdir), 'kresd')
-            os.makedirs(workdir2)
-            forward = Forward(
-                proto='tls', ip='127.0.0.1', port=54021,
-                hostname='transport-test-server.com', ca_file=proxyutils.PROXY_CA_FILE)
-            with make_kresd(workdir2, forward=forward, policy_test_pass=True,
-                            verbose=False) as kresd:
-                sock2 = kresd.ip_tcp_socket()
-                rsa_cannon(sock2, 20)
-                sock3 = kresd.ip_tcp_socket()
-                for hint in proxyutils.HINTS:
-                    proxyutils.resolve_hint(sock3, hint)
+    with kresd_tls_client(str(tmpdir), proxy) as kresd:
+        sock2 = kresd.ip_tcp_socket()
+        rsa_cannon(sock2, 20)
+        sock3 = kresd.ip_tcp_socket()
+        for hint in HINTS:
+            resolve_hint(sock3, hint)
+            time.sleep(0.1)
