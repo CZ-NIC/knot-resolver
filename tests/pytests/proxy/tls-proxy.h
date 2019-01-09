29ae40ec1b1756644d114c79a7df5cc15e34d62a
Tomas Krizek <tomas.krizek@nic.cz>
2019-01-09 14:08:26 +0100

    pytests/proxy: import code
    Original author: Grigorii Demidov <grigorii.demidov@nic.cz>

---
+++ tests/pytests/proxy/tls-proxy.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <netinet/in.h>
+
+struct args {
+	const char *local_addr;
+	uint16_t local_port;
+	const char *upstream;
+	uint16_t upstream_port;
+
+	bool rehandshake;
+	bool close_connection;
+
+	uint64_t close_timeout;
+
+	const char *cert_file;
+	const char *key_file;
+};
+
+struct tls_proxy_ctx;
+
+struct tls_proxy_ctx *tls_proxy_allocate();
+void tls_proxy_free(struct tls_proxy_ctx *proxy);
+int tls_proxy_init(struct tls_proxy_ctx *proxy, const struct args *a);
+int tls_proxy_start_listen(struct tls_proxy_ctx *proxy);
+int tls_proxy_run(struct tls_proxy_ctx *proxy);
