33a631189775aa0ab00f9f1e8f75b247ad745d74
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-01-24 18:18:58 +0100

    split daemon/bindings.c file
    It's now a directory, one C file for each lua table of functions.
    We get more total lines due to per-file copyright headers,
    but the original file was just too long (nearing 2k lines).
    The layout was inspired by the lib/cache/ split.
    
    Disadvantage: git operations (e.g. blame) will stumble on this commit.
    
    During the move, rename the "str" and "xstr" macros, too.
    Otherwise there are no real changes, as they would be hard to spot.

---
+++ daemon/bindings/api.h
@@ -0,0 +1,26 @@
+/*  Copyright (C) 2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <lua.h>
+
+int lib_cache   (lua_State *L);
+int lib_event   (lua_State *L);
+int lib_modules (lua_State *L);
+int lib_net     (lua_State *L);
+int lib_worker  (lua_State *L);
+
---
+++ daemon/bindings/impl.h
@@ -0,0 +1,65 @@
+/*  Copyright (C) 2015-2019 CZ.NIC, z.s.p.o. <knot-dns@labs.nic.cz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "daemon/engine.h"
+
+#include <lua.h>
+#include <lauxlib.h>
+
+
+/** @internal Compatibility wrapper for Lua 5.0 - 5.2
+    https://www.lua.org/manual/5.2/manual.html#luaL_newlib
+ */
+#if LUA_VERSION_NUM >= 502
+#define register_lib(L, name, lib) \
+	luaL_newlib((L), (lib))
+#else
+#define lua_rawlen(L, obj) \
+	lua_objlen((L), (obj))
+#define register_lib(L, name, lib) \
+	luaL_openlib((L), (name), (lib), 0)
+#endif
+
+/** Useful to stringify #defines into error strings. */
+#define STR(s) STRINGIFY_INT(s)
+#define STRINGIFY_INT(s) #s
+
+/** @internal Prefix error with file:line
+ * Implementation lumped into ./worker.c */
+int KR_COLD format_error(lua_State* L, const char *err);
+/** @internal Annotate for static checkers. */
+KR_NORETURN int lua_error(lua_State *L);
+
+static inline struct worker_ctx *wrk_luaget(lua_State *L) {
+	lua_getglobal(L, "__worker");
+	struct worker_ctx *worker = lua_touserdata(L, -1);
+	lua_pop(L, 1);
+	return worker;
+}
+
+static inline int execute_callback(lua_State *L, int argc)
+{
+	int ret = engine_pcall(L, argc);
+	if (ret != 0) {
+		fprintf(stderr, "error: %s\n", lua_tostring(L, -1));
+	}
+	/* Clear the stack, there may be event a/o enything returned */
+	lua_settop(L, 0);
+	return ret;
+}
+
