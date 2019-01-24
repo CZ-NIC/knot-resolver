478ac0bbe9539fb00472d7330de36191c377f163
Tomas Krizek <tomas.krizek@nic.cz>
2019-01-24 14:58:01 +0100

    meson: build contrib as a subdir

--- contrib/base64.c
+++ contrib/base64.c
@@ -14,7 +14,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "contrib/base64.h"
+#include "base64.h"
 #include "libknot/errcode.h"
 
 #include <stdlib.h>
