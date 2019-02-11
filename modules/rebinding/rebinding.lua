1f1d6ed0e4ec7f307787f559578c4f7a6175ceda
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-11 17:10:01 +0100

    meson: change module names in lua code

--- modules/rebinding/rebinding.lua
+++ modules/rebinding/rebinding.lua
@@ -1,6 +1,6 @@
 -- Protection from DNS rebinding attacks
 local kres = require('kres')
-local renumber = require('renumber')
+local renumber = require('kres_module.renumber')
 
 local M = {}
 M.layer = {}
