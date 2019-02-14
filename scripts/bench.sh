df1a38ddb297254c2edb19a242ed6057bbd06311
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-14 17:08:43 +0100

    meson: bench - compile and run

---
+++ scripts/bench.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -o errexit -o nounset
+
+# Run benchmark
+cd "${MESON_SOURCE_ROOT}"
+
+echo "Test LRU with increasing overfill, misses should increase ~ linearly"
+
+for num in 65536 32768 16384 8192 4096; do
+    "${MESON_BUILD_ROOT}/${MESON_SUBDIR}/bench_lru" 23 "${MESON_SOURCE_ROOT}/${MESON_SUBDIR}/bench_lru_set1.tsv" - "${num}"
+done
