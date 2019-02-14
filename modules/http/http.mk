3cf8c3185ee7697f408432e61bdf789b41009428
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-14 16:05:18 +0100

    meson: remove no longer needed makefiles

--- modules/http/http.mk
+++
@@ -1,3 +0,0 @@
-http_SOURCES := http.lua prometheus.lua http_trace.lua
-http_INSTALL := $(wildcard modules/http/static/*)
-$(call make_lua_module,http)
--- modules/ta_sentinel/ta_sentinel.mk
+++
@@ -1,2 +0,0 @@
-ta_sentinel_SOURCES := ta_sentinel.lua
-$(call make_lua_module,ta_sentinel)
