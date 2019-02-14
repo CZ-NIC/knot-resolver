295e35e84dc921addf684599b111171d4839a2d3
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-14 16:05:18 +0100

    meson: remove no longer needed makefiles

--- modules/stats/stats.mk
+++
@@ -1,5 +0,0 @@
-stats_CFLAGS := -fPIC
-stats_SOURCES := modules/stats/stats.c
-stats_DEPEND := $(libkres)
-stats_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS)
-$(call make_c_module,stats)
