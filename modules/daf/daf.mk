c418a7ef9a6eb3209a9b488eee67b447774b12c6
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-14 16:05:18 +0100

    meson: remove no longer needed makefiles

--- modules/daf/daf.mk
+++
@@ -1,3 +0,0 @@
-daf_SOURCES := daf.lua
-daf_INSTALL := modules/daf/daf.js
-$(call make_lua_module,daf)
--- modules/hints/hints.mk
+++
@@ -1,8 +0,0 @@
-hints_CFLAGS := -fPIC
-# We use a symbol that's not in libkres but the daemon.
-# On darwin this isn't accepted by default.
-hints_LDFLAGS := -Wl,-undefined -Wl,dynamic_lookup
-hints_SOURCES := modules/hints/hints.c
-hints_DEPEND := $(libkres)
-hints_LIBS := $(contrib_TARGET) $(libkres_TARGET) $(libkres_LIBS)
-$(call make_c_module,hints)
