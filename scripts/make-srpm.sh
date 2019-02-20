f326023622c0c88c6d9dd45416a4993c5a1098ff
Tomas Krizek <tomas.krizek@nic.cz>
2019-02-20 15:36:45 +0100

    gitlabci: fix pkg:srpm

--- scripts/make-srpm.sh
+++ scripts/make-srpm.sh
@@ -4,7 +4,7 @@ set -o errexit -o nounset -o xtrace
 # Create a source rpm for Fedora/EPEL
 
 cd "$(git rev-parse --show-toplevel)"
-scripts/make-archive.sh
+scripts/make-dev-archive.sh
 scripts/make-distrofiles.sh
 mv knot-resolver_*.orig.tar.xz distro/rpm/
 cd distro/rpm
