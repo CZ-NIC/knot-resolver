c198b4c5cfd1b9fa39aa8def39208a5c862b2f8a
Tomas Krizek <tomas.krizek@nic.cz>
2019-04-10 13:10:54 +0200

    scripts: update obs-testbuild.sh

--- scripts/obs-testbuild.sh
+++ scripts/obs-testbuild.sh
@@ -29,7 +29,7 @@ if [[ $(git status --porcelain | wc -l) -ne 0 ]]; then
 fi
 
 # Create tarball
-scripts/make-archive.sh
+scripts/make-dev-archive.sh
 
 # Submit to OBS
 scripts/make-distrofiles.sh -s
