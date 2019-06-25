6828f033af1f2881377e8a7e8a8e75bd636f2b44
Petr Špaček <petr.spacek@nic.cz>
2019-06-25 13:00:22 +0200

    gc: delete inconsistent entries from cache instead of skipping them

--- utils/cache_gc/kr_cache_gc.h
+++ utils/cache_gc/kr_cache_gc.h
@@ -5,11 +5,12 @@
 #include <stdint.h>
 
 typedef struct {
+        size_t entry_size;          // amount of bytes occupied in cache by this record
+        bool valid;                 // fields further down are valid (ignore them if false)
         int64_t expires_in;         // < 0 => already expired
         uint16_t rrtype;
         uint8_t no_labels;          // 0 == ., 1 == root zone member, 2 == TLD member ...
         uint8_t rank;
-        size_t entry_size;          // amount of bytes occupied in cache by this record
 } gc_record_info_t;
 
 typedef struct {
