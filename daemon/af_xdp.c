3bda49d0517e0af3d26b4798368add800c740ab5
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-08-26 18:57:52 +0200

    copy some code

---
+++ daemon/af_xdp.c
@@ -0,0 +1,197 @@
+#include <errno.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <bpf/xsk.h>
+
+
+// placate libclang :-/
+typedef uint64_t size_t;
+
+
+#define INVALID_UMEM_FRAME SIZE_MAX
+
+#define FRAME_SIZE 2048
+#define NUM_FRAMES 1024
+static const size_t packet_buffer_size = NUM_FRAMES * FRAME_SIZE;
+
+void *packet_buffer;
+
+
+
+struct config {
+	uint32_t xdp_flags;
+	int ifindex;
+	char *ifname;
+	//char ifname_buf[IF_NAMESIZE];
+	int redirect_ifindex;
+	char *redirect_ifname;
+	//char redirect_ifname_buf[IF_NAMESIZE];
+	bool do_unload;
+	bool reuse_maps;
+	char pin_dir[512];
+	char filename[512];
+	char progsec[32];
+	char src_mac[18];
+	char dest_mac[18];
+	uint16_t xsk_bind_flags;
+	int xsk_if_queue;
+	bool xsk_poll_mode;
+};
+
+struct xsk_umem_info {
+	struct xsk_ring_prod fq;
+	struct xsk_ring_cons cq;
+	struct xsk_umem *umem;
+	void *buffer;
+};
+struct xsk_socket_info {
+	struct xsk_ring_cons rx;
+	struct xsk_ring_prod tx;
+	struct xsk_umem_info *umem;
+	struct xsk_socket *xsk;
+
+	uint64_t umem_frame_addr[NUM_FRAMES];
+	uint32_t umem_frame_free;
+
+	uint32_t outstanding_tx;
+
+	//struct stats_record stats;
+	//struct stats_record prev_stats;
+};
+
+
+
+static struct xsk_umem_info *configure_xsk_umem(void *buffer, size_t size)
+{
+	struct xsk_umem_info *umem;
+	int ret;
+
+	umem = calloc(1, sizeof(*umem));
+	if (!umem)
+		return NULL;
+
+	ret = xsk_umem__create(&umem->umem, buffer, size, &umem->fq, &umem->cq,
+			       NULL);
+	if (ret) {
+		errno = -ret;
+		return NULL;
+	}
+
+	umem->buffer = buffer;
+	return umem;
+}
+
+static uint64_t xsk_alloc_umem_frame(struct xsk_socket_info *xsk) // TODO: confusing to use xsk_
+{
+	uint64_t frame;
+	if (xsk->umem_frame_free == 0)
+		return INVALID_UMEM_FRAME;
+
+	frame = xsk->umem_frame_addr[--xsk->umem_frame_free];
+	xsk->umem_frame_addr[xsk->umem_frame_free] = INVALID_UMEM_FRAME;
+	return frame;
+}
+
+
+static struct xsk_socket_info *xsk_configure_socket(struct config *cfg,
+						    struct xsk_umem_info *umem)
+{
+	struct xsk_socket_config xsk_cfg;
+	struct xsk_socket_info *xsk_info;
+	uint32_t idx;
+	uint32_t prog_id = 0;
+	int i;
+	int ret;
+
+	xsk_info = calloc(1, sizeof(*xsk_info));
+	if (!xsk_info)
+		return NULL;
+
+	xsk_info->umem = umem;
+	xsk_cfg.rx_size = XSK_RING_CONS__DEFAULT_NUM_DESCS;
+	xsk_cfg.tx_size = XSK_RING_PROD__DEFAULT_NUM_DESCS;
+	xsk_cfg.libbpf_flags = 0;
+	xsk_cfg.xdp_flags = cfg->xdp_flags;
+	xsk_cfg.bind_flags = cfg->xsk_bind_flags;
+	ret = xsk_socket__create(&xsk_info->xsk, cfg->ifname,
+				 cfg->xsk_if_queue, umem->umem, &xsk_info->rx,
+				 &xsk_info->tx, &xsk_cfg);
+
+	if (ret)
+		goto error_exit;
+
+	ret = bpf_get_link_xdp_id(cfg->ifindex, &prog_id, cfg->xdp_flags);
+	if (ret)
+		goto error_exit;
+
+	/* Initialize umem frame allocation */
+
+	for (i = 0; i < NUM_FRAMES; i++)
+		xsk_info->umem_frame_addr[i] = i * FRAME_SIZE;
+
+	xsk_info->umem_frame_free = NUM_FRAMES;
+
+	/* Stuff the receive path with buffers, we assume we have enough */
+	ret = xsk_ring_prod__reserve(&xsk_info->umem->fq,
+				     XSK_RING_PROD__DEFAULT_NUM_DESCS,
+				     &idx);
+
+	if (ret != XSK_RING_PROD__DEFAULT_NUM_DESCS)
+		goto error_exit;
+
+	for (i = 0; i < XSK_RING_PROD__DEFAULT_NUM_DESCS; i ++)
+		*xsk_ring_prod__fill_addr(&xsk_info->umem->fq, idx++) =
+			xsk_alloc_umem_frame(xsk_info);
+
+	xsk_ring_prod__submit(&xsk_info->umem->fq,
+			      XSK_RING_PROD__DEFAULT_NUM_DESCS);
+
+	return xsk_info;
+
+error_exit:
+	errno = -ret;
+	return NULL;
+}
+
+
+
+
+
+int main(int argc, char **argv)
+{
+	/* Allocate memory for NUM_FRAMES of the default XDP frame size */
+	if (posix_memalign(&packet_buffer,
+			   getpagesize(), /* PAGE_SIZE aligned */
+			   packet_buffer_size)) {
+		fprintf(stderr, "ERROR: Can't allocate buffer memory \"%s\"\n",
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	/* Initialize shared packet_buffer for umem usage */
+	struct xsk_umem_info *umem = configure_xsk_umem(packet_buffer, packet_buffer_size);
+	if (umem == NULL) {
+		fprintf(stderr, "ERROR: Can't create umem \"%s\"\n",
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+
+	struct config cfg = {
+		.ifindex = 0,
+		.ifname = "enp0s31f6",
+	};
+
+	/* Open and configure the AF_XDP (xsk) socket */
+	struct xsk_socket_info *xsk_socket = xsk_configure_socket(&cfg, umem);
+	if (xsk_socket == NULL) {
+		fprintf(stderr, "ERROR: Can't setup AF_XDP socket \"%s\"\n",
+			strerror(errno));
+		exit(EXIT_FAILURE);
+	}
+}
+
