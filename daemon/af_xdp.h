e9324a3d815cba93e7d9b13a85794f42e0ced18b
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-08-28 13:23:39 +0200

    commit the header that's been missing all the time

---
+++ daemon/af_xdp.h
@@ -0,0 +1,16 @@
+
+#pragma once
+
+#include <stdint.h>
+
+int kr_xsk_init_global();
+
+void *kr_xsk_alloc_wire(uint16_t *maxlen);
+
+struct sockaddr;
+struct kr_request;
+struct qr_task;
+/** Send req->answer via UDP, possibly not immediately. */
+void kr_xsk_push(const struct sockaddr *src, const struct sockaddr *dest,
+		 struct kr_request *req, struct qr_task *task);
+
