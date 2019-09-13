fec98963024eeb589baa5054f6cef906fb430bba
Vladimír Čunát <vladimir.cunat@nic.cz>
2019-09-13 18:14:54 +0200

    iterate nitpick: drop unused function parameter

--- lib/layer/iterate.c
+++ lib/layer/iterate.c
@@ -474,7 +474,7 @@ static int process_authority(knot_pkt_t *pkt, struct kr_request *req)
 	return result;
 }
 
-static void finalize_answer(knot_pkt_t *pkt, struct kr_query *qry, struct kr_request *req)
+static void finalize_answer(knot_pkt_t *pkt, struct kr_request *req)
 {
 	/* Finalize header */
 	knot_pkt_t *answer = req->answer;
@@ -677,7 +677,7 @@ static int process_final(knot_pkt_t *pkt, struct kr_request *req,
 			array->at[last_idx] = entry;
 			entry->to_wire = true;
 		}
-		finalize_answer(pkt, query, req);
+		finalize_answer(pkt, req);
 		return KR_STATE_DONE;
 	}
 	return kr_ok();
@@ -801,7 +801,7 @@ static int process_answer(knot_pkt_t *pkt, struct kr_request *req)
 		if (state != kr_ok()) {
 			return KR_STATE_FAIL;
 		}
-		finalize_answer(pkt, query, req);
+		finalize_answer(pkt, req);
 	} else {
 		/* Answer for sub-query; DS, IP for NS etc.
 		 * It may contains NSEC \ NSEC3 records for
@@ -864,7 +864,7 @@ static int process_stub(knot_pkt_t *pkt, struct kr_request *req)
 		return KR_STATE_FAIL;
 	}
 
-	finalize_answer(pkt, query, req);
+	finalize_answer(pkt, req);
 	return KR_STATE_DONE;
 }
 
