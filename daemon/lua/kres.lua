CZ-NIC/knot-resolver

commit b42195a256ee2c5dd8d55c1e68215fdb5288caa4

lua bindings: compatibility with both libknot versions

daemon/lua/kres.lua
@@ -57,6 +57,40 @@ int inet_pton(int af, const char *src, void *dst);
 int gettimeofday(struct timeval *tv, struct timezone *tz);
 ]]
 
+
+-- TMP: compatibility with both libknot 2.8 and 2.9
+local knot_rdataset_t_cdef
+local sover_pos = string.find(libknot_SONAME, '%d')
+if not sover_pos then
+	error('unexpected libknot soname: ' .. libknot_SONAME)
+end
+local sover = string.sub(libknot_SONAME, sover_pos , sover_pos)
+if sover == '8' then
+	knot_rdataset_t_cdef = [[
+		typedef struct {
+			uint16_t count;
+			knot_rdata_t *rdata;
+		} knot_rdataset_t;
+	]]
+elseif sover == '9' then
+	knot_rdataset_t_cdef = [[
+		typedef struct {
+			uint16_t count;
+			uint32_t size;
+			knot_rdata_t *rdata;
+		} knot_rdataset_t;
+	]]
+else
+	error('unexpected libknot version: ' .. sover)
+end
+ffi.cdef([[
+		typedef struct {
+			uint16_t len;
+			uint8_t data[];
+		} knot_rdata_t;
+	]] .. knot_rdataset_t_cdef)
+
+
 require('kres-gen')
 
 -- Error code representation

daemon/lua/kres-gen.lua
@@ -23,14 +23,6 @@ typedef struct {
 } knot_rrinfo_t;
 typedef unsigned char knot_dname_t;
 typedef struct {
-	uint16_t len;
-	uint8_t data[];
-} knot_rdata_t;
-typedef struct {
-	uint16_t count;
-	knot_rdata_t *rdata;
-} knot_rdataset_t;
-typedef struct {
 	knot_dname_t *_owner;
 	uint32_t _ttl;
 	uint16_t type;

daemon/lua/kres-gen.sh
@@ -75,8 +75,8 @@ ${CDEFS} ${LIBKRES} types <<-EOF
 	knot_section_t
 	knot_rrinfo_t
 	knot_dname_t
-	knot_rdata_t
-	knot_rdataset_t
+	#knot_rdata_t
+	#knot_rdataset_t
 EOF
 
 genResType() {

commit fb922d48940af358a4e97576d74cd6e66085cfa0

lua bindings: fix a regression from b42195a2
I'm really sorry; I didn't notice and it only hit parts that
*apparently* aren't tested normally.

daemon/lua/kres.lua
@@ -65,14 +65,14 @@ if not sover_pos then
 	error('unexpected libknot soname: ' .. libknot_SONAME)
 end
 local sover = string.sub(libknot_SONAME, sover_pos , sover_pos)
-if sover == '8' then
+if sover == '9' then
 	knot_rdataset_t_cdef = [[
 		typedef struct {
 			uint16_t count;
 			knot_rdata_t *rdata;
 		} knot_rdataset_t;
 	]]
-elseif sover == '9' then
+elseif sover == '1' then -- it's 10 really, but this is simpler
 	knot_rdataset_t_cdef = [[
 		typedef struct {
 			uint16_t count;