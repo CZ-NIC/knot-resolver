969f9f0937ea6da33bab1a92451f758ce11feaff
Lukáš Ježek <lukas.jezek@nic.cz>
2019-11-04 13:17:36 +0100

    WIP: Setup builddeps, rundeps and build on debian 10 as in the debian package for v4.2.2

--- scripts/distros/debian/10/build.sh
+++ scripts/distros/debian/10/build.sh
@@ -1,3 +1,18 @@
-meson build_packaging --default-library=static --prefix=/usr -Dwerror=true -Dextra_tests=enabled -Dsendmmsg=disabled
+CFLAGS="$CFLAGS -Wall -pedantic -fno-omit-frame-pointer"
+LDFLAGS="$LDFLAGS -Wl,--as-needed"
+meson build_packaging \
+	--buildtype=plain \
+	--prefix=/usr \
+	--libdir=lib \
+	-Ddoc=enabled \
+	-Dsystemd_files=enabled \
+	-Dclient=enabled \
+	-Dkeyfile_default=/usr/share/dns/root.key \
+	-Droot_hints=/usr/share/dns/root.hints \
+	-Dinstall_kresd_conf=enabled \
+	-Dunit_tests=enabled \
+	-Dc_args="${CFLAGS}" \
+	-Dc_link_args="${LDFLAGS}"
+
 ninja -C build_packaging
 
