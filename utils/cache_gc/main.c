2080516a0b9bd2e3a13bc512863ab7554246cfb3
Ales Mrazek <ales.mrazek@nic.cz>
2019-12-06 14:40:03 +0100

    sysrepo: gc poll() implementation

--- utils/cache_gc/main.c
+++ utils/cache_gc/main.c
@@ -3,20 +3,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <string.h>
-#include <inttypes.h>
 #include <libyang/libyang.h>
 #include <sysrepo.h>
+#include <poll.h>
 
 #include <lib/defines.h>
 #include <libknot/libknot.h>
 
 #include "kr_cache_gc.h"
+#include "utils/common/sysrepo_conf.h"
+#include "utils/common/string_helper.h"
+
+#define XPATH_GC    XPATH_BASE"/cache/cznic-resolver-knot:garbage-collector"
+
+typedef struct pollfd pollfd_t;
 
 volatile static int killed = 0;
-
-const char *kr_mod_name = "cznic-resolver-common";
-const char *gc_xpath = "/cznic-resolver-common:dns-resolver/cache/cznic-resolver-knot:garbage-collector";
 
 kr_cache_gc_cfg_t cfg = {
 		.rw_txn_items = 100,
@@ -38,30 +40,6 @@ static void got_killed(int signum)
 	default:
 		assert(0);
 	}
-}
-
-char *sub_str_remove(char *str, const char *sub) {
-    char *p, *q, *r;
-    if ((q = r = strstr(str, sub)) != NULL) {
-        size_t len = strlen(sub);
-        while ((r = strstr(p = r + len, sub)) != NULL) {
-            memmove(q, p, r - p);
-            q += r - p;
-        }
-        memmove(q, p, strlen(p) + 1);
-    }
-    return str;
-}
-
-char* concat(const char *s1, const char *s2)
-{
-    const size_t len1 = strlen(s1);
-    const size_t len2 = strlen(s2);
-    char *result = malloc(len1 + len2 + 1); // +1 for the null-terminator
-    // in real code you would check for errors in malloc here
-    memcpy(result, s1, len1);
-    memcpy(result + len1, s2, len2 + 1); // +1 to copy the null-terminator
-    return result;
 }
 
 static void print_help()
@@ -89,132 +67,122 @@ static long get_nonneg_optarg()
 	exit(2);
 }
 
-int set_gc_config(sr_val_t *value){
-    char *option;
-    option = sub_str_remove(value->xpath, gc_xpath);
+int conf_gc(sr_val_t *value){
 
-    if (strcmp(option, "/collecting-interval")==0)        
+    value->xpath = remove_substr(value->xpath, XPATH_GC"/");
+
+    if (!strcmp(value->xpath, "interval")){
         cfg.gc_interval = value->data.uint64_val*1000;
-    else 
-    if (strcmp(option, "/trigger-threshold")==0)     
+    }   
+    else if (!strcmp(value->xpath, "threshold")){
         cfg.cache_max_usage = value->data.uint8_val;
-    else 
-    if (strcmp(option, "/release-percentage")==0)
+    }   
+    else if (!strcmp(value->xpath, "release-percentage")){
         cfg.cache_to_be_freed = value->data.uint8_val;
-    else 
-    if (strcmp(option, "/temporary-keys-space")==0)
+    }
+    else if (!strcmp(value->xpath, "temporary-keys-space")){
         cfg.temp_keys_space = value->data.uint64_val;
-    else 
-    if (strcmp(option, "/rw-items")==0)
+    }
+    else if (!strcmp(value->xpath, "rw-items")){
         cfg.rw_txn_items = value->data.uint64_val;
-    else 
-    if (strcmp(option, "/rw-duration")==0)
+    }
+    else if (!strcmp(value->xpath, "rw-duration")){
         cfg.rw_txn_duration = value->data.uint64_val;
-    else 
-    if (strcmp(option, "/rw-delay")==0)
-        cfg.rw_txn_delay = value->data.uint64_val;
-    else 
-    if (strcmp(option, "/dry-run")==0)
+    }
+    else if (!strcmp(value->xpath, "rw-delay")){
+        cfg.rw_txn_delay = value->data.uint64_val; 
+    }
+    else if (!strcmp(value->xpath, "dry-run")){
         cfg.dry_run = value->data.bool_val;
-    else
-        return 1;
+    }
+    else return 1;
+
     return 0;
 }
 
-int set_current_config(sr_session_ctx_t *session, const char *module_name)
+int conf_gc_set_current(sr_session_ctx_t *session, const char *module_name)
 {
+    size_t count = 0;  
+    int sr_err = SR_ERR_OK;
+    sr_val_t *values = NULL;
 
-	sr_val_t *values = NULL;
-    size_t count = 0;
-    int rc = SR_ERR_OK;
-    char *xpath,*cpath;
-
-    /* get location of the cache */
-    /*
-    sr_get_item(session,"",0,&cpath);
-    cfg.cache_path = cpath;
-    free(cpath);
-    */
-    asprintf(&xpath, "%s/*", gc_xpath);
-
-    rc = sr_get_items(session, xpath, 0, &values, &count);
-    free(xpath);
-
-    if (rc != SR_ERR_OK) {
-        return rc;
-    }
+    sr_err = sr_get_items(session, XPATH_GC"/*//.", 0, &values, &count);
+    if (sr_err != SR_ERR_OK) goto cleanup;
 
     for (size_t i = 0; i < count; i++){
-
         sr_val_t *value = &values[i];
-
-        rc = set_gc_config(value);
-
-        if (rc != SR_ERR_OK) {
-            printf("Error (%s)\n", sr_strerror(rc));      
-        }
+        conf_gc(value);
     }
-    
-    sr_free_values(values, count);
-    return SR_ERR_OK;
+    cleanup:
+        if(sr_err) printf("%s\n",sr_strerror(sr_err));
+        sr_free_values(values, count);
+        return sr_err;
 }
 
-static int gc_change_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, 
+static int gc_conf_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, 
                         sr_event_t event, uint32_t request_id, void *private_data)
 {
-    sr_change_oper_t oper;
+    int sr_err = SR_ERR_OK;
     sr_change_iter_t *it = NULL;
+    sr_change_oper_t oper;
     sr_val_t *old_value = NULL;
     sr_val_t *new_value = NULL;
-    int rc = SR_ERR_OK;
 
     (void)xpath;
     (void)request_id;
     (void)private_data;
 
+    sr_err = sr_get_changes_iter(session, XPATH_GC"//." , &it);    
+    if (sr_err != SR_ERR_OK) goto cleanup;
     
-
-    rc = sr_get_changes_iter(session, "//." , &it);
-    if (rc != SR_ERR_OK) {
-        goto cleanup;    
-    }
-
-    if (event == SR_EV_CHANGE){
-        printf("Sysrepo: Configuration change callback.\n");
-
-        while ((rc = sr_get_change_next(session, it, &oper, &old_value, &new_value)) == SR_ERR_OK) {
-            rc = set_gc_config(new_value);
-            sr_free_val(old_value);
-            sr_free_val(new_value);
-
-            if (rc != SR_ERR_OK) {
-                goto cleanup;    
-            }
-        }
-    }  
-
     if (event == SR_EV_DONE) {
-        printf("Sysrepo: Configuration succesfully changed.\n");
+        while ((sr_get_change_next(session, it, &oper, &old_value, &new_value)) == SR_ERR_OK) {       
+            conf_gc(new_value);
+        }      
     }
+    else if(event == SR_EV_ABORT)
+    {
+        /* code */
+    }
+    
+    cleanup:
+        if(sr_err != (SR_ERR_OK && SR_ERR_NOT_FOUND)) 
+            printf("%s\n",sr_strerror(sr_err));
+        sr_free_val(old_value);
+        sr_free_val(new_value);
+        sr_free_change_iter(it);   
+        return SR_ERR_OK;    
+}
 
-cleanup:
-    sr_free_change_iter(it);
+static int gc_version_request_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, const char *request_xpath, 
+                              uint32_t request_id, struct lyd_node **parent, void *private_data)
+{
+    (void)session;
+    (void)request_xpath;
+    (void)request_id;
+    (void)private_data;
+
+    if (!strcmp(module_name, YM_COMMON) && !strcmp(xpath, XPATH_GC"/version"))
+        //TODO: this cause memory leaks, check it
+        *parent = lyd_new_path(NULL, sr_get_context(sr_session_get_connection(session)), XPATH_GC"/version", KR_CACHE_GC_VERSION, 0, 0);
+
     return SR_ERR_OK;
 }
 
-static int op_data_request_cb(sr_session_ctx_t *session, const char *module_name, const char *xpath, const char *request_xpath, 
-                              uint32_t request_id, struct lyd_node **parent, void *private_data)
-{
-    //(void)session;
-    //(void)request_xpath;
-    //(void)request_id;
-    //(void)private_data;
+ pollfd_t *new_pollfd(sr_subscription_ctx_t *subscription){
 
-    if (!strcmp(module_name, kr_mod_name) && !strcmp(xpath, concat(gc_xpath,"/version"))) {
-        *parent = lyd_new_path(NULL, sr_get_context(sr_session_get_connection(session)), concat(gc_xpath,"/version"), KR_CACHE_GC_VERSION, 0, 0);
+    int fd, sr_err;
+    sr_err = sr_get_event_pipe(subscription, &fd);
+    if (sr_err != SR_ERR_OK) {
+        printf("Error (%s)\n", sr_strerror(sr_err));
+        return NULL;
     }
 
-    return SR_ERR_OK;
+    pollfd_t *sr_poll = malloc(sizeof(sr_poll));
+    sr_poll->fd = fd;
+    sr_poll->events = POLLIN;
+
+    return sr_poll;
 }
 
 int main(int argc, char *argv[])
@@ -226,13 +194,7 @@ int main(int argc, char *argv[])
 	signal(SIGPIPE, got_killed);
 	signal(SIGCHLD, got_killed);
 	signal(SIGINT, got_killed);
-/*
-	kr_cache_gc_cfg_t cfg = {
-		.rw_txn_items = 100,
-		.cache_max_usage = 80,
-		.cache_to_be_freed = 10
-	};
-*/
+
 	int o;
 	while ((o = getopt(argc, argv, "hnc:d:l:m:u:f:w:t:")) != -1) {
 		switch (o) {
@@ -281,76 +243,55 @@ int main(int argc, char *argv[])
 	}
 
 	/* sysrepo client */
+    int sr_err = SR_ERR_OK;
 	sr_conn_ctx_t *sr_connection = NULL;
     sr_session_ctx_t *sr_session = NULL;
     sr_subscription_ctx_t *sr_subscription = NULL;
-    sr_subscription_ctx_t *sr_oper_subscr = NULL;
+    pollfd_t *sr_poll = NULL;
 
-    int rc = SR_ERR_OK;
+    sr_err = sr_connect(0, &sr_connection);
+    if (sr_err != SR_ERR_OK) goto cleanup;
 
-    /* connect to sysrepo */
-    rc = sr_connect(0, &sr_connection);
-    if (rc != SR_ERR_OK) {
-        printf("Error (%s)\n", sr_strerror(rc));
-        goto cleanup;
-    }
+    //sr_err = sr_connection_recover(sr_connection);
+    //if (sr_err != SR_ERR_OK) goto cleanup;
 
-    /* start session */
-    rc = sr_session_start(sr_connection, SR_DS_RUNNING, &sr_session);
-    if (rc != SR_ERR_OK) {
-        printf("Error (%s)\n", sr_strerror(rc));
-        goto cleanup;
-    }
+    sr_err = sr_session_start(sr_connection, SR_DS_RUNNING, &sr_session);
+    if (sr_err != SR_ERR_OK) goto cleanup;
 
-	/* Set current Garbage collector configuration */
-	rc = set_current_config(sr_session, kr_mod_name);
-    if (rc != SR_ERR_OK) {
-        printf("Error (%s)\n", sr_strerror(rc));
-        goto cleanup;
-    }
+    /* read and set current running configuration */
+	sr_err = conf_gc_set_current(sr_session, YM_COMMON);
+    if (sr_err != SR_ERR_OK) goto cleanup;
 
-	/* subscribe for changes in running config */
-    rc = sr_module_change_subscribe(sr_session, kr_mod_name, gc_xpath, gc_change_cb, NULL, 0, SR_SUBSCR_NO_THREAD , &sr_subscription);
-    if (rc != SR_ERR_OK) {
-        printf("Error (%s)\n", sr_strerror(rc));
-        goto cleanup;
-    }
+    /* setup sysrepo subscriptions */
+    sr_err = sr_module_change_subscribe(sr_session, YM_COMMON, XPATH_GC, gc_conf_cb, NULL, 0, SR_SUBSCR_NO_THREAD, &sr_subscription);
+    if (sr_err != SR_ERR_OK) goto cleanup;
+
     /* subscribe for providing the operational data */
-    rc = sr_oper_get_items_subscribe(sr_session, kr_mod_name, concat(gc_xpath,"/version"), op_data_request_cb, NULL, SR_SUBSCR_NO_THREAD, &sr_oper_subscr);
-    if (rc != SR_ERR_OK) {
-        printf("Error (%s)\n", sr_strerror(rc));
-        goto cleanup;
-    }
+    sr_err = sr_oper_get_items_subscribe(sr_session, YM_COMMON, XPATH_GC"/version", gc_version_request_cb, NULL, SR_SUBSCR_NO_THREAD|SR_SUBSCR_CTX_REUSE, &sr_subscription);
+    if (sr_err != SR_ERR_OK) goto cleanup;
+
+    sr_poll = new_pollfd(sr_subscription);
+    if(!sr_poll) goto cleanup;
 
 	do {
-		int ret = kr_cache_gc(&cfg);
+		int gc_ret = kr_cache_gc(&cfg);
 		// ENOENT: kresd may not be started yet or cleared the cache now
-		if (ret && ret != -ENOENT) {
-			printf("Error (%s)\n", knot_strerror(ret));
-			return 10;
+		if (gc_ret && gc_ret != -ENOENT) {
+			printf("Error (%s)\n", knot_strerror(gc_ret));
+			goto cleanup;
 		}
 
-		//usleep(cfg.gc_interval);
-        int sleep_time = 0;
-        do {
-            rc = sr_process_events(sr_subscription, sr_session, NULL);
-            if (rc != SR_ERR_OK) {
-                printf("Error (%s)\n", sr_strerror(rc));
-            }
-            rc = sr_process_events(sr_oper_subscr, sr_session, NULL);
-            if (rc != SR_ERR_OK) {
-                printf("Error (%s)\n", sr_strerror(rc));
-            }
-            if (cfg.gc_interval<100){
-                usleep(100);          
-            }
-            sleep_time += 100;
-        }while( sleep_time < cfg.gc_interval);
+        int poll_ret = poll(sr_poll, (unsigned long)1, (int)cfg.gc_interval/1000);
+        if(poll_ret) 
+            sr_process_events(sr_subscription, sr_session, NULL);
 
 	} while (cfg.gc_interval > 0 && !killed);
 
 	cleanup:
-	sr_disconnect(sr_connection);
+        if (sr_err != SR_ERR_OK) 
+            printf("Error (%s)\n", sr_strerror(sr_err));
+        free(sr_poll);
+	    sr_disconnect(sr_connection);
 
 	return 0;
 }
