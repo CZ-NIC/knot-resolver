15202fe0ebede74a1c89a687604fb79a43cc52f0
Lukáš Ježek <lukas.jezek@nic.cz>
2019-12-18 15:44:54 +0100

    fixup! daemon/lua: add kluautil.lua file for helpers functions

--- daemon/lua/kluautil.lua
+++ daemon/lua/kluautil.lua
@@ -1,12 +1,10 @@
 
--- Module API
-local kluautil = {
-	-- Get length of table
-	tableLen  = function (t)
-		local len = 0
-		for _ in pairs(t) do len = len + 1 end
-		return len
-	end,
-}
+-- Get length of table
+function kr_table_len (t)
+	local len = 0
+	for _ in pairs(t) do
+		len = len + 1
+	end
+	return len
+end
 
-return kluautil
