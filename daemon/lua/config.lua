78248070ed4929a7e4f70132211a47e49424ccc2
Tomas Krizek <tomas.krizek@nic.cz>
2020-01-10 16:45:19 +0100

    daemon/lua/config.lua: bind to control socket by default

--- daemon/lua/config.lua
+++ daemon/lua/config.lua
@@ -1,5 +1,32 @@
+local ffi = require('ffi')
+local C = ffi.C
+
+local function count_sockets()
+	local dns_socks = 0
+	local control_socks = 0
+	for _, socket in ipairs(net.list()) do
+		if socket.kind == 'control' then
+			control_socks = control_socks + 1
+		elseif socket.kind == 'dns' or socket.kind == 'tls' or socket.kind == 'doh' then
+			dns_socks = dns_socks + 1
+		end
+	end
+	return dns_socks, control_socks
+end
+
+local n_dns_socks, n_control_socks = count_sockets()
+
+-- Bind to control socket by default
+if not C.the_args.interactive and n_control_socks == 0 then
+	local path = worker.cwd..'/control/'..worker.pid
+	local ok, err = pcall(net.listen, path, nil, { kind = 'control' })
+	if not ok then
+		error('bind to '..path..' failed '..err)
+	end
+end
+
 -- Listen on localhost
-if not next(net.list()) and not env.KRESD_NO_LISTEN then
+if n_dns_socks == 0 and not env.KRESD_NO_LISTEN then
 	local ok, err = pcall(net.listen, '127.0.0.1')
 	if not ok then
 		error('bind to 127.0.0.1@53 '..err)
@@ -18,7 +45,8 @@ if not next(net.list()) and not env.KRESD_NO_LISTEN then
 		log('bind to ::1@853 '..err)
 	end
 	-- Exit when kresd isn't listening on any interfaces
-	if not next(net.list()) then
+	local n_dns_socks, _ = count_sockets()
+	if n_dns_socks == 0 then
 		panic('not listening on any interface, exiting...')
 	end
 end
@@ -28,6 +56,6 @@ if not cache.current_size then
 end
 
 -- If no addresses for root servers are set, load them from the default file
-if require('ffi').C.kr_zonecut_is_empty(kres.context().root_hints) then
+if C.kr_zonecut_is_empty(kres.context().root_hints) then
 	_hint_root_file()
 end
