00ff7615913d235d5770b8d165bb374f37c44875
Vladimír Čunát <vladimir.cunat@nic.cz>
2020-03-04 13:55:15 +0100

    lib/cache: ignore EINVAL from space pre-allocation

--- lib/cache/cdb_lmdb.c
+++ lib/cache/cdb_lmdb.c
@@ -333,7 +333,12 @@ static int cdb_open(struct lmdb_env *env, const char *path, size_t mapsize,
 	}
 
 	ret = posix_fallocate(fd, 0, mapsize);
-	if (ret != 0) {
+	if (ret == EINVAL) {
+		/* POSIX says this can happen when the feature isn't supported by the FS.
+		 * We haven't seen this happen on Linux+glibc but it was reported on FreeBSD.*/
+		kr_log_info("[cache] space pre-allocation failed and ignored; "
+				"your (file)system probably doesn't support it.\n");
+	} else if (ret != 0) {
 		mdb_txn_abort(txn);
 		stats->close++;
 		mdb_env_close(env->env);
