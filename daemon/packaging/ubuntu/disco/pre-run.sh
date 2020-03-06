2d5083a851ce18ce2118ef608f23c36cbbb282b6
Lukáš Ježek <lukas.jezek@nic.cz>
2020-03-06 12:32:34 +0100

    fixup! tests: pytest packaging tests for Debian 10 and Ubuntu Disco

--- daemon/packaging/ubuntu/disco/pre-run.sh
+++
@@ -1,7 +0,0 @@
-apt-get update
-apt-get install -y wget gnupg apt-utils
-
-wget https://secure.nic.cz/files/knot-resolver/knot-resolver-release.deb
-dpkg -i knot-resolver-release.deb
-
-apt-get update
