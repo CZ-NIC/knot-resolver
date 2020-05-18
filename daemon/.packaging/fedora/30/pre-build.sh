3468496a679635ec44438a8283d64254f5608d53
Tomas Krizek <tomas.krizek@nic.cz>
2020-05-18 13:41:13 +0200

    tests/packaging: add f32, remove f30

--- daemon/.packaging/fedora/30/pre-build.sh
+++
@@ -1,7 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-dnf install -y wget 'dnf-command(config-manager)'
-
-dnf config-manager --add-repo https://download.opensuse.org/repositories/home:CZ-NIC:knot-resolver-build/Fedora_30/home:CZ-NIC:knot-resolver-build.repo
-dnf install -y knot
-dnf upgrade -y
--- daemon/.packaging/fedora/30/pre-run.sh
+++
@@ -1,6 +0,0 @@
-# SPDX-License-Identifier: GPL-3.0-or-later
-
-dnf install -y wget 'dnf-command(config-manager)'
-
-dnf config-manager --add-repo https://download.opensuse.org/repositories/home:CZ-NIC:knot-resolver-latest/Fedora_30/home:CZ-NIC:knot-resolver-latest.repo
-dnf upgrade -y
