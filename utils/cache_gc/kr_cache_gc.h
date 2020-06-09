73d363bd23f1a79d4c8008b67eed46631ea97712
Vladimír Čunát <vladimir.cunat@nic.cz>
2020-06-09 08:09:32 +0200

    gc: fix integer overflow when computing how much to GC
    On 32-bit systems the insufficient GC could commonly happen:
    https://lists.nic.cz/pipermail/knot-resolver-users/2020/000265.html
    
    The meaning of -f parameter got slightly changed, so that the buggy
    computation could be greatly simplified.  GC seems to make sense when
    most of cache space is used, in which case the difference is small.

--- utils/cache_gc/kr_cache_gc.h
+++ utils/cache_gc/kr_cache_gc.h
@@ -25,7 +25,7 @@ typedef struct {
 	unsigned long rw_txn_delay;	// waiting time between two RW transactions in usecs
 
 	uint8_t cache_max_usage;	// maximum cache usage before triggering GC (percent)
-	uint8_t cache_to_be_freed;	// percent of cache to be freed during GC
+	uint8_t cache_to_be_freed;	// percent of current cache usage to be freed during GC
 
 	bool dry_run;
 } kr_cache_gc_cfg_t;
