CZ-NIC/knot-resolver

commit e460f9215660822813e868de5859e51111e70201

iterate: use resolve_badmsg() fallback for more RCODEs
+ REFUSED and SERVFAIL (in non-STUB)

lib/layer/iterate.c
@@ -1069,14 +1069,12 @@ static int resolve(kr_layer_t *ctx, knot_pkt_t *pkt)
 	case KNOT_RCODE_NXDOMAIN:
 		break; /* OK */
 	case KNOT_RCODE_REFUSED:
-	case KNOT_RCODE_SERVFAIL: {
+	case KNOT_RCODE_SERVFAIL:
 		if (query->flags.STUB) {
-			 /* Pass through in stub mode */
+			 /* just pass answer through if in stub mode */
 			break;
 		}
-		VERBOSE_MSG("<= rcode: %s\n", rcode ? rcode->name : "??");
-		return KR_STATE_FAIL;
-	}
+		/* fall through */
 	case KNOT_RCODE_FORMERR:
 		VERBOSE_MSG("<= rcode: %s\n", rcode ? rcode->name : "??");
 		return resolve_badmsg(pkt, req, query);

commit 7f0a4ed0bbbce8d4cd83e3d5f120441419366cee

iterate resolve_badmsg(): fix retries when forwarding
This function just caused immediate SERVFAIL in FORWARD mode.
If we get a bad message (various RCODEs, failed parse, etc.),
we surely want to try other IP addresses even in forwarding modes.
For SERVFAIL and REFUSED this regressed in e460f9215 (since v5.1.0);
for others it probably didn't work that way until now.
Behavior for these two RCODEs in STUB mode remains an exception for now,
as it's been there since forever (aecffe74e).

lib/layer/iterate.c
@@ -972,7 +972,10 @@ static int resolve_badmsg(knot_pkt_t *pkt, struct kr_request *req, struct kr_que
 
 #ifndef STRICT_MODE
 	/* Work around broken auths/load balancers */
-	if (query->flags.SAFEMODE) {
+	/* In case we get called in FORWARD or STUB mode,
+	 * let's return _FAIL causing retries with other IP addresses.
+	 * Otherwise we would cause immediate SERVFAIL. */
+	if (query->flags.SAFEMODE || query->flags.FORWARD || query->flags.STUB) {
 		return resolve_error(pkt, req);
 	} else if (query->flags.NO_MINIMIZE) {
 		query->flags.SAFEMODE = true;
@@ -1097,6 +1100,7 @@ static int resolve(kr_layer_t *ctx, knot_pkt_t *pkt)
 	case KNOT_RCODE_SERVFAIL:
 		if (query->flags.STUB) {
 			 /* just pass answer through if in stub mode */
+			 /* TODO: reconsider this "inconsistency" (e.g. with other RCODEs) */
 			break;
 		}
 		/* fall through */