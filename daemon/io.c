09d9ed4bf60bee93806f9cbd73f540e2bb285546
Štěpán Balážik <stepan.balazik@nic.cz>
2020-08-18 11:41:07 +0200

    io print

--- daemon/io.c
+++ daemon/io.c
@@ -61,6 +61,7 @@ void udp_recv(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf,
 	const struct sockaddr *addr, unsigned flags)
 {
 	struct session *s = handle->data;
+	printf("msg_id %02x%02x, nread %d\n", buf->base[0] & 0xff, buf->base[1] & 0xff, nread);
 	if (session_flags(s)->closing) {
 		return;
 	}
