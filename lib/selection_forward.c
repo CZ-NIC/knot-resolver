4e5eb6b60b179f46da8c86ed6f2d59251dd1f226
Štěpán Balážik <stepan.balazik@nic.cz>
2020-08-29 14:30:06 +0200

    selection: exponential back off after timeout

--- lib/selection_forward.c
+++ lib/selection_forward.c
@@ -70,7 +70,7 @@ void forward_choose_transport(struct kr_query *qry, struct kr_transport **transp
         };
     }
 
-    *transport = choose_transport(choices, valid, NULL, 0, &qry->request->pool, qry->flags.TCP, &local_state->last_choice_index);
+    *transport = choose_transport(choices, valid, NULL, 0, qry->server_selection.timeouts, &qry->request->pool, qry->flags.TCP, &local_state->last_choice_index);
 }
 
 void forward_success(struct kr_query *qry, const struct kr_transport *transport) {
