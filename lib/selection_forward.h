73a8f9b42f251e74ca1ff24189e9cf1235bff2b4
Štěpán Balážik <stepan.balazik@nic.cz>
2020-09-01 09:58:43 +0200

    selection: rename *_state_init to *_state_alloc

--- lib/selection_forward.h
+++ lib/selection_forward.h
@@ -7,7 +7,7 @@
 #include "lib/selection.h"
 #include "lib/resolve.h"
 
-void forward_local_state_init(struct knot_mm *mm, void **local_state, struct kr_request *req);
+void forward_local_state_alloc(struct knot_mm *mm, void **local_state, struct kr_request *req);
 void forward_choose_transport(struct kr_query *qry, struct kr_transport **transport);
 void forward_success(struct kr_query *qry, const struct kr_transport *transport);
 void forward_error(struct kr_query *qry, const struct kr_transport *transport, enum kr_selection_error sel_error);
