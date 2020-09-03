df41e8b3ab9fa42947bc2e93023d8df0cb5aab6c
Štěpán Balážik <stepan.balazik@nic.cz>
2020-09-03 16:36:56 +0200

    WIP: timeout tweaks
    Set retries to 10 and static forwarding timeout to 2000 ms.

--- lib/selection_forward.c
+++ lib/selection_forward.c
@@ -75,6 +75,10 @@ void forward_choose_transport(struct kr_query *qry, struct kr_transport **transp
 	}
 
 	*transport = choose_transport(choices, valid, NULL, 0, qry->server_selection.timeouts, &qry->request->pool, qry->flags.TCP, &local_state->last_choice_index);
+	if (*transport) {
+		// Set static timeout for forwarding
+		(*transport)->timeout = 2000;
+	}
 }
 
 void forward_success(struct kr_query *qry, const struct kr_transport *transport) {
