605a9af8dc35dd9946acc9608cd1083282173cfd
Tomas Krizek <tomas.krizek@nic.cz>
2020-09-30 14:10:23 +0200

    daemon: make nghttp2 optional

--- daemon/io.c
+++ daemon/io.c
@@ -306,6 +306,7 @@ static void tcp_recv(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
 		data = session_wirebuf_get_free_start(s);
 		data_len = consumed;
 	}
+#ifdef ENABLE_DOH2
 	if (session_flags(s)->has_http) {
 		consumed = http_process_input_data(s, data, data_len);
 		if (consumed < 0) {
@@ -324,6 +325,7 @@ static void tcp_recv(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
 		data = session_wirebuf_get_free_start(s);
 		data_len = consumed;
 	}
+#endif
 
 	/* data points to start of the free space in session wire buffer.
 	   Simple increase internal counter. */
@@ -339,6 +341,7 @@ static void tcp_recv(uv_stream_t *handle, ssize_t nread, const uv_buf_t *buf)
 	mp_flush(the_worker->pkt_pool.ctx);
 }
 
+#ifdef ENABLE_DOH2
 static ssize_t tls_send(const uint8_t *buf, const size_t len, struct session *session)
 {
 	struct tls_ctx *ctx = session_tls_get_server_ctx(session);
@@ -353,6 +356,7 @@ static ssize_t tls_send(const uint8_t *buf, const size_t len, struct session *se
 	}
 	return sent;
 }
+#endif
 
 static void _tcp_accept(uv_stream_t *master, int status, bool tls, bool http)
 {
@@ -452,6 +456,7 @@ static void _tcp_accept(uv_stream_t *master, int status, bool tls, bool http)
 			session_tls_set_server_ctx(s, ctx);
 		}
 	}
+#ifdef ENABLE_DOH2
 	if (http) {
 		struct http_ctx *ctx = session_http_get_server_ctx(s);
 		if (!ctx) {
@@ -467,6 +472,7 @@ static void _tcp_accept(uv_stream_t *master, int status, bool tls, bool http)
 			session_http_set_server_ctx(s, ctx);
 		}
 	}
+#endif
 	session_timer_start(s, tcp_timeout_trigger, timeout, idle_in_timeout);
 	io_start_read((uv_handle_t *)client);
 }
@@ -481,16 +487,18 @@ static void tls_accept(uv_stream_t *master, int status)
 	_tcp_accept(master, status, true, false);
 }
 
+#ifdef ENABLE_DOH2
 static void https_accept(uv_stream_t *master, int status)
 {
 	_tcp_accept(master, status, true, true);
 }
+#endif
 
 int io_listen_tcp(uv_loop_t *loop, uv_tcp_t *handle, int fd, int tcp_backlog, bool has_tls, bool has_http)
 {
 	uv_connection_cb connection;
 	if (has_tls && has_http) {
-#ifdef NGHTTP2_VERSION_NUM
+#ifdef ENABLE_DOH2
 		connection = https_accept;
 #else
 		kr_log_error("[ io ] kresd was compiled without libnghttp2 support");
