d3dfe46fe0206f1534fdf4d01d153d5f8b62b863
Simon South <simon@simonsouth.net>
2020-10-14 17:57:18 -0400

    Generate Info manual
    Simon South <simon@simonsouth.net> writes:
    > Here's a patch...
    
    So of course that doesn't actually do the right thing.
    
    Here's a patch that does, and also avoids GNU-isms so it should be
    portable everywhere:

--- scripts/make-doc.sh
+++ scripts/make-doc.sh
@@ -20,6 +20,22 @@ if command -v makeinfo &>/dev/null; then
     rm -rf doc/texinfo
     ${SPHINX} ${@} -b texinfo -d doc/.doctrees doc doc/texinfo && \
         make -C doc/texinfo info
+
+    # Sphinx < 2 doesn't create a separate directory for figures, so if
+    # necessary move them to the correct location and update the references in
+    # the generated Info file
+    if [ ! -d doc/texinfo/knot-resolver-figures ]; then
+        cd doc/texinfo
+        mkdir knot-resolver-figures
+        mv *.png *.svg knot-resolver-figures/
+        sed -e 's/\(\[image .*src="\)/\1knot-resolver-figures\//' \
+            knot-resolver.info > knot-resolver.info.tmp
+        mv knot-resolver.info.tmp knot-resolver.info
+        cd ../..
+    fi
+
     mkdir doc/texinfo/.install
-    mv doc/texinfo/knot-resolver.info doc/texinfo/.install/
+    mv doc/texinfo/knot-resolver.info \
+       doc/texinfo/knot-resolver-figures \
+       doc/texinfo/.install/
 fi
