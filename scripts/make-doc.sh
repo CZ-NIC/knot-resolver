b46079037c0a89cd2103b4841abc4b1eea38c7c5
Simon South <simon@simonsouth.net>
2020-10-15 07:13:20 -0400

    doc/info: create and install figures

--- scripts/make-doc.sh
+++ scripts/make-doc.sh
@@ -18,8 +18,25 @@ ${SPHINX} ${@} -b html -d doc/.doctrees doc doc/html
 
 if command -v makeinfo &>/dev/null; then
     rm -rf doc/texinfo
-    ${SPHINX} ${@} -b texinfo -d doc/.doctrees doc doc/texinfo && \
-        make -C doc/texinfo info
+    ${SPHINX} ${@} -b texinfo -d doc/.doctrees doc doc/texinfo
+
+    # Sphinx < 2 doesn't create a separate directory for figures, so if
+    # necessary move them to the correct location and update the references in
+    # the generated Texinfo file
+    if [ ! -d doc/texinfo/knot-resolver-figures ]; then
+        cd doc/texinfo
+        mkdir knot-resolver-figures
+        mv *.png *.svg knot-resolver-figures/
+        sed -e 's/\(@image{\)/\1knot-resolver-figures\//' \
+            knot-resolver.texi > knot-resolver.texi.tmp
+        mv knot-resolver.texi.tmp knot-resolver.texi
+        cd ../..
+    fi
+
+    make -C doc/texinfo info
+
     mkdir doc/texinfo/.install
-    mv doc/texinfo/knot-resolver.info doc/texinfo/.install/
+    mv doc/texinfo/knot-resolver.info \
+       doc/texinfo/knot-resolver-figures \
+       doc/texinfo/.install/
 fi
