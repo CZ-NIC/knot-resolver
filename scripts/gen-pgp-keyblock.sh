6c81de9bae32cc03cadd4a3e26c6a39bea0dfd0a
Tomas Krizek <tomas.krizek@nic.cz>
2020-11-18 13:38:44 +0100

    fixup! scrips: generate kresd PGP keyblock

--- scripts/gen-pgp-keyblock.sh
+++ scripts/gen-pgp-keyblock.sh
@@ -7,22 +7,24 @@ keys=(
     '4A8BA48C2AED933BD495C509A1FBA5F7EF8C4869'  # tomas.krizek@nic.cz
     '457996D019A7B7C6C2F6862A16CFB506F21D834A'  # lukas.jezek@nic.cz
 )
-url="https://keys.openpgp.org/vks/v1/by-fingerprint/"
 outfile="kresd-keyblock.asc"
+url="https://secure.nic.cz/files/knot-resolver/kresd-keyblock.asc"
 
-tmpdir=$(mktemp -d)
-keyring="${tmpdir}/.gnupg"
+# obtain keys from keys.openpgp.org
+keyring="$(mktemp -d)"
+gpg --homedir "${keyring}" -q --keyserver keys.openpgp.org --recv-keys "${keys[@]}"
 
-for key in "${keys[@]}"; do
-    curl -sfo "${tmpdir}/${key}" "${url}${key}"
-    GNUPGHOME=${keyring} gpg -q --import "${tmpdir}/${key}"
-done
-
+# export minimal size keys with just the necessary signatures
 rm -f "${outfile}"
-GNUPGHOME=${keyring} gpg -q --export --export-options export-minimal --armor --output "${outfile}"
+gpg --homedir "${keyring}" -q --export --export-options export-minimal --armor --output "${outfile}" "${keys[@]}"
 
 # display keys after import
-keyring2=$(mktemp -d)
-GNUPGHOME=${keyring2} gpg -q --import "${outfile}"
-GNUPGHOME=${keyring2} gpg -k
+keyring_import="$(mktemp -d)"
+gpg --homedir "${keyring_import}" -q --import "${outfile}"
+gpg --homedir "${keyring_import}" -k
 echo "Created: ${outfile}"
+
+# check if update of secure.nic.cz keyblock might be needed
+published="$(mktemp)"
+curl -sfo "${published}" "${url}"
+diff -q "${outfile}" "${published}" &>/dev/null || echo "Generated keyblock differs from ${url}"
