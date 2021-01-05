0679754a2185c56749ede27ca534ccf18e9ed87a
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-01-05 19:31:46 +0100

    fixup! tests/dnstap: fix, refactor and integrate into meson

--- tests/dnstap/src/dnstap-test/run.sh
+++ tests/dnstap/src/dnstap-test/run.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 export GOPATH=$(realpath tests/dnstap)
 DNSTAP_TEST=dnstap-test
 DNSTAP_PATH=$(realpath ../tests/dnstap/src/$DNSTAP_TEST)
@@ -7,7 +7,7 @@ CMD=$1
 ZONES="fake1.localdomain,fake2.localdomain,fake3.localdomain"
 TIMEOUT=60s
 
-type go || exit 77
+type -P go || exit 77
 # some packages may be missing on the system right now
 go get github.com/{FiloSottile/gvt,cloudflare/dns,dnstap/golang-dnstap}
 (cd tests/dnstap/src && rm -f $DNSTAP_TEST && ln -s $DNSTAP_PATH .)
