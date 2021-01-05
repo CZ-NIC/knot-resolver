a0ae1f309299c06ee058453b0b63022e7d2051f6
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-01-05 16:59:48 +0100

    tests/dnstap: fix, refactor and integrate into meson
    They need one go package which I can't find in Debian,
    so it probably won't work without network access.
    They now run if dnstap is built and go is found.

---
+++ tests/dnstap/src/dnstap-test/run.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+KRESD_CMD=$1
+MESON_BUILD_ROOT=$(pwd)
+export GOPATH=$MESON_BUILD_ROOT/tests/dnstap
+cd "$(dirname $0)"
+DNSTAP_TEST=dnstap-test
+
+type -P go >/dev/null || exit 77
+echo "Building the dnstap tests and its dependencies..."
+# some packages may be missing on the system right now
+go get github.com/{FiloSottile/gvt,cloudflare/dns,dnstap/golang-dnstap}
+DTAP=$GOPATH/src/$DNSTAP_TEST
+rm -f $DTAP && ln -s $(realpath ..)/$DNSTAP_TEST $DTAP
+go install $DNSTAP_TEST
+
+
+CONFIG=./config
+ZONES="fake1.localdomain,fake2.localdomain,fake3.localdomain"
+TIMEOUT=60s
+GRACE=5s
+$GOPATH/bin/$DNSTAP_TEST -c $CONFIG -cmd $KRESD_CMD -q $ZONES -t $TIMEOUT -g $GRACE -d
+
