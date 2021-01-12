de211f38cf9a43fc0ed446e787e2816706e87d3f
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-01-12 10:13:05 +0100

    fixup! tests/dnstap: fix, refactor and integrate into meson

--- tests/dnstap/src/dnstap-test/run.sh
+++ tests/dnstap/src/dnstap-test/run.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
-export GOPATH=$(realpath tests/dnstap)
+set -e
+KRESD_CMD=$1
+MESON_BUILD_ROOT=$(pwd)
+export GOPATH=$MESON_BUILD_ROOT/tests/dnstap
+cd "$(dirname $0)"
 DNSTAP_TEST=dnstap-test
-DNSTAP_PATH=$(realpath ../tests/dnstap/src/$DNSTAP_TEST)
-CONFIG=$DNSTAP_PATH/config
-CMD=$1
-ZONES="fake1.localdomain,fake2.localdomain,fake3.localdomain"
-TIMEOUT=60s
 
-type -P go || exit 77
+type -P go >/dev/null || exit 77
+echo "Building the dnstap tests and its dependencies..."
 # some packages may be missing on the system right now
 go get github.com/{FiloSottile/gvt,cloudflare/dns,dnstap/golang-dnstap}
-(cd tests/dnstap/src && rm -f $DNSTAP_TEST && ln -s $DNSTAP_PATH $DNSTAP_TEST)
+DTAP=$GOPATH/src/$DNSTAP_TEST
+rm -f $DTAP && ln -s $(realpath ..)/$DNSTAP_TEST $DTAP
 go install $DNSTAP_TEST
-(cd tests/dnstap && $GOPATH/bin/$DNSTAP_TEST -c $CONFIG -cmd $CMD -q $ZONES -t $TIMEOUT -d)
+
+
+CONFIG=./config
+ZONES="fake1.localdomain,fake2.localdomain,fake3.localdomain"
+TIMEOUT=60s
+$GOPATH/bin/$DNSTAP_TEST -c $CONFIG -cmd $KRESD_CMD -q $ZONES -t $TIMEOUT -d
 
