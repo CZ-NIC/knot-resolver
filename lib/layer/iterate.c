3367c2779d3d46a428700846cd25498dad241f09
Štěpán Balážik <stepan.balazik@nic.cz>
2021-01-14 16:10:03 +0100

    fixup! fixup! fixup! iterate: remove SAFEMODE

--- lib/layer/iterate.c
+++ lib/layer/iterate.c
@@ -1064,10 +1064,12 @@ static int resolve(kr_layer_t *ctx, knot_pkt_t *pkt)
 		ret = 0;
 		break; /* OK */
 	case KNOT_RCODE_YXDOMAIN: /* Basically a successful answer; name just doesn't fit. */
-		if (!kr_request_ensure_answer(req))
+		if (!kr_request_ensure_answer(req)) {
 			ret = req->state;
+		} else {
+			ret = 0;
+		}
 		knot_wire_set_rcode(req->answer->wire, KNOT_RCODE_YXDOMAIN);
-		ret = 0;
 		break;
 	case KNOT_RCODE_REFUSED:
 		if (query->flags.STUB) {
