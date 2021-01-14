9f050a5dd663b98f854103f4385a1fe4dab6c2b0
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-01-14 10:37:32 +0100

    fixup! tests/dnstap: fix, refactor and integrate into meson

--- tests/dnstap/src/dnstap-test/run.sh
+++ tests/dnstap/src/dnstap-test/run.sh
@@ -12,16 +12,19 @@ if [ -z "$GITLAB_CI" ]; then
 	# some packages may be missing on the system right now
 	go get github.com/{FiloSottile/gvt,cloudflare/dns,dnstap/golang-dnstap}
 else
-	export GOPATH=/root/go #default; we don't care in scratch container
+	# In CI we've prebuilt dependencies into the default GOPATH.
+	# We're in a scratch container, so we just add the dnstap test inside.
+	export GOPATH=/root/go
 fi
 DTAP=$GOPATH/src/$DNSTAP_TEST
 rm -f $DTAP && ln -s $(realpath ..)/$DNSTAP_TEST $DTAP
 go install $DNSTAP_TEST
 
 
-CONFIG=./config
+CONFIG=$(realpath ./config)
 ZONES="fake1.localdomain,fake2.localdomain,fake3.localdomain"
 TIMEOUT=60s
 GRACE=5s
+cd $MESON_BUILD_ROOT/tests/dnstap # don't leave stuff like *.mdb in ./.
 $GOPATH/bin/$DNSTAP_TEST -c $CONFIG -cmd $KRESD_CMD -q $ZONES -t $TIMEOUT -g $GRACE -d
 
