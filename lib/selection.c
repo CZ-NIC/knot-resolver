7b6486fc3e968d2fffce5c26eb5196817fb63e86
Štěpán Balážik <stepan.balazik@nic.cz>
2021-01-19 13:29:21 +0100

    fixup! iterate: rework error handling from iterate.c

--- lib/selection.c
+++ lib/selection.c
@@ -539,6 +539,11 @@ void error(struct kr_query *qry, struct address_state *addr_state,
 		}
 		break;
 	case KR_SELECTION_REFUSED:
+		if (qry->flags.NO_MINIMIZE) {
+			addr_state->broken = true;
+		} else {
+			qry->flags.NO_MINIMIZE = true;
+		}
 	case KR_SELECTION_SERVFAIL:
 	case KR_SELECTION_NOTIMPL:
 	case KR_SELECTION_OTHER_RCODE:
