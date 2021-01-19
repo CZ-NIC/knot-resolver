8db64ae2b508de97e702fdc1a00afb047146c37c
Štěpán Balážik <stepan.balazik@nic.cz>
2021-01-19 13:39:04 +0100

    iterate.c: don't copy NO_MINIMIZE when following a CNAME
    Instead copy it from the request's options.
    
    Reasoning: Minimization might have been turned off as a workaround for
    broken authoritative servers which doesn't support it. There is no
    reason to drop minimization when switching zones when following a CNAME.

--- lib/layer/iterate.c
+++ lib/layer/iterate.c
@@ -779,8 +779,10 @@ static int process_answer(knot_pkt_t *pkt, struct kr_request *req)
 		/* Copy transitive flags from original query to CNAME followup. */
 		next->flags.TRACE = query->flags.TRACE;
 		next->flags.ALWAYS_CUT = query->flags.ALWAYS_CUT;
-		next->flags.NO_MINIMIZE = query->flags.NO_MINIMIZE;
 		next->flags.NO_THROTTLE = query->flags.NO_THROTTLE;
+
+		/* Original query might have turned minimization off, revert. */
+		next->flags.NO_MINIMIZE = req->options.NO_MINIMIZE;
 
 		if (query->flags.FORWARD) {
 			next->forward_flags.CNAME = true;
