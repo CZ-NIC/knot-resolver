2c27bf6560f0292f45514eb60a5d7a06d5a60267
Aleš <ales.mrazek@nic.cz>
2021-03-23 21:11:05 +0100

    conf: render Lua config using jinja2

--- manager/knot_resolver_manager/configuration.py
+++ manager/knot_resolver_manager/configuration.py
@@ -1,11 +1,23 @@
+from typing import Text
+from jinja2 import Environment
+
 from .datamodel import ConfData
 
+_LUA_TEMPLATE_STR = """
+{% if lua_config -%}
+{{ config.lua_config }}
+{% endif -%}
+"""
 
-class ConfigValidationException(Exception):
-    pass
+_ENV = Environment(enable_async=True)
+_LUA_TEMPLATE = _ENV.from_string(_LUA_TEMPLATE_STR)
 
 
-async def parse(yaml: str) -> ConfData:
-    conf = ConfData.from_yaml(yaml)
-    await conf.validate()
-    return conf
+async def render_lua(config: ConfData) -> Text:
+    return await _LUA_TEMPLATE.render_async(config=config)
+
+
+async def parse_yaml(yaml: str) -> ConfData:
+    config = ConfData.from_yaml(yaml)
+    await config.validate()
+    return config
