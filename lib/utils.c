dc255007bb3810fef3ce96812d0c43463a71eabb
Tomas Krizek <tomas.krizek@nic.cz>
2021-03-25 15:59:19 +0100

    daemon: allow kr_assume() configuration
    - add lua options debugging.assumption_abort and
      debugging.assumption_fork to make the behaviour configurable
    - select default values based on meson buildtype:
      - debug/debugoptimized (default, e.g. used in CI) - abort and don't
        fork
      - plain/release - don't abort, but fork if abort is user-configured

--- lib/utils.c
+++ lib/utils.c
@@ -38,15 +38,18 @@
 
 /* Logging & debugging */
 bool kr_verbose_status = false;
-bool kr_debug_assumption = true;
+bool kr_dbg_assumption_abort = DBG_ASSUMPTION_ABORT;
+bool kr_dbg_assumption_fork = DBG_ASSUMPTION_FORK;
 
 void kr_fail(bool is_fatal, const char *expr, const char *func, const char *file, int line)
 {
 	kr_log_error("%s \"%s\" failed in %s@%s:%d\n",
 			(is_fatal ? "requirement" : "assumption"),
 			expr, func, file, line);
-	if (is_fatal || kr_debug_assumption)
+	if (is_fatal || (kr_dbg_assumption_abort && !kr_dbg_assumption_fork))
 		abort();
+	else if (kr_dbg_assumption_abort && kr_dbg_assumption_fork)
+		fork() == 0 ? abort() : (void)0;
 }
 
 /*
