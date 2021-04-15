2d7d2daf383295f99fc07b545e661fe726b594a6
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-04-15 14:13:11 +0200

    fixup! ci: check for "assert" in most C files

--- ci/no_assert_check.sh
+++ ci/no_assert_check.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-grep '\<assert\>' $(git ls-files | grep '\.[hc]$' | grep -vE '^(contrib|bench|tests)/')
+grep '\<assert\>' -- $(git ls-files | grep '\.[hc]$' | grep -vE '^(contrib|bench|tests)/')
 test $? -eq 1
