20ae53013c708a3fe69917190af9fb2a211ae31c
Aleš <ales.mrazek@nic.cz>
2021-04-15 16:47:58 +0200

    config: server: private attribute for the number of instances The value of the attribute is determined indirectly from loaded configuration or based on the number of system's CPUs.

--- manager/knot_resolver_manager/datamodel/server_config.py
+++ manager/knot_resolver_manager/datamodel/server_config.py
@@ -12,21 +12,24 @@ from .errors import DataValidationError
 @dataclass
 class ServerConfig(DataclassParserValidatorMixin):
     hostname: Optional[str] = None
-    instances: Union[Literal["auto"], int] = 1
-    instances_num: int = 1
+    instances: Union[Literal["auto"], int, None] = None
+    _instances: int = 1
     use_cache_gc: bool = True
 
     def __post_init__(self):
         if isinstance(self.instances, int):
-            self.instances_num = self.instances
+            self._instances = self.instances
         elif self.instances == "auto":
             cpu_count = os.cpu_count()
             if cpu_count is not None:
-                self.instances_num = cpu_count
+                self._instances = cpu_count
             else:
                 # TODO: do better logging
-                print("cannot get number of CPUs")
+                print("cannot find number of system available CPUs")
+
+    def get_instances(self) -> int:
+        return self._instances
 
     def _validate(self):
-        if not 0 < self.instances_num <= 256:
+        if not 0 < self._instances <= 256:
             raise DataValidationError("number of kresd instances must be in range 1..256")
