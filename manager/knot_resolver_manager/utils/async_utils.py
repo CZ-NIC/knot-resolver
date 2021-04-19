6d65f816c89500f47eed9712a98142ce75835892
Vasek Sraier <git@vakabus.cz>
2021-04-19 16:03:28 +0200

    manager: supervisord backend: initial untested support

---
+++ manager/knot_resolver_manager/utils/async_utils.py
@@ -0,0 +1,70 @@
+import os
+import time
+from asyncio import create_subprocess_exec, create_subprocess_shell
+from pathlib import PurePath
+from typing import List, Union
+
+from knot_resolver_manager.compat.asyncio import to_thread
+
+
+async def call(cmd: Union[str, bytes, List[str], List[bytes]], shell: bool = False) -> int:
+    """
+    custom async alternative to subprocess.call()
+    """
+    if shell:
+        if isinstance(cmd, list):
+            raise RuntimeError("can't use list of arguments with shell=True")
+        proc = await create_subprocess_shell(cmd)
+    else:
+        if not isinstance(cmd, list):
+            raise RuntimeError(
+                "Please use list of arguments, not a single string. It will prevent ambiguity when parsing"
+            )
+        proc = await create_subprocess_exec(*cmd)
+
+    return await proc.wait()
+
+
+async def readfile(path: Union[str, PurePath]) -> str:
+    """
+    asynchronously read whole file and return its content
+    """
+
+    def readfile_sync(path: Union[str, PurePath]):
+        with open(path, "r") as f:
+            return f.read()
+
+    return await to_thread(readfile_sync, path)
+
+
+async def writefile(path: Union[str, PurePath], content: str):
+    """
+    asynchronously set content of a file to a given string `content`.
+    """
+
+    def writefile_sync(path: Union[str, PurePath], content: str):
+        with open(path, "w") as f:
+            return f.write(content)
+
+    await to_thread(writefile_sync, path, content)
+
+
+async def wait_for_process_termination(pid: int, sleep_sec: float = 0):
+    """
+    will wait for any process (does not have to be a child process) given by its PID to terminate
+
+    sleep_sec configures the granularity, with which we should return
+    """
+
+    def wait_sync(pid: int, sleep_sec: float):
+        while True:
+            try:
+                os.kill(pid, 0)
+                if sleep_sec == 0:
+                    os.sched_yield()
+                else:
+                    time.sleep(sleep_sec)
+            except ProcessLookupError:
+                break
+
+    await to_thread(wait_sync, pid, sleep_sec)
