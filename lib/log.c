c9573e248ddb88da6bddaf649eac6e967e3b6cef
Lukáš Ježek <lukas.jezek@nic.cz>
2021-05-04 10:39:33 +0200

    log: remove kr_verbose_set

--- lib/log.c
+++ lib/log.c
@@ -3,6 +3,7 @@
  */
 
 #include <stdio.h>
+#include <gnutls/gnutls.h>
 #include "lib/log.h"
 
 log_level_t kr_log_level = LOG_CRIT;
@@ -14,6 +15,34 @@ void kr_log_fmt(log_level_t level, const char *fmt, ...)
 	if (level <= kr_log_level)
 		vfprintf(stdout, fmt, args);
 	va_end(args);
+}
+
+static void kres_gnutls_log(int level, const char *message)
+{
+	kr_log_debug("[gnutls] (%d) %s", level, message);
+}
+
+int kr_log_level_set(log_level_t level)
+{
+	if (level < LOG_CRIT || level > LOG_DEBUG)
+		return kr_log_level;
+
+	kr_log_level = level;
+
+	/* gnutls logs messages related to our TLS and also libdnssec,
+	 * and the logging is set up in a global way only */
+	if (kr_log_level >= LOG_DEBUG) {
+		gnutls_global_set_log_function(kres_gnutls_log);
+	}
+	gnutls_global_set_log_level(level);
+
+	return kr_log_level;
+
+}
+
+log_level_t kr_log_level_get(void)
+{
+	return kr_log_level;
 }
 
 void kr_log_init(log_level_t level)
