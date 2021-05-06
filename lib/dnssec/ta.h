a0482d0dffc4fdae6eb94e64da7a400081787011
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-05-06 15:31:11 +0200

    lib/dnssec/ta: remove unused kr_ta_covers() function
    It was word-for-word identical with kr_ta_get_longest_name() anyway,
    except for additional pointer->bool conversion.

--- lib/dnssec/ta.h
+++ lib/dnssec/ta.h
@@ -31,16 +31,6 @@ KR_EXPORT
 int kr_ta_add(map_t *trust_anchors, const knot_dname_t *name, uint16_t type,
                uint32_t ttl, const uint8_t *rdata, uint16_t rdlen);
 
-/**
- * Return true if the name is below/at any TA in the store.
- * This can be useful to check if it's possible to validate a name beforehand.
- * @param  trust_anchors trust store
- * @param  name          name of the TA
- * @return boolean
- */
-KR_EXPORT KR_PURE
-int kr_ta_covers(map_t *trust_anchors, const knot_dname_t *name);
-
 struct kr_context;
 
 /**
@@ -90,3 +80,4 @@ void kr_ta_clear(map_t *trust_anchors);
  */
 KR_EXPORT
 const knot_dname_t *kr_ta_get_longest_name(map_t *trust_anchors, const knot_dname_t *name);
+
