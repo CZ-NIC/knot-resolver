10c657f6e3414a73e20b5c92924990640c55362b
Aleš <ales.mrazek@nic.cz>
2021-05-06 17:07:19 +0200

    config: several config options and features - initial implementation of network.interfaces - network.edns-buffer-size accepts one or two arguments - suffixes for byte size and time interval

--- manager/knot_resolver_manager/datamodel/cache_config.py
+++ manager/knot_resolver_manager/datamodel/cache_config.py
@@ -1,8 +1,22 @@
+from typing import Optional
+
 from knot_resolver_manager.compat.dataclasses import dataclass
+from knot_resolver_manager.datamodel.types import SizeUnits
 from knot_resolver_manager.utils.dataclasses_parservalidator import DataclassParserValidatorMixin
 
 
 @dataclass
 class CacheConfig(DataclassParserValidatorMixin):
+    storage: str = "/var/cache/knot-resolver"
+    size_max: Optional[str] = None
+    _size_max_bytes: int = 100 * SizeUnits.mebibyte
+
+    def __post_init__(self):
+        if self.size_max:
+            self._size_max_bytes = SizeUnits.parse(self.size_max)
+
+    def get_size_max(self) -> int:
+        return self._size_max_bytes
+
     def _validate(self):
         pass
--- manager/knot_resolver_manager/datamodel/types.py
+++ manager/knot_resolver_manager/datamodel/types.py
@@ -1,3 +1,41 @@
 import re
 
-IPV6_PREFIX_96 = re.compile(r"^([0-9A-Fa-f]{1,4}:){2}:$")
+from .errors import DataValidationError
+
+RE_IPV6_PREFIX_96 = re.compile(r"^([0-9A-Fa-f]{1,4}:){2}:$")
+
+
+class TimeUnits:
+    second = 1
+    minute = 60
+    hour = 3600
+    day = 24 * 3600
+
+    _re = re.compile(r"^(\d+)\s{0,1}([smhd]){0,1}$")
+    _map = {"s": second, "m": minute, "h": hour, "d": day}
+
+    @staticmethod
+    def parse(time_str: str) -> int:
+        searched = TimeUnits._re.search(time_str)
+        if searched:
+            value, unit = searched.groups()
+            return int(value) * TimeUnits._map.get(unit, 1)
+        raise DataValidationError(f"failed to parse: {time_str}")
+
+
+class SizeUnits:
+    byte = 1
+    kibibyte = 1024
+    mebibyte = 1024 ** 2
+    gibibyte = 1024 ** 3
+
+    _re = re.compile(r"^([0-9]+)\s{0,1}([BKMG]){0,1}$")
+    _map = {"B": byte, "K": kibibyte, "M": mebibyte, "G": gibibyte}
+
+    @staticmethod
+    def parse(size_str: str) -> int:
+        searched = SizeUnits._re.search(size_str)
+        if searched:
+            value, unit = searched.groups()
+            return int(value) * SizeUnits._map.get(unit, 1)
+        raise DataValidationError(f"failed to parse: {size_str}")
