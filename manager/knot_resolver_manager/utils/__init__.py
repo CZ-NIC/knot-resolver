7dbace39d38b3f60cf3d511324e42eb3b9097ab9
Vasek Sraier <git@vakabus.cz>
2021-06-12 19:45:18 +0200

    types: type fixes for a new version of pyright

--- manager/knot_resolver_manager/utils/__init__.py
+++ manager/knot_resolver_manager/utils/__init__.py
@@ -6,9 +6,20 @@ from .overload import Overloaded
 T = TypeVar("T")
 
 
-def ignore_exceptions(
-    default: Optional[T], *exceptions: Type[BaseException]
+def ignore_exceptions_optional(
+    _tp: Type[T], default: Optional[T], *exceptions: Type[BaseException]
 ) -> Callable[[Callable[..., Optional[T]]], Callable[..., Optional[T]]]:
+    """
+    Decorator, that wraps around a function preventing it from raising exceptions
+    and instead returning the configured default value.
+
+    :param Type[T] _tp: Return type of the function. Essentialy only a template argument for type-checking
+    :param T default: The value to return as a default
+    :param List[Type[BaseException]] exceptions: The list of exceptions to catch
+    :return: value of the decorated function, or default if exception raised
+    :rtype: T
+    """
+
     def decorator(func: Callable[..., Optional[T]]) -> Callable[..., Optional[T]]:
         def f(*nargs: Any, **nkwargs: Any) -> Optional[T]:
             try:
@@ -24,7 +35,14 @@ def ignore_exceptions(
     return decorator
 
 
+def ignore_exceptions(
+    default: T, *exceptions: Type[BaseException]
+) -> Callable[[Callable[..., Optional[T]]], Callable[..., Optional[T]]]:
+    return ignore_exceptions_optional(type(default), default, *exceptions)
+
+
 __all__ = [
+    "ignore_exceptions_optional",
     "ignore_exceptions",
     "types",
     "DataclassParserValidatorMixin",
