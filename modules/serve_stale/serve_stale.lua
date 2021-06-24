da606f3e2fd3759189c421ae4b056d62329e9504
Lukáš Ježek <lukas.jezek@nic.cz>
2021-06-24 13:53:22 +0200

    log: lua logging

--- modules/serve_stale/serve_stale.lua
+++ modules/serve_stale/serve_stale.lua
@@ -9,7 +9,7 @@ M.timeout = 3*sec
 
 M.callback = ffi.cast("kr_stale_cb",
 	function (ttl) --, name, type, qry)
-		--log('[     ][stal]   => called back with TTL: ' .. tostring(ttl))
+		--log_debug(ffi.C.SRVSTALE, '   => called back with TTL: ' .. tostring(ttl))
 		if ttl + 3600 * 24 > 0 then -- at most one day stale
 			return 1
 		else
@@ -27,9 +27,7 @@ M.layer = {
 		local now = ffi.C.kr_now()
 		local deadline = qry.creation_time_mono + M.timeout
 		if now > deadline or qry.flags.NO_NS_FOUND then
-			if verbose() then
-				log('[     ][stal]   => no reachable NS, using stale data')
-			end
+			log_info(ffi.C.LOG_GRP_SRVSTALE, '   => no reachable NS, using stale data')
 			qry.stale_cb = M.callback
 			-- TODO: probably start the same request that doesn't stale-serve,
 			-- but first we need some detection of non-interactive / internal requests.
