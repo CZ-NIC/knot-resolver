6cd7118814b2cdf6678bd8a3e694d23664cd28ab
Vladimír Čunát <vladimir.cunat@nic.cz>
2021-07-01 19:29:21 +0200

    fixup! ci/pkgtest: nix WIP

--- distro/pkg/nix/top-level.nix
+++ distro/pkg/nix/top-level.nix
@@ -3,5 +3,10 @@ with import <nixpkgs> {};
 (callPackage ./. {
 }).overrideAttrs (attrs: {
   src = ./knot-resolver-{{ version }}.tar.xz;
+
+  # This just breaks in CI (though not locally).
+  installCheckPhase = ''
+    meson test --print-errorlogs --no-suite snowflake
+  '';
 })
 
