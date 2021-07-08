d3fd83052e7c1a75e2a194659698d347299f2fd5
Tomas Krizek <tomas.krizek@nic.cz>
2021-07-08 16:40:32 +0200

    logging: use dedicated DOTAUTH log for experimental auth DoT

--- lib/log.h
+++ lib/log.h
@@ -69,6 +69,7 @@ enum kr_log_groups_type {
 	LOG_GRP_NSID,
 	LOG_GRP_DNSTAP,
 	LOG_GRP_TESTS,
+	LOG_GRP_DOTAUTH,
 	/* ^^ Add new log groups above ^^. */
 	LOG_GRP_DEVEL,  /* Must be last entry in enum! */
 };
@@ -113,6 +114,7 @@ enum kr_log_groups_type {
 #define LOG_GRP_NSID_TAG		"nsid"
 #define LOG_GRP_DNSTAP_TAG		"dnstap"
 #define LOG_GRP_TESTS_TAG		"tests"
+#define LOG_GRP_DOTAUTH_TAG		"dotaut"
 #define LOG_GRP_DEVEL_TAG		"devel"
 
 KR_EXPORT
