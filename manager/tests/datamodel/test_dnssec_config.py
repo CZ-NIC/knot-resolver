ce9780c4ccfd89c40d490549993a2de7418d05f9
Aleš <ales.mrazek@nic.cz>
2021-09-09 15:49:11 +0200

    fixup! fixup! datamodel: dnssec: section completed, tests

--- manager/tests/datamodel/test_dnssec_config.py
+++ manager/tests/datamodel/test_dnssec_config.py
@@ -30,7 +30,9 @@ def test_parsing():
     assert config.refresh_time == TimeUnit("10s")
     assert config.hold_down_time == TimeUnit("45d")
 
-    assert config.trust_anchors == [". 3600 IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"]
+    assert config.trust_anchors == [
+        ". 3600 IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"
+    ]
     assert config.negative_trust_anchors == ["bad.boy", "example.com"]
     assert config.trust_anchors_files[0].file == "root.key"
     assert config.trust_anchors_files[0].read_only == True
@@ -44,7 +46,9 @@ def test_validating():
     assert strict.refresh_time == 10
     assert strict.hold_down_time == 45 * 24 * 60 ** 2
 
-    assert strict.trust_anchors == [". 3600 IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"]
+    assert strict.trust_anchors == [
+        ". 3600 IN DS 19036 8 2 49AAC11D7B6F6446702E54A1607371607A1A41855200FD2CE1CDDE32F24E8FB5"
+    ]
     assert strict.negative_trust_anchors == ["bad.boy", "example.com"]
     assert strict.trust_anchors_files[0].file == "root.key"
     assert strict.trust_anchors_files[0].read_only == True
