fc0048a9bb55e8a51cf3bc531066b44f885c03f9
Aleš <ales.mrazek@nic.cz>
2021-09-21 12:40:35 +0200

    tests: datamodel: dns64, dnssec defaults/conversion check

--- manager/tests/datamodel/test_config.py
+++ manager/tests/datamodel/test_config.py
@@ -2,15 +2,27 @@ from knot_resolver_manager.datamodel import KresConfig
 from knot_resolver_manager.datamodel.types import IPv6Network96, TimeUnit
 
 
-def test_dns64_true_default():
+def test_dns64_true():
     config = KresConfig({"dns64": True})
 
     assert config.dns64
     assert config.dns64.prefix == IPv6Network96("64:ff9b::/96")
 
 
-def test_dnssec_true_default():
-    config = KresConfig({"dnssec": True})
+def test_dns64_default_false():
+    config = KresConfig()
+
+    assert config.dns64 == False
+
+
+def test_dnssec_false():
+    config = KresConfig({"dnssec": False})
+
+    assert config.dnssec == False
+
+
+def test_dnssec_default_true():
+    config = KresConfig()
 
     assert config.dnssec.trust_anchor_sentinel == True
     assert config.dnssec.trust_anchor_signal_query == True
