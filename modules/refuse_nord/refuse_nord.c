41e9647fcbb9c3e5b2f07a51f16279d1508c5d37
Tomas Krizek <tomas.krizek@nic.cz>
2021-11-15 17:42:58 +0100

    ede: handle not authoritative

--- modules/refuse_nord/refuse_nord.c
+++ modules/refuse_nord/refuse_nord.c
@@ -21,6 +21,7 @@ static int refuse_nord_query(kr_layer_t *ctx)
 		return ctx->state;
 	knot_wire_set_rcode(answer->wire, KNOT_RCODE_REFUSED);
 	knot_wire_clear_ad(answer->wire);
+	kr_request_set_extended_error(req, KNOT_EDNS_EDE_NOTAUTH, NULL);
 	ctx->state = KR_STATE_DONE;
 	return ctx->state;
 }
