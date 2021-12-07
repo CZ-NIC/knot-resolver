62839f7a55bc4253265e4bac8daf9847ae602c88
Aleš <ales.mrazek@nic.cz>
2021-12-07 12:40:19 +0100

    datamodel: stub-zone: added StubServerSchema - added to be in line with forward-zones

--- manager/knot_resolver_manager/datamodel/stub_zone_schema.py
+++ manager/knot_resolver_manager/datamodel/stub_zone_schema.py
@@ -1,11 +1,15 @@
-from typing import List, Optional
+from typing import List, Optional, Union
 
 from knot_resolver_manager.datamodel.types import IPAddressPort
 from knot_resolver_manager.datamodel.view_schema import FlagsEnum
 from knot_resolver_manager.utils import SchemaNode
 
 
+class StubServerSchema(SchemaNode):
+    address: IPAddressPort
+
+
 class StubZoneSchema(SchemaNode):
-    servers: List[IPAddressPort]
+    servers: Union[List[IPAddressPort], List[StubServerSchema]]
     views: Optional[List[str]] = None
     options: Optional[List[FlagsEnum]] = None
