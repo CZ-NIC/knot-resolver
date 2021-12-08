730c7c31774f03ee14c5bab12619d52a80094164
Aleš <ales.mrazek@nic.cz>
2021-12-08 19:39:18 +0100

    datamodel: server: added watchdog config

--- manager/knot_resolver_manager/datamodel/rpz_schema.py
+++ manager/knot_resolver_manager/datamodel/rpz_schema.py
@@ -1,14 +1,14 @@
 from typing import List, Optional
 
 from knot_resolver_manager.datamodel.policy_schema import ActionEnum
-from knot_resolver_manager.datamodel.types import AnyPath
+from knot_resolver_manager.datamodel.types import CheckedPath
 from knot_resolver_manager.datamodel.view_schema import FlagsEnum
 from knot_resolver_manager.utils import SchemaNode
 
 
 class RPZSchema(SchemaNode):
     action: ActionEnum
-    file: AnyPath
+    file: CheckedPath
     watch: bool = True
     views: Optional[List[str]] = None
     options: Optional[List[FlagsEnum]] = None
