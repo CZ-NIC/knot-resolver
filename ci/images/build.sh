cd43a4405c70fbedb6e4ef751dd86f6ef2f31179
Tomas Krizek <tomas.krizek@nic.cz>
2022-01-20 17:08:46 +0100

    ci/images: add LXC-based images with knot and apkg
    These container images are intended to run packaging tests which
    require systemd and compiled kresd.

--- ci/images/build.sh
+++ ci/images/build.sh
@@ -5,4 +5,4 @@ CURRENT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" >/dev/null 2>&1 && pwd)"
 source "${CURRENT_DIR}"/vars.sh "$@"
 set -ex
 
-docker build --no-cache -t "${FULL_NAME}" "${IMAGE}" --build-arg KNOT_BRANCH=${KNOT_BRANCH}
+docker build --no-cache --squash -t "${FULL_NAME}" "${IMAGE}" --build-arg KNOT_BRANCH=${KNOT_BRANCH}
