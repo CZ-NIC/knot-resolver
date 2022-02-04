ff5aa75385ad133e631095f81e0de217f05fa7cb
Aleš <ales.mrazek@nic.cz>
2022-02-04 13:47:41 +0100

    datamodel: docstrings annotatinons (stub-zone, forward-zone, rpz, cache) - related to #706

--- manager/knot_resolver_manager/datamodel/stub_zone_schema.py
+++ manager/knot_resolver_manager/datamodel/stub_zone_schema.py
@@ -5,10 +5,26 @@ from knot_resolver_manager.utils import SchemaNode
 
 
 class StubServerSchema(SchemaNode):
+    """
+    Configuration of Stub server.
+
+    ---
+    address: IP address of Stub server.
+    """
+
     address: IPAddressOptionalPort
 
 
 class StubZoneSchema(SchemaNode):
+    """
+    Configuration of Stub Zone.
+
+    ---
+    servers: IP address of Stub server.
+    views: Use this Stub Zone only for clients defined by views.
+    options: Configuration flags for Stub Zone.
+    """
+
     servers: Union[List[IPAddressOptionalPort], List[StubServerSchema]]
     views: Optional[List[str]] = None
     options: Optional[List[PolicyFlagEnum]] = None
