64e253cd8ec209b2da9477cabe2726d247f0e6fb
Aleš <ales.mrazek@nic.cz>
2022-02-07 16:34:07 +0100

    datamodel: docstrings annotatinons (dns64, logging, lua) - related to #706

--- manager/knot_resolver_manager/datamodel/dns64_schema.py
+++ manager/knot_resolver_manager/datamodel/dns64_schema.py
@@ -3,4 +3,11 @@ from knot_resolver_manager.utils import SchemaNode
 
 
 class Dns64Schema(SchemaNode):
+    """
+    DNS64 (RFC 6147) configuration.
+
+    ---
+    prefix: IPv6 prefix to be used for synthesizing AAAA records.
+    """
+
     prefix: IPv6Network96 = IPv6Network96("64:ff9b::/96")
