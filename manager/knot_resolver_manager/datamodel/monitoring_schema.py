e9a04f34337eed82cc083b2699bfabcd800d0854
Vasek Sraier <git@vakabus.cz>
2022-02-08 15:10:31 +0100

    manager: monitoring: graphite bridge

--- manager/knot_resolver_manager/datamodel/monitoring_schema.py
+++ manager/knot_resolver_manager/datamodel/monitoring_schema.py
@@ -1,23 +1,29 @@
-from typing import Optional
+from typing import Union
 
 from typing_extensions import Literal
 
+from knot_resolver_manager.datamodel.types import TimeUnit
 from knot_resolver_manager.utils.modelling import SchemaNode
 
 
 class GraphiteSchema(SchemaNode):
-    endpoint: str
-    prefix: str
-    interval_sec: int
-    tcp: bool
+    host: str
+    port: int = 2003
+    prefix: str = ""
+    interval_sec: TimeUnit = TimeUnit("5s")
+    tcp: bool = False
+
+    def _validate(self):
+        if not 0 < self.port < 65_536:
+            raise ValueError("port must be between 0 and 65536 (both exclusive)")
 
 
 class MonitoringSchema(SchemaNode):
     """
     ---
-    state: configures, whether statistics module will be loaded into resolver
+    enabled: configures, whether statistics module will be loaded into resolver
     graphite: optionally configures where should graphite metrics be sent to
     """
 
-    state: Literal["manager-only", "lazy", "always"] = "always"
-    graphite: Optional[GraphiteSchema] = None
+    enabled: Literal["manager-only", "lazy", "always"] = "always"
+    graphite: Union[Literal[False], GraphiteSchema] = False
