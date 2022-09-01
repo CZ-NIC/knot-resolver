1ce4b6827005ddcdfcca1c5788a4e474a04f455f
Aleš Mrázek <ales.mrazek@nic.cz>
2022-09-01 14:28:17 +0200

    manager: kresctl: command parsing done

--- manager/knot_resolver_manager/cli/cmd/exit.py
+++ manager/knot_resolver_manager/cli/cmd/exit.py
@@ -1,23 +1,21 @@
 import argparse
+import sys
 from typing import Tuple, Type
 
 from knot_resolver_manager.cli.command import Command, CommandArgs, register_command
-from knot_resolver_manager.utils.requests import request
 
 
 @register_command
-class StopCommand(Command):
+class ExitCommand(Command):
     def __init__(self, namespace: argparse.Namespace) -> None:
         super().__init__(namespace)
 
     def run(self, args: CommandArgs) -> None:
-        url = f"{args.socket}/stop"
-        response = request("POST", url)
-        print(response)
+        sys.exit()
 
     @staticmethod
     def register_args_subparser(
         parser: "argparse._SubParsersAction[argparse.ArgumentParser]",
     ) -> Tuple[argparse.ArgumentParser, "Type[Command]"]:
-        stop = parser.add_parser("stop", help="shutdown everything")
-        return stop, StopCommand
+        stop = parser.add_parser("exit", help="exit kresctl")
+        return stop, ExitCommand
