3cac9152e26f9e8ee81fe3a6ad18f5c46b55f6e3
Aleš Mrázek <ales.mrazek@nic.cz>
2022-09-13 18:47:47 +0200

    manager: kresctl: config command - schema, metrics command modules lint

--- manager/knot_resolver_manager/cli/cmd/schema.py
+++ manager/knot_resolver_manager/cli/cmd/schema.py
@@ -1,15 +1,12 @@
 import argparse
-from optparse import Option
-import sys
 from typing import Optional, Tuple, Type
-from knot_resolver_manager.utils.requests import request
 
 from knot_resolver_manager.cli.command import Command, CommandArgs, register_command
+from knot_resolver_manager.utils.requests import request
 
 
 @register_command
 class SchemaCommand(Command):
-
     def __init__(self, namespace: argparse.Namespace) -> None:
         self.file: Optional[str] = namespace.file
 
@@ -20,7 +17,7 @@ class SchemaCommand(Command):
         parser: "argparse._SubParsersAction[argparse.ArgumentParser]",
     ) -> Tuple[argparse.ArgumentParser, "Type[Command]"]:
         schema = parser.add_parser("schema", help="get JSON schema reprezentation of the configuration")
-        schema.add_argument('file', help="optional, file to export JSON schema to", nargs='?', default=None)
+        schema.add_argument("file", help="optional, file to export JSON schema to", nargs="?", default=None)
         return schema, SchemaCommand
 
     def run(self, args: CommandArgs) -> None:
@@ -28,7 +25,7 @@ class SchemaCommand(Command):
         response = request("GET", url)
 
         if self.file and response.status == 200:
-            with open(self.file, 'w') as f:
+            with open(self.file, "w") as f:
                 f.write(response.body)
         else:
             print(response)
