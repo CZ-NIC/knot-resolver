ca8f03637cb37e5712f79f328ede539c83466d54
Aleš Mrázek <ales.mrazek@nic.cz>
2022-09-13 18:47:47 +0200

    manager: kresctl: config command - schema, metrics command modules lint

--- manager/knot_resolver_manager/cli/cmd/metrics.py
+++ manager/knot_resolver_manager/cli/cmd/metrics.py
@@ -1,15 +1,12 @@
 import argparse
-from optparse import Option
-import sys
 from typing import Optional, Tuple, Type
-from knot_resolver_manager.utils.requests import request
 
 from knot_resolver_manager.cli.command import Command, CommandArgs, register_command
+from knot_resolver_manager.utils.requests import request
 
 
 @register_command
 class MetricsCommand(Command):
-
     def __init__(self, namespace: argparse.Namespace) -> None:
         self.file: Optional[str] = namespace.file
 
@@ -20,7 +17,7 @@ class MetricsCommand(Command):
         parser: "argparse._SubParsersAction[argparse.ArgumentParser]",
     ) -> Tuple[argparse.ArgumentParser, "Type[Command]"]:
         metrics = parser.add_parser("metrics", help="get prometheus metrics data")
-        metrics.add_argument('file', help="optional, file to export metrics to", nargs='?', default=None)
+        metrics.add_argument("file", help="optional, file to export metrics to", nargs="?", default=None)
         return metrics, MetricsCommand
 
     def run(self, args: CommandArgs) -> None:
@@ -28,7 +25,7 @@ class MetricsCommand(Command):
         response = request("GET", url)
 
         if self.file and response.status == 200:
-            with open(self.file, 'w') as f:
+            with open(self.file, "w") as f:
                 f.write(response.body)
         else:
             print(response)
