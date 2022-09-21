0deb873105d6ef7b65f500a89064c63e819ea05a
Vasek Sraier <git@vakabus.cz>
2022-09-21 16:47:02 +0200

    Revert "tests/packaging: skip workers.sh"
    This reverts commit 16ea2c8fc493f3175bfa510682579317ef58587a.

--- manager/tests/packaging/interactive/workers.sh
+++ manager/tests/packaging/interactive/workers.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# set -e
+set -e
 
-# kresctl config /workers 5
+kresctl config /workers 5
 
-# test "$(ps -a -x | grep kresd | grep -v grep | wc -l)" -eq 5
+test "$(ps -a -x | grep kresd | grep -v grep | wc -l)" -eq 5
