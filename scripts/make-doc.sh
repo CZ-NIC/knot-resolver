2eea2134ab6d82067e5b69c3a04b40bb132e100a
Vasek Sraier <git@vakabus.cz>
2022-09-26 13:21:32 +0200

    docs: generating and linking JSON schema for the declarative config

--- scripts/make-doc.sh
+++ scripts/make-doc.sh
@@ -2,6 +2,15 @@
 # SPDX-License-Identifier: GPL-3.0-or-later
 cd "$(dirname ${0})/.."
 
+
+# generate JSON schema for the manager's declarative config
+pushd manager
+## the following python command should hopefully run without any dependencies except for standard python
+mkdir -p ../doc/_static/
+python3 -m knot_resolver_manager.cli schema > ../doc/_static/config-schema.json
+popd
+
+
 pushd doc
 doxygen
 popd
