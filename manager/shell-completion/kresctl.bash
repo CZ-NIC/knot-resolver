14565d90ec09c3d9eda6bbf62aaf382ec6a38e7c
Aleš Mrázek <ales.mrazek@nic.cz>
2022-09-30 14:02:36 +0200

    manager: pkg: rpm: completion files for kresctl utility

---
+++ manager/shell-completion/kresctl.bash
@@ -0,0 +1,22 @@
+#/usr/bin/env bash
+
+# completion function for the kresctl
+_kresctl_completion() {
+    local cur prev opts
+    COMPREPLY=()
+    cur="${COMP_WORDS[COMP_CWORD]}"
+    prev="${COMP_WORDS[COMP_CWORD-1]}"
+
+    # get options
+    opts=$(kresctl completion --bash "${COMP_WORDS}")
+
+    if [[ ${cur} == -* || ${COMP_CWORD} -eq 1 ]] ; then
+    COMPREPLY=( $(compgen -W "${opts}" -- ${cur}) )
+    return 0
+    fi
+
+    return 0
+}
+
+# use the bash default completion for other arguments
+complete -o filenames -o nospace -o bashdefault -F _kresctl_completion kresctl
