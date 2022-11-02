c7f46d7448d7813dc22aeb8b39acdd4bb9ebc858
Aleš Mrázek <ales.mrazek@nic.cz>
2022-11-02 12:36:59 +0100

    manager: kresctl: 'reload' cmd added

--- manager/knot_resolver_manager/server.py
+++ manager/knot_resolver_manager/server.py
@@ -91,17 +91,7 @@ class Server:
             )
         return Result.ok(None)
 
-    async def sigint_handler(self) -> None:
-        logger.info("Received SIGINT, triggering graceful shutdown")
-        self.trigger_shutdown(0)
-
-    async def sigterm_handler(self) -> None:
-        logger.info("Received SIGTERM, triggering graceful shutdown")
-        self.trigger_shutdown(0)
-
-    async def sighup_handler(self) -> None:
-        logger.info("Received SIGHUP, reloading configuration file")
-        systemd_notify(RELOADING="1")
+    async def _reload_config(self) -> None:
 
         if self._config_path is None:
             logger.warning("The manager was started with inlined configuration - can't reload")
@@ -124,6 +114,18 @@ class Server:
                 logger.error(f"Reloading of the configuration file failed: {e}")
                 logger.error("Configuration have NOT been changed.")
 
+    async def sigint_handler(self) -> None:
+        logger.info("Received SIGINT, triggering graceful shutdown")
+        self.trigger_shutdown(0)
+
+    async def sigterm_handler(self) -> None:
+        logger.info("Received SIGTERM, triggering graceful shutdown")
+        self.trigger_shutdown(0)
+
+    async def sighup_handler(self) -> None:
+        logger.info("Received SIGHUP, reloading configuration file")
+        systemd_notify(RELOADING="1")
+        await self._reload_config()
         systemd_notify(READY="1")
 
     @staticmethod
@@ -260,6 +262,15 @@ class Server:
         logger.info("Shutdown event triggered...")
         return web.Response(text="Shutting down...")
 
+    async def _handler_reload(self, _request: web.Request) -> web.Response:
+        """
+        Route handler for reloading the server
+        """
+
+        logger.info("Reloading event triggered...")
+        await self._reload_config()
+        return web.Response(text="Reloading...")
+
     def _setup_routes(self) -> None:
         self.app.add_routes(
             [
@@ -269,6 +280,7 @@ class Server:
                 web.delete(r"/v1/config{path:.*}", self._handler_config_query),
                 web.patch(r"/v1/config{path:.*}", self._handler_config_query),
                 web.post("/stop", self._handler_stop),
+                web.post("/reload", self._handler_reload),
                 web.get("/schema", self._handler_schema),
                 web.get("/schema/ui", self._handle_view_schema),
                 web.get("/metrics", self._handler_metrics),
