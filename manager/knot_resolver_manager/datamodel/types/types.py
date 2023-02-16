a07701e01cb25117f19f3eb508098ced15836944
Aleš Mrázek <ales.mrazek@nic.cz>
2023-02-16 10:22:12 +0100

    manager: datamodel: types: alias for union of list and single element

--- manager/knot_resolver_manager/datamodel/types/types.py
+++ manager/knot_resolver_manager/datamodel/types/types.py
@@ -1,9 +1,13 @@
 import ipaddress
 import re
-from typing import Any, Dict, Optional, Type, Union
+from typing import Any, Dict, List, Optional, Type, TypeVar, Union
 
 from knot_resolver_manager.datamodel.types.base_types import IntRangeBase, PatternBase, StrBase, UnitBase
 from knot_resolver_manager.utils.modeling import BaseValueType
+
+_ElementType = TypeVar("_ElementType")
+
+ListOrSingle = Union[List[_ElementType], _ElementType]
 
 
 class IntNonNegative(IntRangeBase):
