d2c3853f1225223a65e77efd3c3d00e65c70765a
Aleš Mrázek <ales.mrazek@nic.cz>
2023-03-31 14:56:46 +0200

    manager: datamodel: template: macro for TAGS_ASSIGN

--- manager/tests/unit/datamodel/templates/test_policy_macros.py
+++ manager/tests/unit/datamodel/templates/test_policy_macros.py
@@ -28,6 +28,16 @@ def test_policy_flags():
     )
 
 
+def test_policy_tags_assign():
+    tags: List[str] = ["t01", "t02", "t03"]
+    tmpl_str = """{% from 'macros/policy_macros.lua.j2' import policy_tags_assign %}
+{{ policy_tags_assign(tags) }}"""
+
+    tmpl = template_from_str(tmpl_str)
+    assert tmpl.render(tags=tags[1]) == f"policy.TAGS_ASSIGN('{tags[1]}')"
+    assert tmpl.render(tags=tags) == "policy.TAGS_ASSIGN({" + ",".join([f"'{x}'" for x in tags]) + ",})"
+
+
 # Filters
 
 
