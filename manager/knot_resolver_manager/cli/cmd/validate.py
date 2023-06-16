3a511cd25028acd863e2c0da7387c6fa9dee36db
Aleš Mrázek <ales.mrazek@nic.cz>
2023-06-16 14:43:53 +0200

    manager: cli: validate: --no-strict arg added

--- manager/knot_resolver_manager/cli/cmd/validate.py
+++ manager/knot_resolver_manager/cli/cmd/validate.py
@@ -5,7 +5,11 @@ from typing import List, Tuple, Type
 
 from knot_resolver_manager.cli.command import Command, CommandArgs, CompWords, register_command
 from knot_resolver_manager.datamodel import KresConfig
-from knot_resolver_manager.datamodel.globals import Context, set_global_validation_context
+from knot_resolver_manager.datamodel.globals import (
+    Context,
+    reset_global_validation_context,
+    set_global_validation_context,
+)
 from knot_resolver_manager.utils.modeling import try_to_parse
 from knot_resolver_manager.utils.modeling.exceptions import DataParsingError, DataValidationError
 
@@ -15,12 +19,20 @@ class ValidateCommand(Command):
     def __init__(self, namespace: argparse.Namespace) -> None:
         super().__init__(namespace)
         self.input_file: str = namespace.input_file
+        self.strict: bool = namespace.strict
 
     @staticmethod
     def register_args_subparser(
         subparser: "argparse._SubParsersAction[argparse.ArgumentParser]",
     ) -> Tuple[argparse.ArgumentParser, "Type[Command]"]:
         validate = subparser.add_parser("validate", help="Validates configuration in JSON or YAML format.")
+        validate.set_defaults(strict=True)
+        validate.add_argument(
+            "--no-strict",
+            help="Ignore strict rules during validation, e.g. path/file existence.",
+            action="store_false",
+            dest="strict",
+        )
         validate.add_argument(
             "input_file",
             type=str,
@@ -43,8 +55,9 @@ class ValidateCommand(Command):
             data = input("Type configuration to validate: ")
 
         try:
-            set_global_validation_context(Context(resolve_directory=Path(self.input_file).parent))
+            set_global_validation_context(Context(Path(self.input_file).parent, self.strict))
             KresConfig(try_to_parse(data))
+            reset_global_validation_context()
         except (DataParsingError, DataValidationError) as e:
             print(e)
             sys.exit(1)
