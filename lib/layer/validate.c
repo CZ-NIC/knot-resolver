a51d08c1c97306dd41900143006918a4d620e50e
Vladimír Čunát <vladimir.cunat@nic.cz>
2024-01-01 16:21:10 +0100

    mitigate KeyTrap DoS = CVE-2023-50387
    (cherry picked from commit feb65eb97b93f0f024d70c7f5f6cbc6802ba02ec)

--- lib/layer/validate.c
+++ lib/layer/validate.c
@@ -275,6 +275,7 @@ static int validate_records(struct kr_request *req, knot_pkt_t *answer, knot_mm_
 		.err_cnt	= 0,
 		.cname_norrsig_cnt = 0,
 		.result		= 0,
+		.limit_crypto_remains = &qry->vld_limit_crypto_remains,
 		.log_qry	= qry,
 	};
 
@@ -383,6 +384,7 @@ static int validate_keyset(struct kr_request *req, knot_pkt_t *answer, bool has_
 			.has_nsec3	= has_nsec3,
 			.flags		= 0,
 			.result		= 0,
+			.limit_crypto_remains = &qry->vld_limit_crypto_remains,
 			.log_qry	= qry,
 		};
 		int ret = kr_dnskeys_trusted(&vctx, sig_rds, qry->zone_cut.trust_anchor);
@@ -1028,6 +1030,11 @@ static int validate(kr_layer_t *ctx, knot_pkt_t *pkt)
 	int ret = 0;
 	struct kr_request *req = ctx->req;
 	struct kr_query *qry = req->current_query;
+
+	if (qry->vld_limit_uid != qry->uid) {
+		qry->vld_limit_uid = qry->uid;
+		qry->vld_limit_crypto_remains = req->ctx->vld_limit_crypto;
+	}
 
 	/* Ignore faulty or unprocessed responses. */
 	if (ctx->state & (KR_STATE_FAIL|KR_STATE_CONSUME)) {
