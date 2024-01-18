2e9fc7fb64544ab3f74fd69c3c88014561e55338
Oto Šťáva <oto.stava@nic.cz>
2024-01-18 20:05:40 +0100

    manager/tests/packaging: More verbose systemd_service tests

--- manager/tests/packaging/interactive/etag.sh
+++ manager/tests/packaging/interactive/etag.sh
@@ -4,7 +4,11 @@ set -e
 
 socket_opt="--unix-socket /var/run/knot-resolver/manager.sock"
 
+echo "  etag"
 etag="$(curl --silent $socket_opt --fail http://localhost:5000/v1/config -o /dev/null -v 2>&1 | grep ETag | sed 's/< ETag: //;s/\s//')"
-status=$(curl --silent $socket_opt --fail  http://localhost:5000/v1/config --header "If-None-Match: $etag" -w "%{http_code}" -o /dev/null)
+echo "  etag OK"
 
+echo "  status"
+status=$(curl --silent $socket_opt --fail  http://localhost:5000/v1/config --header "If-None-Match: $etag" -w "%{http_code}" -o /dev/null)
 test "$status" -eq 304
+echo "  status OK"
