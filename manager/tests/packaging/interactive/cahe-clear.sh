466a20397d6bf2989b6cd6e6d02e9ed2f283c146
Aleš Mrázek <ales.mrazek@nic.cz>
2024-01-29 15:19:58 +0100

    manager/tests: simple test for /cache-clear API endpoint

---
+++ manager/tests/packaging/interactive/cahe-clear.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# clear full cache
+kresctl cache-clear
+if [ "$?" -ne "0" ]; then
+	echo "Could not clear full cache"
+	exit 1
+fi
+
+# clear just example.com. AAAA record
+kresctl cache-clear --exact-name --rr-type AAAA example.com.
+if [ "$?" -ne "0" ]; then
+	echo "Could not clear example.com. AAAA record"
+	exit 1
+fi
