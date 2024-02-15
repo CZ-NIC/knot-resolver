a4934c72893e1b2fd3e3b8941470835f64b64177
Aleš Mrázek <ales.mrazek@nic.cz>
2024-02-15 12:10:08 +0100

    manager/tests: validate JSON output from "kresctl cache clear --json" command

--- manager/tests/packaging/interactive/cache-clear.sh
+++ manager/tests/packaging/interactive/cache-clear.sh
@@ -7,9 +7,9 @@ if [ "$?" -ne "0" ]; then
 	exit 1
 fi
 
-# clear just example.com. AAAA record
-kresctl cache clear --exact-name --rr-type AAAA example.com.
+# clear just example.com. AAAA record, get JSON output
+kresctl cache clear --json --exact-name --rr-type AAAA example.com. | python3 -m json.tool
 if [ "$?" -ne "0" ]; then
-	echo "Could not clear example.com. AAAA record"
+	echo "Could not clear example.com. AAAA record or output is not a valid JSON"
 	exit 1
 fi
