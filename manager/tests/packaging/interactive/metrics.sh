f8d829597bf585f2c580df9028ddbc6d31096e4a
Aleš Mrázek <ales.mrazek@nic.cz>
2024-03-21 15:27:10 +0100

    tests/packaging: management API /metrics update

--- manager/tests/packaging/interactive/metrics.sh
+++ manager/tests/packaging/interactive/metrics.sh
@@ -3,3 +3,15 @@
 set -e
 
 curl --silent --fail --unix-socket /var/run/knot-resolver/manager.sock http://localhost/metrics > /dev/null
+
+kresctl metrics
+if [ "$?" -ne "0" ]; then
+	echo "Could not get metrics in JSON format"
+	exit 1
+fi
+
+kresctl metrics --prometheus
+if [ "$?" -ne "0" ]; then
+	echo "Could not get metrics in Prometheus format"
+	exit 1
+fi
