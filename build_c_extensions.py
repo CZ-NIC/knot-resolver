97728c44883137fbc00f68b3bc9e6d118d8c6507
Aleš Mrázek <ales.mrazek@nic.cz>
2024-07-12 10:12:36 +0200

    manager: poetry project moved to the repository root

--- build_c_extensions.py
+++ build_c_extensions.py
@@ -9,7 +9,7 @@ def build(setup_kwargs: Dict[Any, Any]) -> None:
             "ext_modules": [
                 Extension(
                     name="knot_resolver_manager.kresd_controller.supervisord.plugin.notify",
-                    sources=["knot_resolver_manager/kresd_controller/supervisord/plugin/notifymodule.c"],
+                    sources=["manager/knot_resolver_manager/kresd_controller/supervisord/plugin/notifymodule.c"],
                 ),
             ]
         }
