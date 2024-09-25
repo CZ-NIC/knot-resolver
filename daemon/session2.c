ea6d00f41636d04d1d715bd6540db7fb253ee76f
Vladimír Čunát <vladimir.cunat@nic.cz>
2024-09-25 10:39:43 +0200

    daemon/session2: avoid incorrectly generated errors
    The _try_ functions additionally return positive values on success,
    and the code around didn't deal with that.
    
    So far there's no evidence that this caused any issues beyond debug-logs:
      [prlayr] ... iteration of group ... ended with status 'Unknown error ...

--- daemon/session2.c
+++ daemon/session2.c
@@ -1450,10 +1450,12 @@ static int session2_transport_pushv(struct session2 *s,
 				int ret = uv_udp_try_send((uv_udp_t*)handle, (uv_buf_t *)iov, iovcnt,
 					the_network->enable_connect_udp ? NULL : comm->comm_addr);
 
-				if (ret == UV_EAGAIN) {
+				if (ret > 0) { // equals buffer size, only confuses us
+					ret = 0;
+				} else if (ret == UV_EAGAIN) {
 					ret = kr_error(ENOBUFS);
 					session2_event(s, PROTOLAYER_EVENT_OS_BUFFER_FULL, NULL);
-				} else if (ret < 0) {
+				} else {
 					kr_log_error(IO, "UDP connected send failed unexpectedly: %s\n",
 						uv_strerror(ret));
 					session2_transport_pushv_finished(ret, ctx);
@@ -1486,6 +1488,8 @@ static int session2_transport_pushv(struct session2 *s,
 				ret = kr_error(ENOBUFS);
 				session2_event(s, PROTOLAYER_EVENT_OS_BUFFER_FULL, NULL);
 			}
+			else if (ret > 0) // iovec_sum was checked, let's not get confused anymore
+				ret = 0;
 
 			if (false && ret == UV_EAGAIN) {
 				uv_write_t *req = malloc(sizeof(*req));
