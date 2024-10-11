37330d7288126b917d2e3ccab70400705f99c05d
Frantisek Tobias <frantisek.tobias@nic.cz>
2024-10-11 10:36:47 +0200

    kresctl: tab-completion: auto-complete for all commands except config

--- python/knot_resolver/client/commands/convert.py
+++ python/knot_resolver/client/commands/convert.py
@@ -3,7 +3,7 @@ import sys
 from pathlib import Path
 from typing import List, Optional, Tuple, Type
 
-from knot_resolver.client.command import Command, CommandArgs, CompWords, register_command
+from knot_resolver.client.command import Command, CommandArgs, CompWords, get_subparsers_words, register_command
 from knot_resolver.datamodel import KresConfig
 from knot_resolver.datamodel.globals import Context, reset_global_validation_context, set_global_validation_context
 from knot_resolver.utils.modeling import try_to_parse
@@ -52,7 +52,7 @@ class ConvertCommand(Command):
 
     @staticmethod
     def completion(args: List[str], parser: argparse.ArgumentParser) -> CompWords:
-        return {}
+        return get_subparsers_words(parser._actions)  # noqa: SLF001
 
     def run(self, args: CommandArgs) -> None:
         with open(self.input_file, "r") as f:
