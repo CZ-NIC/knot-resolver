8029a019bacf4b111bed58dfeb16545a834d84cf
Aleš Mrázek <ales.mrazek@nic.cz>
2024-11-26 21:43:48 +0100

    tests/packaging/interactive: suppress some commands stdout

--- tests/packaging/interactive/metrics.sh
+++ tests/packaging/interactive/metrics.sh
@@ -4,13 +4,13 @@ set -e
 
 curl --silent --fail --unix-socket /run/knot-resolver/kres-api.sock http://localhost/metrics > /dev/null
 
-kresctl metrics
+kresctl metrics > /dev/null
 if [ "$?" -ne "0" ]; then
 	echo "Could not get metrics in JSON format"
 	exit 1
 fi
 
-kresctl metrics --prometheus
+kresctl metrics --prometheus > /dev/null
 if [ "$?" -ne "0" ]; then
 	echo "Could not get metrics in Prometheus format"
 	exit 1
--- tests/packaging/interactive/schema.sh
+++ tests/packaging/interactive/schema.sh
@@ -1,14 +1,14 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -e
 
-kresctl schema
+kresctl schema > /dev/null
 if [ "$?" -ne "0" ]; then
 	echo "Failed to generate JSON schema with 'kresctl'"
 	exit 1
 fi
 
-kresctl schema --live
+kresctl schema --live > /dev/null
 if [ "$?" -ne "0" ]; then
 	echo "Failed to get JSON schema from the running resolver"
 	exit 1
