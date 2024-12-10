a48d032e8e3e1d66e0c9c050c710a410ea8cec7b
Frantisek Tobias <frantisek.tobias@nic.cz>
2024-12-10 08:23:44 +0100

    kresctl: tab-completion: stop appending space after one config layer is completed

--- utils/shell-completion/client.bash
+++ utils/shell-completion/client.bash
@@ -1,4 +1,4 @@
-#!/usr/bin/env bash
+#/usr/bin/env bash
 
 _kresctl_completion()
 {
@@ -15,6 +15,8 @@ _kresctl_completion()
         args="$args${args:+ }-d"
     elif [[ "$opt" == "#filenames#" ]]; then
         args="$args${args:+ }-f"
+    elif [[ "$opt" == "#nospace#" ]]; then
+        compopt -o nospace
     else
         words="$words${words:+ }$opt"
     fi
