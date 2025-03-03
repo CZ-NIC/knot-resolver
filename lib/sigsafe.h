2f2ec7bf40866e9bf7e7210438487358597534a4
Lukáš Ondráček <lukas.ondracek@nic.cz>
2025-03-03 19:25:12 +0100

    lib/sigsafe: convince linter of async-signal-safety

--- lib/sigsafe.h
+++ lib/sigsafe.h
@@ -2,6 +2,7 @@
  *  SPDX-License-Identifier: GPL-3.0-or-later
  */
 
+#pragma once
 #include "lib/defines.h"
 
 /// Async-signal-safe snprintf-like formatting function, it supports:
@@ -13,4 +14,5 @@
 ///   * %f takes double,   supports width and precision (defaults to .3);
 ///   * %r takes (struct sockaddr *).
 KR_EXPORT
-int sigsafe_format(char *str, size_t size, const char *fmt, ...);  // NOLINT(bugprone-signal-handler)
+int sigsafe_format(char *str, size_t size, const char *fmt, ...);
+#define sigsafe_format(...) sigsafe_format(__VA_ARGS__) // NOLINT, all calls are async-signal-safe
