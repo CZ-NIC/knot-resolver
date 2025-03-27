4a30bdedf3fd22752bda8397fef00f144b96dc73
Hynek Šabacký <hynek.sabacky@nic.cz>
2025-03-27 16:45:04 +0100

    dnamelimiting -> dns_tunnel_filter: Rename

--- daemon/dns_tunnel_filter.h
+++ daemon/dns_tunnel_filter.h
@@ -12,14 +12,14 @@
   * The existing data are used if another instance is already using the file
   * and it was initialized with the same parameters; it fails on mismatch. */
  KR_EXPORT
- int dnamelimiting_init(const char *mmap_file, size_t capacity, uint32_t instant_limit,
+ int dns_tunnel_filter_init(const char *mmap_file, size_t capacity, uint32_t instant_limit,
 		uint32_t rate_limit, uint16_t slip, uint32_t log_period, bool dry_run);
  
  /** Do rate-limiting, during knot_layer_api::begin. */
  KR_EXPORT
- bool dnamelimiting_request_begin(struct kr_request *req);
+ bool dns_tunnel_filter_request_begin(struct kr_request *req);
  
  /** Remove mmapped file data if not used by other processes. */
  KR_EXPORT
- void dnamelimiting_deinit(void);
+ void dns_tunnel_filter_deinit(void);
  
