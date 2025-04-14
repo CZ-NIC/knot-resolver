e0399a4f59565526e20b783b90c96ea657d6e484
Aleš Mrázek <ales.mrazek@nic.cz>
2025-04-14 20:08:29 +0200

    datamodel: stabilize dns64 schema

--- python/knot_resolver/datamodel/dns64_schema.py
+++ python/knot_resolver/datamodel/dns64_schema.py
@@ -9,11 +9,13 @@ class Dns64Schema(ConfigSchema):
     DNS64 (RFC 6147) configuration.
 
     ---
+    enabled: Enable/disable DNS64.
     prefix: IPv6 prefix to be used for synthesizing AAAA records.
-    rev_ttl: TTL in CNAME generated in the reverse 'ip6.arpa.' subtree.
+    reverse_ttl: TTL in CNAME generated in the reverse 'ip6.arpa.' subtree.
     exclude_subnets: IPv6 subnets that are disallowed in answer.
     """
 
+    enabled: bool = False
     prefix: IPv6Network96 = IPv6Network96("64:ff9b::/96")
-    rev_ttl: Optional[TimeUnit] = None
+    reverse_ttl: Optional[TimeUnit] = None
     exclude_subnets: Optional[List[IPv6Network]] = None
