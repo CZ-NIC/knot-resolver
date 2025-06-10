7ef74d7e0ee19d34b43aefd7aa0596d27e98d01c
Lukáš Ondráček <lukas.ondracek@nic.cz>
2025-06-10 17:39:08 +0200

    lib/cache: count only unique accesses per request in KRU

--- utils/cache_gc/categories.h
+++ utils/cache_gc/categories.h
@@ -8,4 +8,4 @@ typedef uint8_t category_t;
 
 #define CATEGORIES 100		// number of categories
 
-category_t kr_gc_categorize(union kr_cache_top *top, gc_record_info_t * info, void *key, size_t key_len);
+category_t kr_gc_categorize(struct kr_cache_top *top, gc_record_info_t * info, void *key, size_t key_len);
