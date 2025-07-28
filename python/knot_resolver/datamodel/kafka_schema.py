921167da4fd7c2528e09ab87fa8dc3fdc6485d36
Aleš Mrázek <ales.mrazek@nic.cz>
2025-07-28 11:00:30 +0200

    datamodel/kafka_schema.py: added 'files_dir' option
    Directory for storing files received via Kafka.

--- python/knot_resolver/datamodel/kafka_schema.py
+++ python/knot_resolver/datamodel/kafka_schema.py
@@ -1,12 +1,14 @@
 from typing import Literal, Optional
 
-from knot_resolver.constants import KAFKA_LIB
+from knot_resolver.constants import KAFKA_LIB, WORK_DIR
 from knot_resolver.datamodel.types import (
     EscapedStr,
     IPAddressOptionalPort,
     ReadableFile,
+    WritableDir,
 )
 from knot_resolver.utils.modeling import ConfigSchema
+from knot_resolver.utils.modeling.base_schema import lazy_default
 
 
 class KafkaSchema(ConfigSchema):
@@ -16,6 +18,7 @@ class KafkaSchema(ConfigSchema):
     enable: Enable/disable Kafka client.
     topic: Topic to subscribe data from.
     server: Kafka server to connect.
+    files_dir: Directory for storing files received via Kafka.
     security_protocol: Protocol used to communicate with server(broker).
     cert_file: Optional, the client's certificate file in PEM format.
     key_file: Optional, the client's private key file.
@@ -24,7 +27,7 @@ class KafkaSchema(ConfigSchema):
 
     enable: bool = False
     topic: EscapedStr = EscapedStr("knot-resolver")
-    server: IPAddressOptionalPort = IPAddressOptionalPort("127.0.0.1@9092")
+    files_dir: WritableDir = lazy_default(WritableDir, str(WORK_DIR))
     security_protocol: Literal["plaintext", "ssl"] = "plaintext"
     cert_file: Optional[ReadableFile] = None
     key_file: Optional[ReadableFile] = None
