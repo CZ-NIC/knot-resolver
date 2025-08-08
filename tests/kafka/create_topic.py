17697d6ea66405c9aad1c7272fa22094667a5932
Aleš Mrázek <ales.mrazek@nic.cz>
2025-08-08 15:58:44 +0200

    fixup! fixup! fixup! fixup! fixup! fixup! fixup! fixup! .gitlab-ci.yml: added 'python:kafka' job

--- tests/kafka/create_topic.py
+++ tests/kafka/create_topic.py
@@ -1,15 +1,14 @@
 import sys
 
 from kafka.admin import KafkaAdminClient, NewTopic  # type: ignore[import-untyped]
-from kafka.errors import TopicAlreadyExistsError  # type: ignore[import-untyped]
+from kafka.errors import TopicAlreadyExistsError, NodeNotReadyError # type: ignore[import-untyped]
 
 server = sys.argv[1]
 topic = NewTopic(name=sys.argv[2], num_partitions=1, replication_factor=1)
 
 # create topic if not exists
-admin_client = KafkaAdminClient(bootstrap_servers=[server], client_id="client-test")
+admin_client = KafkaAdminClient(bootstrap_servers=[server], api_version=(0, 10, 2))
 try:
-    print(admin_client.describe_cluster())
     admin_client.create_topics(new_topics=[topic], validate_only=False)
     print(f"Topic {topic.name} created Successfully")
     admin_client.close()
