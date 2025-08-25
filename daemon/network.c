bd9e789e672ec3cf3d7fe7de55b1ba6f1f5ad9b3
Frantisek Tobias <frantisek.tobias@nic.cz>
2025-08-25 16:30:52 +0200

    daemon/io: catch fdopen fail, close fd and default back to stdout

--- daemon/network.c
+++ daemon/network.c
@@ -919,8 +919,9 @@ static int clear_bpf_cb(trie_val_t *val, void *ctx)
 			uv_fileno(endpoint->handle, &sockfd);
 		kr_require(sockfd != -1);
 
+		/* appease linter, kernel ignores optval for SO_DETACH_BPF */
 		int no = 0;
-		if (setsockopt(sockfd, SOL_SOCKET, SO_DETACH_BPF, &no, sizeof(no)) != 0) {
+		if (setsockopt(sockfd, SOL_SOCKET, SO_DETACH_BPF, &no, 0) != 0) {
 			kr_log_error(NETWORK, "failed to clear SO_DETACH_BPF socket option\n");
 		}
 		/* Proceed even if setsockopt() failed,
