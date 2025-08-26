51de61cabbdc22d4dfa0c5ba7a9823e917a7a442
Aleš Mrázek <ales.mrazek@nic.cz>
2025-08-26 13:41:51 +0200

    manager/logger.py: added constants for logging targets

--- python/knot_resolver/manager/logger.py
+++ python/knot_resolver/manager/logger.py
@@ -10,7 +10,9 @@ from knot_resolver.manager.config_store import ConfigStore, only_on_real_changes
 
 from .constants import LOGGING_LEVEL_STARTUP
 
-logger = logging.getLogger(__name__)
+STDOUT = "stdout"
+SYSLOG = "syslog"
+STDERR = "stderr"
 
 NOTICE_LEVEL = (logging.WARNING + logging.INFO) // 2
 NOTICE_NAME = "NOTICE"
@@ -33,6 +35,8 @@ _level_to_name = {
     logging.DEBUG: "DEBUG",
 }
 
+logger = logging.getLogger(__name__)
+
 
 def get_log_format(config: KresConfig) -> str:
     """
@@ -44,10 +48,10 @@ def get_log_format(config: KresConfig) -> str:
         return "[%(levelname)s] %(name)s: %(message)s"
     # In this case, we are running standalone during inicialization and we need to add a prefix to each line
     # by ourselves to make it consistent
-    assert config.logging.target != "syslog"
+    assert config.logging.target != SYSLOG
     stream = ""
-    if config.logging.target == "stderr":
-        stream = " (stderr)"
+    if config.logging.target == STDERR:
+        stream = f" ({STDERR})"
 
     pid = os.getpid()
     return f"%(asctime)s manager[{pid}]{stream}: [%(levelname)s] %(name)s: %(message)s"
@@ -73,13 +77,13 @@ async def _set_logging_handler(config: KresConfig) -> None:
         target = "stdout"
 
     handler: logging.Handler
-    if target == "syslog":
+    if target == SYSLOG:
         handler = logging.handlers.SysLogHandler(address="/dev/log")
         handler.setFormatter(logging.Formatter("%(name)s: %(message)s"))
-    elif target == "stdout":
+    elif target == STDOUT:
         handler = logging.StreamHandler(sys.stdout)
         handler.setFormatter(logging.Formatter(get_log_format(config)))
-    elif target == "stderr":
+    elif target == STDERR:
         handler = logging.StreamHandler(sys.stderr)
         handler.setFormatter(logging.Formatter(get_log_format(config)))
     else:
