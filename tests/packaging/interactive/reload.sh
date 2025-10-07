8d862950a68bc9dd4ab1b7b4835eca02b31da84e
Aleš Mrázek <ales.mrazek@nic.cz>
2025-10-07 14:39:06 +0200

    tests/packaging/interactive/reload.sh: improved test for reload and force reload

--- tests/packaging/interactive/reload.sh
+++ tests/packaging/interactive/reload.sh
@@ -2,4 +2,27 @@
 
 set -e
 
+function count_errors(){
+    echo "$(journalctl -u knot-resolver.service | grep -c error)"
+}
+
+function count_apply(){
+    echo "$(journalctl -u knot-resolver.service | grep -c "Config applied successfully to all workers")"
+}
+
+err_count=$(count_errors)
+rel_count=$(count_apply)
+
 kresctl reload
+sleep 6
+if [ $(count_errors) -ne $err_count ] || [ $(count_apply) -ne $rel_count ]; then
+    echo "Failed to reload."
+    exit 1
+fi
+
+kresctl reload --force
+sleep 6
+if [ $(count_errors) -ne $err_count ] || [ $(count_apply) -eq $rel_count ]; then
+    echo "Failed to force a reload."
+    exit 1
+fi
