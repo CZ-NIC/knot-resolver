c8f470625342f535e972e291f34361c9346fe0a2
Aleš Mrázek <ales.mrazek@nic.cz>
2025-10-08 15:50:02 +0200

    manager/triggers.py: force renew/reload

--- python/knot_resolver/manager/triggers.py
+++ python/knot_resolver/manager/triggers.py
@@ -17,6 +17,8 @@ class Triggers:
     def __init__(self, config: KresConfig) -> None:
         self._config = config
 
+        self._reload_force = False
+        self._renew_force = False
         self._renew_timer: Optional[Timer] = None
         self._reload_timer: Optional[Timer] = None
         self._cmd_timers: Dict[str, Timer] = {}
@@ -54,45 +56,60 @@ class Triggers:
         if cmd in self._cmd_timers:
             self._cmd_timers[cmd].cancel()
 
-    def trigger_renew(self) -> None:
+    def trigger_renew(self, force: bool = False) -> None:
         def _renew() -> None:
-            response = request(self._socket, "POST", "renew")
+            response = request(self._socket, "POST", "renew/force" if force else "renew")
             if response.status != 200:
                 logger.error(f"Failed to renew configuration: {response.body}")
             logger.info("Renewing configuration has finished")
+            self._renew_force = False
 
         # do not trigger renew if reload is scheduled
-        if self._reload_timer and self._reload_timer.is_alive():
+        if self._reload_timer and self._reload_timer.is_alive() and self._reload_force >= force:
+            logger.info("Skipping renewing configuration, reload was already triggered")
             return
 
         # skipping if reload was already triggered
         if self._renew_timer and self._renew_timer.is_alive():
-            logger.info("Skipping renewing configuration, it was already triggered")
-            return
-        # start a 5sec timer
+            if self._renew_force >= force:
+                logger.info("Skipping renewing configuration, it was already triggered")
+                return
+            self._renew_timer.cancel()
+            self.renew_force = False
+
         logger.info("Delayed configuration renew has started")
+        # start a 5sec timer
         self._renew_timer = Timer(5, _renew)
         self._renew_timer.start()
+        self._renew_force = force
 
-    def trigger_reload(self) -> None:
+    def trigger_reload(self, force: bool = False) -> None:
         def _reload() -> None:
-            response = request(self._socket, "POST", "reload")
+            response = request(self._socket, "POST", "reload/force" if force else "reload")
             if response.status != 200:
                 logger.error(f"Failed to reload configuration: {response.body}")
             logger.info("Reloading configuration has finished")
+            self._reload_force = False
 
         # cancel renew
-        if self._renew_timer and self._renew_timer.is_alive():
+        if self._renew_timer and self._renew_timer.is_alive() and force >= self._renew_force:
             self._renew_timer.cancel()
+            self._renew_force = False
 
         # skipping if reload was already triggered
         if self._reload_timer and self._reload_timer.is_alive():
-            logger.info("Skipping renewing configuration, it was already triggered")
-            return
-        # start a 5sec timer
+            if self._reload_force >= force:
+                logger.info("Skipping reloading configuration, it was already triggered")
+                return
+            logger.info("Cancelling already scheduled configuration reload, force reload triggered")
+            self._reload_timer.cancel()
+            self._reload_force = False
+
         logger.info("Delayed configuration reload has started")
+        # start a 5sec timer
         self._reload_timer = Timer(5, _reload)
         self._reload_timer.start()
+        self._reload_force = force
 
 
 def trigger_cmd(config: KresConfig, cmd: str) -> None:
@@ -108,15 +125,15 @@ def cancel_cmd(cmd: str) -> None:
         _triggers.cancel_cmd(cmd)
 
 
-def trigger_renew(config: KresConfig) -> None:
+def trigger_renew(config: KresConfig, force: bool = False) -> None:
     global _triggers
     if not _triggers:
         _triggers = Triggers(config)
-    _triggers.trigger_renew()
+    _triggers.trigger_renew(force)
 
 
-def trigger_reload(config: KresConfig) -> None:
+def trigger_reload(config: KresConfig, force: bool = False) -> None:
     global _triggers
     if not _triggers:
         _triggers = Triggers(config)
-    _triggers.trigger_reload()
+    _triggers.trigger_reload(force)
