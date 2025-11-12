ea9e20082510228453d1d224f13c67673d8f55b6
Jakub Ružička <jakub.ruzicka@nic.cz>
2025-11-12 14:11:18 +0100

    distro: update upstream-version.sh to apkg compat 6

--- scripts/lib/upstream-version.sh
+++ scripts/lib/upstream-version.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 # SPDX-License-Identifier: GPL-3.0-or-later
 #
-# return latest upstream version of Knot Resolver
+# Return latest upstream version of Knot Resolver
+#
+# This script is used by apkg to detect latest upstream version, test with:
+#
+#     apkg info upstream-version
+#     apkg get-archive
+#
+# It must only output valid YAML to stdout!
 set -o errexit
 
 REPO=https://gitlab.nic.cz/knot/knot-resolver.git
 
-git ls-remote --tags --refs $REPO | cut -f2- | sed -n "s#^refs/tags/v##p" | sort -V | tail -1
+VERSION=$(git ls-remote --tags --refs $REPO | cut -f2- | sed -n "s#^refs/tags/v##p" | sort -V | tail -1)
+echo "version: $VERSION"
