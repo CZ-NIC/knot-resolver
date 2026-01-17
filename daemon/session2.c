CZ-NIC/knot-resolver

commit 82e1d59687d1b9f291e902763456f0102d937469

daemon udp connect: use connected udp communication

daemon/session2.c
@@ -1447,8 +1447,8 @@ static int session2_transport_pushv(struct session2 *s,
 						ctx);
 				return kr_ok();
 			} else {
-				int ret = uv_udp_try_send((uv_udp_t*)handle,
-						(uv_buf_t *)iov, iovcnt, comm->comm_addr);
+				int ret = uv_udp_try_send((uv_udp_t*)handle, (uv_buf_t *)iov, iovcnt,
+					the_network->enable_connect_udp ? NULL : comm->comm_addr);
 				if (ret > 0) // equals buffer size, only confuses us
 					ret = 0;
 				if (ret == UV_EAGAIN) {

daemon/io.c
@@ -67,7 +67,7 @@ void udp_recv(uv_udp_t *handle, ssize_t nread, const uv_buf_t *buf,
 	if (s->closing || nread <= 0 || comm_addr->sa_family == AF_UNSPEC)
 		return;
 
-	if (s->outgoing) {
+	if (!the_network->enable_connect_udp && s->outgoing) {
 		const struct sockaddr *peer = session2_get_peer(s);
 		if (kr_fails_assert(peer->sa_family != AF_UNSPEC))
 			return;

daemon/lua/kres-gen-33.lua
@@ -590,6 +590,7 @@ struct network {
 		int snd;
 		int rcv;
 	} listen_tcp_buflens;
+	_Bool enable_connect_udp;
 };
 struct args *the_args;
 struct endpoint {

daemon/network.c
@@ -78,6 +78,7 @@ void network_init(uv_loop_t *loop, int tcp_backlog)
 	the_network->tcp.tls_handshake_timeout = TLS_MAX_HANDSHAKE_TIME;
 	the_network->tcp.user_timeout = 1000; // 1s should be more than enough
 	the_network->tcp_backlog = tcp_backlog;
+	the_network->enable_connect_udp = true;
 
 	// On Linux, unset means some auto-tuning mechanism also depending on RAM,
 	// which might be OK default (together with the user_timeout above)

daemon/network.h
@@ -115,6 +115,13 @@ struct network {
 	struct {
 		int snd, rcv;
 	} listen_udp_buflens, listen_tcp_buflens;
+
+	/** Use uv_udp_connect as the transport method for UDP.
+	 * Enabling this increases the total number of syscalls, with a variable
+	 * impact on the time spent processing them, sometimes resulting in
+	 * a slight improvement in syscall processing efficiency.
+	 * Note: This does not necessarily lead to overall performance gains. */
+	bool enable_connect_udp;
 };
 
 /** Pointer to the singleton network state. NULL if not initialized. */

daemon/worker.c
@@ -829,6 +829,17 @@ static int transmit(struct qr_task *task)
 	struct comm_info out_comm = {
 		.comm_addr = (struct sockaddr *)choice
 	};
+
+	if (the_network->enable_connect_udp && session->outgoing && !session->stream) {
+		uv_udp_t *udp = (uv_udp_t *)session2_get_handle(session);
+		int connect_tries = 3;
+
+		do {
+			ret = uv_udp_connect(udp, out_comm.comm_addr);
+		} while (ret == UV_EADDRINUSE && --connect_tries > 0);
+		if (ret < 0)
+			kr_log_error(IO, "Failed to establish udp connection: %s\n", uv_strerror(ret));
+	}
 	ret = qr_task_send(task, session, &out_comm, task->pktbuf);
 	if (ret) {
 		session2_close(session);

commit b66cc821048ee18d6d0224a6c3160b1f927474b8

daemon/session: fix UDP answers without sendmmsg
This is quite a serious issue on non-Linux.
It's a regression from commit 82e1d596, i.e. v6.0.9.

daemon/session2.c
@@ -1521,8 +1521,9 @@ static int session2_transport_pushv(struct session2 *s,
 						ctx);
 				return kr_ok();
 			} else {
+				bool conn = s->outgoing && the_network->enable_connect_udp;
 				int ret = uv_udp_try_send((uv_udp_t*)handle, (uv_buf_t *)iov, iovcnt,
-					the_network->enable_connect_udp ? NULL : comm->comm_addr);
+								conn ? NULL : comm->comm_addr);
 				if (ret > 0) // equals buffer size, only confuses us
 					ret = 0;
 				if (ret == UV_EAGAIN) {