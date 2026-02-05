1652be558085eeb6ad1cc95e228158d99811340f
Aleš Mrázek <ales.mrazek@nic.cz>
2026-02-05 13:58:34 +0100

    python/knot_resolver: start from scratch and remove everything

--- python/knot_resolver/manager/__main__.py
+++
@@ -1,4 +0,0 @@
-from knot_resolver.manager.main import main
-
-if __name__ == "__main__":
-    main()
--- python/knot_resolver/utils/modeling/base_generic_type_wrapper.py
+++
@@ -1,9 +0,0 @@
-from typing import Generic, TypeVar
-
-from .base_value_type import BaseTypeABC
-
-T = TypeVar("T")
-
-
-class BaseGenericTypeWrapper(Generic[T], BaseTypeABC):  # pylint: disable=abstract-method
-    pass
