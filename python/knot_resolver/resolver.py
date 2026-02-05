07732fa828e600d931b27f910a12bf76e039ca48
Aleš Mrázek <ales.mrazek@nic.cz>
2026-02-05 14:42:10 +0100

    python/knot_resolver: new basic structure

---
+++ python/knot_resolver/resolver.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+async def start_resolver(config: list[str]) -> int:
+    logger.info("Starting Knot Resolver...")
+
+    return 0
