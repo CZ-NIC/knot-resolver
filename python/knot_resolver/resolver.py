e9b100b22634f18a980d794d0392a84b6c4ebba9
Aleš Mrázek <ales.mrazek@nic.cz>
2026-02-06 15:41:21 +0100

    fixup! fixup! python/knot_resolver: new basic structure

--- python/knot_resolver/resolver.py
+++ python/knot_resolver/resolver.py
@@ -1,11 +1,33 @@
 from __future__ import annotations
 
 import logging
+from pathlib import Path
+
+from .controller import SupervisordController
+from .controller.errors import ControllerError
+from .utils.modeling.errors import DataModelingError
 
 logger = logging.getLogger(__name__)
 
 
-async def start_resolver(config: list[str]) -> int:
-    logger.info("Starting Knot Resolver...")
+def start_resolver(config: list[str]) -> int:
+    logger.notice("Starting Knot Resolver...")
 
-    return 0
+    try:
+        # Ensure that configuration files paths do not change
+        # even when the working directory is changed.
+        config_paths = [Path(file).absolute() for file in config]
+
+        logger.info("Loading configuration...")
+
+        # load configuration here
+
+        controller = SupervisordController()
+        controller.initialize()
+        controller.exec()
+
+    except DataModelingError as e:
+        logger.exception("")
+
+    except ControllerError as e:
+        logger.critical("")
