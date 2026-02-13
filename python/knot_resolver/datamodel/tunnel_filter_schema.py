449179bea3712d51227cd06cb113b127bdf510b3
Aleš Mrázek <ales.mrazek@nic.cz>
2026-02-13 13:45:44 +0100

    python: datamodel: tunnel_filter: fixed schema and description

--- python/knot_resolver/datamodel/tunnel_filter_schema.py
+++ python/knot_resolver/datamodel/tunnel_filter_schema.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from knot_resolver.datamodel.types import IDPattern, ReadableFile, Int0_512
+from knot_resolver.datamodel.types import IDPattern, Percent, ReadableFile
 from knot_resolver.utils.modeling import ConfigSchema
 
 
@@ -11,13 +11,15 @@ class TunnelFilterSchema(ConfigSchema):
     ---
     enable: enable/disable this filtering
     file: path to the neural network to be used
+    sensitivity: deep inspection sensitivity to domain name length
+    threshold: neural network inference threshold in percentage
     tags: set of tags when to apply the filtering (same as in other local-data)
     """
 
     enable: bool = False
     file: Optional[ReadableFile] = None
-    sensitivity: Optional[Int0_512] = 10
-    threshold: Optional[Int0_512] = 95
+    sensitivity: Percent = Percent(10)
+    threshold: Percent = Percent(95)
     tags: Optional[List[IDPattern]] = None
 
     def _validate(self) -> None:
