f0ecf46201c4d48873e91e1e850bbdf25729b537
Aleš Mrázek <ales.mrazek@nic.cz>
2026-02-26 14:50:09 +0100

    python: modeling: types: added integer and float types

--- python/knot_resolver/utils/modeling/types/__init__.py
+++ python/knot_resolver/utils/modeling/types/__init__.py
@@ -1,5 +1,23 @@
 from .base_generic_custom_types import ListOrItem
+from .float_types import FloatNonNegative
+from .integer_types import (
+    Integer0_32,
+    Integer0_512,
+    Integer0_65535,
+    IntegerNonNegative,
+    IntegerPositive,
+    Percent,
+    PortNumber,
+)
 
 __all__ = [
+    "FloatNonNegative",
+    "Integer0_32",
+    "Integer0_512",
+    "Integer0_65535",
+    "IntegerNonNegative",
+    "IntegerPositive",
     "ListOrItem",
+    "Percent",
+    "PortNumber",
 ]
