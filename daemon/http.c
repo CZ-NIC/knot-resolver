e6de5ceece8bd5fd91d306a7612f292f643935bd
Vladimír Čunát <vladimir.cunat@nic.cz>
2026-03-26 12:56:44 +0100

    daemon/http.c nit: fix const-warnings with C23
    Interestingly, with C23, functions like strstr()
    return a const-qualified pointer iff one was passed to them.
    Right now we ran into this in Arch CI (unreleased gcc 15 version):
    https://gitlab.nic.cz/knot/knot-resolver/-/jobs/1713767

--- daemon/http.c
+++ daemon/http.c
@@ -121,7 +121,7 @@ static int check_uri(const char* path)
 	if (!path)
 		return kr_error(EINVAL);
 
-	char *query_mark = strstr(path, "?");
+	const char *query_mark = strstr(path, "?");
 
 	/* calculating of endpoint_len - for POST or GET method */
 	endpoint_len = (query_mark) ? query_mark - path - 1 : strlen(path) - 1;
@@ -167,16 +167,16 @@ static int process_uri_path(struct pl_http_sess_data *ctx, const char* path, int
 
 	static const char key[] = "dns=";
 	static const char *delim = "&";
-	char *beg, *end;
+	const char *beg, *end;
 	uint8_t *dest;
 	uint32_t remaining;
 
-	char *query_mark = strstr(path, "?");
+	const char *query_mark = strstr(path, "?");
 	if (!query_mark || strlen(query_mark) == 0) /* no parameters in path */
 		return kr_error(EINVAL);
 
 	/* go over key:value pair */
-	for (beg = strtok(query_mark + 1, delim); beg != NULL; beg = strtok(NULL, delim)) {
+	for (beg = strtok((char *)query_mark + 1, delim); beg != NULL; beg = strtok(NULL, delim)) {
 		if (!strncmp(beg, key, 4)) /* dns variable in path found */
 			break;
 	}
