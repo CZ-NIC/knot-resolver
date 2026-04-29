74a27684ffee814481cec0f5814091ca0ecce243
Hynek Šabacký <hynek.sabacky@nic.cz>
2026-04-29 12:06:14 +0200

    tunnel: add an auto-detect option for a new character level neural network model

--- modules/dns_tunnel_filter/libblcnn.h
+++ modules/dns_tunnel_filter/libblcnn.h
@@ -6,10 +6,12 @@ extern "C" {
 #endif
 
 #include <stddef.h>
+#include <stdint.h>
 
 typedef void* TorchModule;
 
 TorchModule load_model(const char *nn_file);
+uint8_t get_model_version(TorchModule model);
 float predict_packet(TorchModule model, const unsigned char *data, size_t size);
 void free_model(TorchModule model);
 
