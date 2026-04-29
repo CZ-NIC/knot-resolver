5f83f18fbda2558ad12357e7935efae3736785f0
Hynek Šabacký <hynek.sabacky@nic.cz>
2026-04-29 19:32:42 +0200

    tunnel: make parameters for hit detection configurable

--- python/knot_resolver/datamodel/tunnel_filter_schema.py
+++ python/knot_resolver/datamodel/tunnel_filter_schema.py
@@ -1,6 +1,6 @@
 from typing import List, Optional
 
-from knot_resolver.datamodel.types import IDPattern, Percent, ReadableFile
+from knot_resolver.datamodel.types import IDPattern, Int0_65535, Percent, ReadableFile
 from knot_resolver.utils.modeling import ConfigSchema
 
 
@@ -13,6 +13,8 @@ class TunnelFilterSchema(ConfigSchema):
     file: path to the neural network to be used
     sensitivity: deep inspection sensitivity to domain name length
     threshold: neural network inference threshold in percentage
+    hitms: domain name hash table item expiry time
+    hitcap: number of malicious queries from SLD needed for automatic blacklisting
     tags: set of tags when to apply the filtering (same as in other local-data)
     """
 
@@ -20,6 +22,8 @@ class TunnelFilterSchema(ConfigSchema):
     file: Optional[ReadableFile] = None
     sensitivity: Percent = Percent(10)
     threshold: Percent = Percent(95)
+    hitms: Int0_65535 = Int0_65535(60000)
+    hitcap: Int0_65535 = Int0_65535(10)
     tags: Optional[List[IDPattern]] = None
 
     def _validate(self) -> None:
