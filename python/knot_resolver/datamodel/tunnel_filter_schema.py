b0701189488987d67b1416d624c191c536dab01f
Hynek Šabacký <hynek.sabacky@nic.cz>
2026-05-21 14:58:05 +0200

    tunnel: migrate configurations rpz_builder variables to a seperate object

--- python/knot_resolver/datamodel/tunnel_filter_schema.py
+++ python/knot_resolver/datamodel/tunnel_filter_schema.py
@@ -4,6 +4,23 @@ from knot_resolver.datamodel.types import IDPattern, Int0_65535, Percent, Readab
 from knot_resolver.utils.modeling import ConfigSchema
 
 
+class RpzBuilderSchema(ConfigSchema):
+    """
+    Create an RPZ from domains, which are frequently classified as tunneling.
+
+    ---
+    time_window_ms: time between subsequent tunneling classifications of the same registrable domain
+    threshold: number of malicious queries from a registrable domain needed for automatic blacklisting
+    add_tags: set of tags when to add the RPZ record for the registrable domain
+    rpz_tags: set of tags when to apply the filtering using the created RPZ
+    """
+
+    time_window_ms: Int0_65535 = Int0_65535(60000)
+    threshold: Int0_65535 = Int0_65535(10)
+    add_tags: Optional[List[IDPattern]] = None
+    rpz_tags: Optional[List[IDPattern]] = None
+
+
 class TunnelFilterSchema(ConfigSchema):
     """
     Block suspected attempts of data exfiltration via DNS tunneling.
@@ -13,20 +30,16 @@ class TunnelFilterSchema(ConfigSchema):
     file: path to the neural network to be used
     sensitivity: deep inspection sensitivity to domain name length
     threshold: neural network inference threshold in percentage
-    hit_time_window_ms: domain name hash table item expiry time
-    hit_threshold: number of malicious queries from SLD needed for automatic blacklisting
     tags: set of tags when to apply the filtering (same as in other local-data)
-    rpz_tags: set of tags when to apply the filtering for RPZ (same as in other local-data)
+    rpz_builder: Create an RPZ from domains, which are frequently classified as tunneling
     """
 
     enable: bool = False
     file: Optional[ReadableFile] = None
     sensitivity: Percent = Percent(10)
     threshold: Percent = Percent(95)
-    hit_time_window_ms: Int0_65535 = Int0_65535(60000)
-    hit_threshold: Int0_65535 = Int0_65535(10)
     tags: Optional[List[IDPattern]] = None
-    rpz_tags: Optional[List[IDPattern]] = None
+    rpz_builder: RpzBuilderSchema = RpzBuilderSchema()
 
     def _validate(self) -> None:
         if not self.enable:
