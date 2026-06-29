ea7b6fd6f98bfd70fc09a6863c1c4996f6eb3a9a
Aleš Mrázek <ales.mrazek@nic.cz>
2026-06-30 00:44:17 +0200

    python/knot_resolver: new architecture

---
+++ tests/python/knot_resolver/test_knot_resolver.py
@@ -0,0 +1,10 @@
+from pathlib import Path
+
+from knot_resolver import __version__
+
+
+def test_version() -> None:
+    with Path("VERSION").open() as version_file:
+        version = version_file.read().strip()
+
+    assert __version__ == version
