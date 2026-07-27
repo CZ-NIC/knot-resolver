CZ-NIC/knot-resolver

commit 29b55ceb426efaafcc955181637072a36a99486e

restrict RRSIGs claiming wildcard reach out of the zone [QZKF-005]
This issue was reported by Qifan Zhang from Palo Alto Networks.

Nit: this assignments of `ret` aren't important,
but they might be a bit useful when debugging.

lib/cache/api.c
@@ -556,15 +556,37 @@ static ssize_t stash_rrset(struct kr_cache *cache, const struct kr_query *qry,
 		return kr_error(EINVAL);
 
 	int ret = kr_ok();
-	if (rrset_has_min_range_or_weird(rr, qry))
+	if (rrset_has_min_range_or_weird(rr, qry)) {
+		ret = kr_error(ERANGE);
 		goto return_needs_pkt;
+	}
+
 	const int wild_labels = rr_sigs == NULL ? 0 :
 	       knot_dname_labels(rr->owner, NULL) - knot_rrsig_labels(rr_sigs->rrs.rdata);
-	if (wild_labels < 0)
+	if (wild_labels < 0) {
+		ret = kr_error(ERANGE);
 		goto return_needs_pkt;
+	}
 	const knot_dname_t *encloser = rr->owner; /**< the closest encloser name */
 	for (int i = 0; i < wild_labels; ++i) {
 		encloser = knot_dname_next_label(encloser);
+	}
+
+	/* Sanity-check that encloser didn't get too short,
+	 * in particular not outside the zone which signed it.
+	 * As we don't know which RRSIG succeeded, we take the maximum to be safe. */
+	if (rr_sigs && wild_labels) {
+		int signer_labels = 0;
+		const int count = rr_sigs->rrs.count;
+		knot_rdata_t *rd = rr_sigs->rrs.rdata;
+		for (int i = 0; i < count; ++i, rd = knot_rdataset_next(rd)) {
+			int l = knot_dname_labels(knot_rrsig_signer_name(rd), NULL);
+			signer_labels = MAX(signer_labels, l);
+		}
+		if (knot_dname_labels(encloser, NULL) < signer_labels) {
+			ret = kr_error(ERANGE);
+			goto return_needs_pkt;
+		}
 	}
 
 	/* Construct the key under which RRs will be stored,

lib/dnssec.c
@@ -88,6 +88,11 @@ static int validate_rrsig_rr(int *flags, int cov_labels,
 		if (rrsig_labels < cov_labels) {
 			*flags |= FLG_WILDCARD_EXPANSION;
 		}
+		/* Also refuse if RRSIG claims rrsig_labels outside the zone. */
+		if (rrsig_labels < knot_dname_labels(signer_name, NULL)) {
+			vctx->rrs_counters.labels_invalid++;
+			return kr_error(EINVAL);
+		}
 	}
 
 	/* bullet 7

commit b9327b8fab7f3e8b4e60dde2c146daa30e9fb80f

lib/cache: fix excessive logging regression
Regressed in 29b55ceb426efaa and annoyingly logging e.g.:
> [cache ] [35589.02] stash failed, ret = 1

Example QNAME to trigger this: foo.bar.cloudflare.net

lib/cache/api.c
@@ -556,15 +556,13 @@ static ssize_t stash_rrset(struct kr_cache *cache, const struct kr_query *qry,
 		return kr_error(EINVAL);
 
 	int ret = kr_ok();
-	if (rrset_has_min_range_or_weird(rr, qry)) {
-		ret = kr_error(ERANGE);
+	if (rrset_has_min_range_or_weird(rr, qry))
 		goto return_needs_pkt;
-	}
 
 	const int wild_labels = rr_sigs == NULL ? 0 :
 	       knot_dname_labels(rr->owner, NULL) - knot_rrsig_labels(rr_sigs->rrs.rdata);
 	if (wild_labels < 0) {
-		ret = kr_error(ERANGE);
+		VERBOSE_MSG(qry, "=> invalid wildcard labels %d\n", wild_labels);
 		goto return_needs_pkt;
 	}
 	const knot_dname_t *encloser = rr->owner; /**< the closest encloser name */
@@ -584,7 +582,7 @@ static ssize_t stash_rrset(struct kr_cache *cache, const struct kr_query *qry,
 			signer_labels = MAX(signer_labels, l);
 		}
 		if (knot_dname_labels(encloser, NULL) < signer_labels) {
-			ret = kr_error(ERANGE);
+			VERBOSE_MSG(qry, "=> invalid wildcard labels %d\n", wild_labels);
 			goto return_needs_pkt;
 		}
 	}