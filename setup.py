d4f004a4aadec9fbefa65269c3e67e2d78b9c390
Aleš Mrázek <ales.mrazek@nic.cz>
2026-08-18 10:42:09 +0200

    pyproject: add script for the manager and regenerate setup.py

--- setup.py
+++ setup.py
@@ -50,8 +50,9 @@ extras_require = \
  'watchdog': ['watchdog']}
 
 entry_points = \
-{'console_scripts': ['kresctl = knot_resolver.client.main:main',
-                     'knot-resolver = knot_resolver.manager.main:main']}
+{'console_scripts': ['knot-resolver = knot_resolver.main:main',
+                     'kres-manager = knot_resolver.manager.main:main',
+                     'kresctl = knot_resolver.client.main:main']}
 
 setup_kwargs = {
     'name': 'knot-resolver',
