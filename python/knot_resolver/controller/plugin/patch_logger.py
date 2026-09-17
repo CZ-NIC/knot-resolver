b297d5db05859783ba27c01a77be444ebe1a0f1c
Aleš Mrázek <ales.mrazek@nic.cz>
2026-09-17 23:26:50 +0200

    python: controller: plugin: improve logger patch

--- python/knot_resolver/controller/plugin/patch_logger.py
+++ python/knot_resolver/controller/plugin/patch_logger.py
@@ -1,93 +1,117 @@
-# type: ignore
-# pylint: disable=protected-access
+# mypy: disable-error-code=import-untyped
+
+from __future__ import annotations
 
 import os
 import sys
 import traceback
-from typing import Any, Literal
+from dataclasses import dataclass
+from typing import TYPE_CHECKING, Literal, cast
 
 from supervisor.dispatchers import POutputDispatcher
 from supervisor.loggers import LevelsByName, StreamHandler, SyslogHandler
-from supervisor.supervisord import Supervisor
 
-FORWARD_LOG_LEVEL = LevelsByName.CRIT  # to make sure it's always printed
+if TYPE_CHECKING:
+    from collections.abc import Callable
+
+    from supervisor.supervisord import Supervisor
+
+    SupervisordLogLevel = Literal["CRIT", "ERRO", "WARN", "INFO", "DEBG"]
+
+SupervisordLogTarget = Literal["stdout", "stderr", "syslog"]
+
+SERVICE_NAME = "supervisord"
+FORWARD_LOGGING_LEVEL = LevelsByName.CRIT
+FORWARD_LOGGING_FORMAT = "[%(pid)d]%(stream)s: %(data)s"
 
 
-def empty_function(*args, **kwargs):
-    pass
+@dataclass(frozen=True)
+class LoggingPatchConfig:
+    logtarget: SupervisordLogTarget
 
 
-FORWARD_MSG_FORMAT: str = "%(name)s[%(pid)d]%(stream)s: %(data)s"
+def _make_p_output_dispatcher_log(
+    supervisord_handlers: list[StreamHandler], forward_handlers: list[StreamHandler]
+) -> Callable[[POutputDispatcher, bytes], None]:
+    def _p_output_dispatcher_log(self: POutputDispatcher, data: bytes) -> None:
+        """Forward subprocess output to the selected logging backend."""
+        if not data:
+            return
 
+        try:
+            text = data.decode("utf-8")
+        except UnicodeDecodeError:
+            text = f"Undecodable log data: {data!r}"
 
-def p_output_dispatcher_log(self: POutputDispatcher, data: bytearray):
-    if data:
-        # parse the input
-        if not isinstance(data, bytes):
-            text = data
-        else:
-            try:
-                text = data.decode("utf-8")
-            except UnicodeDecodeError:
-                text = "Undecodable: %r" % data
-
-        # print line by line prepending correct prefix to match the style
         config = self.process.config
+        # set logger handlers for forwarding
         config.options.logger.handlers = forward_handlers
+
+        service = config.name
+        pid = self.process.pid
+
+        stream = ""
+        if self.channel == "stderr":
+            stream = " (stderr)"
+
+        # log every line with correct format
         for line in text.splitlines():
-            stream = ""
-            if self.channel == "stderr":
-                stream = " (stderr)"
             config.options.logger.log(
-                FORWARD_LOG_LEVEL, FORWARD_MSG_FORMAT, name=config.name, stream=stream, data=line, pid=self.process.pid
+                FORWARD_LOGGING_LEVEL,
+                "%(service)s[%(pid)d]%(stream)s: %(line)s",
+                service=service,
+                pid=pid,
+                stream=stream,
+                line=line,
             )
+        # revert to the original logger handlers
         config.options.logger.handlers = supervisord_handlers
 
+    return _p_output_dispatcher_log
 
-def _create_handler(fmt, level, target: Literal["stdout", "stderr", "syslog"]) -> StreamHandler:
-    if target == "syslog":
-        handler = SyslogHandler()
-    else:
-        handler = StreamHandler(sys.stdout if target == "stdout" else sys.stderr)
-        handler.setFormat(fmt)
-        handler.setLevel(level)
+
+def _create_logger_handler(fmt: str, level: SupervisordLogLevel, config: LoggingPatchConfig) -> StreamHandler:
+    logtarget = config.logtarget
+
+    if logtarget == "syslog":
+        return SyslogHandler()
+    handler = StreamHandler(sys.stderr) if logtarget == "stderr" else StreamHandler(sys.stdout)
+
+    handler.setFormat(fmt)
+    handler.setLevel(level)
     return handler
 
 
-supervisord_handlers = []
-forward_handlers = []
+def inject(supervisord: Supervisor, **config_dict: object) -> None:
+    logtarget = config_dict.get("logtarget")
 
+    if logtarget not in ("stdout", "stderr", "syslog"):
+        msg = f"Unknown 'logtarget' configuration: {logtarget!r}"
+        raise ValueError(msg)
 
-def inject(supervisord: Supervisor, **config: Any) -> Any:  # pylint: disable=useless-return
+    config = LoggingPatchConfig(
+        logtarget=cast("SupervisordLogTarget", logtarget),
+    )
+    pid = os.getpid()
+
     try:
-        # reconfigure log handlers
-        supervisord.options.logger.info("reconfiguring log handlers")
+        supervisord_handlers: list[StreamHandler] = []
         supervisord_handlers.append(
-            _create_handler(
-                f"%(asctime)s supervisor[{os.getpid()}]: [%(levelname)s] %(message)s\n",
+            _create_logger_handler(
+                f"%(asctime)s {SERVICE_NAME}[{pid}]: [%(levelname)s] %(message)s\n",
                 supervisord.options.loglevel,
-                config["target"],
-            )
+                config,
+            ),
         )
+        forward_handlers: list[StreamHandler] = []
         forward_handlers.append(
-            _create_handler("%(asctime)s %(message)s\n", supervisord.options.loglevel, config["target"])
+            _create_logger_handler("%(asctime)s %(message)s\n", supervisord.options.loglevel, config)
         )
+
         supervisord.options.logger.handlers = supervisord_handlers
 
         # replace output handler for subprocesses
-        POutputDispatcher._log = p_output_dispatcher_log  # noqa: SLF001
-
-        # we forward stdio in all cases, even when logging to syslog. This should prevent the unforturtunate
-        # case of swallowing an error message leaving the users confused. To make the forwarded lines obvious
-        # we just prepend a explanatory string at the beginning of all messages
-        if config["target"] == "syslog":
-            global FORWARD_MSG_FORMAT
-            FORWARD_MSG_FORMAT = "captured stdio output from " + FORWARD_MSG_FORMAT
-
-        # this method is called by supervisord when loading the plugin,
-        # it should return XML-RPC object, which we don't care about
-        # That's why why are returning just None
-        return None
+        POutputDispatcher._log = _make_p_output_dispatcher_log(supervisord_handlers, forward_handlers)
 
     # if we fail to load the module, print some explanation
     # should not happen when run by endusers
