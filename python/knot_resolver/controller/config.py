df6b98e85e023ca9a973b58dbfdc684b61114842
Aleš Mrázek <ales.mrazek@nic.cz>
2026-09-18 15:29:27 +0200

    python: controller: improve supervisord config

--- python/knot_resolver/controller/config.py
+++ python/knot_resolver/controller/config.py
@@ -1,227 +1,162 @@
-import asyncio
-import logging
-import os
+from __future__ import annotations
+
 import shutil
 from dataclasses import dataclass
-from importlib.resources import files
 from pathlib import Path
-from typing import Literal
+from typing import TYPE_CHECKING, Literal
 
-from jinja2 import Template
-
-from knot_resolver.args import KresArgs
-from knot_resolver.constants import (
-    CACHE_GC_EXECUTABLE,
-    DAEMON_EXECUTABLE,
-    MANAGER_EXECUTABLE,
-    NOTIFY_SUPPORT,
-)
-from knot_resolver.controller.interface import KresID, SubprocessType
-from knot_resolver.datamodel.config_schema import KresConfig, workers_max_count
-from knot_resolver.datamodel.logging_schema import LogTargetEnum
+from knot_resolver.constants import CACHE_GC_EXECUTABLE, DAEMON_EXECUTABLE, MANAGER_EXECUTABLE, NOTIFY_SUPPORT
 from knot_resolver.logging import NO_PREFIX_FORMAT_ENV_VAR
-from knot_resolver.manager.constants import (
-    kres_cache_dir,
-    kresd_config_file_supervisord_pattern,
-    policy_loader_config_file,
-    supervisord_config_file,
-    supervisord_config_file_tmp,
-    supervisord_pid_file,
-    supervisord_sock_file,
-    supervisord_subprocess_log_dir,
-)
-from knot_resolver.utils.async_utils import writefile
 
-logger = logging.getLogger(__name__)
+if TYPE_CHECKING:
+    from knot_resolver.args import KresArgs
+    from knot_resolver.datamodel import KresConfig
+    from knot_resolver.datamodel.logging_schema import LogLevelEnum
+
+SupervisordLogLevel = Literal["critical", "error", "warn", "info", "debug", "trace", "blather"]
+SupervisordLogTarget = Literal["stdout", "stderr", "syslog"]
+
+SUPERVISORD_SOCKET_NAME = "supervisord.sock"
+SUPERVISORD_PIDFILE_NAME = "supervisord.pid"
+SUPERVISORD_CONFIGFILE_NAME = "supervisord.conf"
+SUPERVISORD_CONFIGFILE_NAME_TMP = f"{SUPERVISORD_CONFIGFILE_NAME}.tmp"
+WORKER_CONFIGFILE_NAME = "worker%(process_num)d.conf"
+LOADER_CONFIGFILE_NAME = "loader.conf"
+
+X_TYPE_VAR_NAME = "X-SUPERVISORD-TYPE"
+X_TYPE_NOTIFY = "notify"
+INSTANCE_VAR_NAME = "SYSTEMD_INSTANCE"
+ENVIRONMENT_TYPE_NOTIFY = f"{X_TYPE_VAR_NAME}={X_TYPE_NOTIFY}"
+ENVIRONMENT_INSTANCE_NUM = f'{INSTANCE_VAR_NAME}="%(process_num)d"'
+
+LOGLEVEL_MAP: dict[LogLevelEnum, SupervisordLogLevel] = {
+    "crit": "critical",
+    "err": "error",
+    "warning": "warn",
+    "notice": "warn",
+    "info": "info",
+    "debug": "debug",
+}
 
 
-class SupervisordKresID(KresID):
-    # WARNING: be really careful with renaming. If the naming schema is changing,
-    # we should be able to parse the old one as well, otherwise updating manager will
-    # cause weird behavior
+@dataclass(frozen=True)
+class SupervisordConfig:
+    workdir: Path
+    pidfile: Path
+    loglevel: SupervisordLogLevel
+    logtarget: SupervisordLogTarget
+    unix_http_server: Path
 
     @staticmethod
-    def from_string(val: str) -> "SupervisordKresID":
-        # the double name is checked because thats how we read it from supervisord
-        if val in ("cache-gc", "cache-gc:cache-gc"):
-            return SupervisordKresID.new(SubprocessType.GC, 0)
-        if val in ("policy-loader", "policy-loader:policy-loader"):
-            return SupervisordKresID.new(SubprocessType.POLICY_LOADER, 0)
-        val = val.replace("kresd:kresd", "")
-        return SupervisordKresID.new(SubprocessType.KRESD, int(val))
-
-    def __str__(self) -> str:
-        if self.subprocess_type is SubprocessType.GC:
-            return "cache-gc"
-        if self.subprocess_type is SubprocessType.POLICY_LOADER:
-            return "policy-loader"
-        if self.subprocess_type is SubprocessType.KRESD:
-            return f"kresd:kresd{self._id}"
-        raise RuntimeError(f"Unexpected subprocess type {self.subprocess_type}")
+    def create(_args: KresArgs, config: KresConfig) -> SupervisordConfig:
+        return SupervisordConfig(
+            workdir=Path.cwd().absolute(),
+            pidfile=Path(SUPERVISORD_PIDFILE_NAME).absolute(),
+            loglevel=LOGLEVEL_MAP[config.logging.level],
+            logtarget=config.logging.target,
+            unix_http_server=Path(SUPERVISORD_SOCKET_NAME).absolute(),
+        )
 
 
-def kres_cache_gc_args(config: KresConfig) -> str:
-    args = ""
-
-    if config.logging.level == "debug" or (config.logging.groups and "cache-gc" in config.logging.groups):
-        args += " -v"
-
-    gc_config = config.cache.garbage_collector
-    args += (
-        f" -d {gc_config.interval.millis()}"
-        f" -u {gc_config.threshold}"
-        f" -f {gc_config.release}"
-        f" -l {gc_config.rw_deletes}"
-        f" -L {gc_config.rw_reads}"
-        f" -t {gc_config.temp_keys_space.mbytes()}"
-        f" -m {gc_config.rw_duration.micros()}"
-        f" -w {gc_config.rw_delay.micros()}"
-    )
-    if gc_config.dry_run:
-        args += " -n"
-    return args
-
-
-@dataclass
-class ProcessTypeConfig:
-    """Data structure holding data for supervisord config template."""
-
-    logfile: Path
-    workdir: str
+@dataclass(frozen=True)
+class SubprocessConfig:
     command: str
-    startsecs: int
-    environment: str
+    workdir: Path
+    startsecs: int = 0
     max_procs: int = 1
+    environment: str = ""
 
     @staticmethod
-    def create_gc_config(config: KresConfig) -> "ProcessTypeConfig":
-        cwd = str(os.getcwd())
-        return ProcessTypeConfig(  # type: ignore[call-arg]
-            logfile=supervisord_subprocess_log_dir(config) / "gc.log",
-            workdir=cwd,
-            command=f"{CACHE_GC_EXECUTABLE} -c {kres_cache_dir(config)}{kres_cache_gc_args(config)}",
-            startsecs=0,
-            environment="",
+    def create_manager(args: KresArgs, _config: KresConfig) -> SubprocessConfig:
+        startsecs = 0
+        environment = f"{NO_PREFIX_FORMAT_ENV_VAR}=true"
+
+        if NOTIFY_SUPPORT:
+            startsecs = 600
+            environment += f",{ENVIRONMENT_TYPE_NOTIFY}"
+
+        command_args: tuple[str, ...] = (str(MANAGER_EXECUTABLE),)
+        if not MANAGER_EXECUTABLE.exists():
+            command_args = (
+                str(shutil.which("python3")),
+                "-m",
+                "knot_resolver.manager",
+            )
+
+        command_args += (
+            "--logtarget",
+            args.logtarget,
+            "--loglevel",
+            args.loglevel,
+            "--config",
+            *map(str, args.config),
+        )
+
+        return SubprocessConfig(
+            command=" ".join(command_args),
+            workdir=Path.cwd().absolute(),
+            environment=environment,
+            startsecs=startsecs,
         )
 
     @staticmethod
-    def create_policy_loader_config(config: KresConfig) -> "ProcessTypeConfig":
-        cwd = str(os.getcwd())
-        return ProcessTypeConfig(  # type: ignore[call-arg]
-            logfile=supervisord_subprocess_log_dir(config) / "policy-loader.log",
-            workdir=cwd,
-            command=f"{DAEMON_EXECUTABLE} -c {(policy_loader_config_file(config))} -c - -n",
-            startsecs=0,
-            environment="",
-        )
-
-    @staticmethod
-    def create_kresd_config(config: KresConfig) -> "ProcessTypeConfig":
-        cwd = str(os.getcwd())
-        environment = 'SYSTEMD_INSTANCE="%(process_num)d"'
+    def create_worker(_args: KresArgs, _config: KresConfig) -> SubprocessConfig:
+        max_procs = 1
 
         # Default for non-Linux systems without support for systemd NOTIFY message.
         # Therefore, we need to give the kresd workers a few seconds to start properly.
+        environment = ENVIRONMENT_INSTANCE_NUM
         startsecs = 3
 
         if NOTIFY_SUPPORT:
             # There is support for systemd NOTIFY message.
-            # Here, 'startsecs' serves as a timeout for waiting for NOTIFY message.
+            # Here, 'startsecs' serves as a timeout for waiting for notify message.
+            environment += f",{ENVIRONMENT_TYPE_NOTIFY}"
             startsecs = 60
-            environment += ",X-SUPERVISORD-TYPE=notify"
 
-        return ProcessTypeConfig(  # type: ignore[call-arg]
-            logfile=supervisord_subprocess_log_dir(config) / "kresd%(process_num)d.log",
-            workdir=cwd,
-            command=f"{DAEMON_EXECUTABLE} -c {kresd_config_file_supervisord_pattern(config)} -n",
-            startsecs=startsecs,
-            environment=environment,
-            max_procs=int(workers_max_count()) + 1,  # +1 for the canary process
-        )
+        config_path = Path(WORKER_CONFIGFILE_NAME).absolute()
+        command_args: tuple[str, ...] = (str(DAEMON_EXECUTABLE), "--config", str(config_path), "-n")
 
-    @staticmethod
-    def create_manager_config(args: KresArgs, _config: KresConfig) -> "ProcessTypeConfig":
-        if MANAGER_EXECUTABLE.exists():
-            command_args = [str(MANAGER_EXECUTABLE)]
-        else:
-            command_args = [
-                str(shutil.which("python3")),
-                "-m",
-                "knot_resolver.manager",
-            ]
-
-        if args:
-            command_args += [
-                "--logtarget",
-                args.logtarget,
-                "--loglevel",
-                args.loglevel,
-                "--config",
-                *map(str, args.config),
-            ]
-
-        environment = f"{NO_PREFIX_FORMAT_ENV_VAR}=true"
-        if NOTIFY_SUPPORT:
-            environment += ",X-SUPERVISORD-TYPE=notify"
-
-        cwd = str(os.getcwd())
-        return ProcessTypeConfig(  # type: ignore[call-arg]
-            workdir=cwd,
+        return SubprocessConfig(
             command=" ".join(command_args),
-            startsecs=600 if NOTIFY_SUPPORT else 0,
+            workdir=Path.cwd().absolute(),
             environment=environment,
-            logfile=Path(""),  # this will be ignored
+            startsecs=startsecs,
+            max_procs=max_procs,
         )
-
-
-@dataclass
-class SupervisordConfig:
-    unix_http_server: Path
-    pid_file: Path
-    workdir: str
-    logfile: Path
-    loglevel: Literal["critical", "error", "warn", "info", "debug", "trace", "blather"]
-    target: LogTargetEnum
-    notify_support: bool
 
     @staticmethod
-    def create(config: KresConfig) -> "SupervisordConfig":
-        # determine the correct logging level
-        if config.logging.groups and "supervisord" in config.logging.groups:
-            loglevel = "info"
-        else:
-            loglevel = {
-                "crit": "critical",
-                "err": "error",
-                "warning": "warn",
-                "notice": "warn",
-                "info": "info",
-                "debug": "debug",
-            }[config.logging.level]
-        cwd = str(os.getcwd())
-        return SupervisordConfig(  # type: ignore[call-arg]
-            unix_http_server=supervisord_sock_file(config),
-            pid_file=supervisord_pid_file(config),
-            workdir=cwd,
-            logfile=Path("syslog" if config.logging.target == "syslog" else "/dev/null"),
-            loglevel=loglevel,  # type: ignore[arg-type]
-            target=config.logging.target,
-            notify_support=NOTIFY_SUPPORT,
+    def create_loader(_args: KresArgs, _config: KresConfig) -> SubprocessConfig:
+        config_path = Path(LOADER_CONFIGFILE_NAME).absolute()
+        command_args: tuple[str, ...] = (str(DAEMON_EXECUTABLE), "--config", str(config_path), "-c", "-", "-n")
+
+        return SubprocessConfig(
+            command=" ".join(command_args),
+            workdir=Path.cwd().absolute(),
         )
 
+    @staticmethod
+    def create_cache_gc(_args: KresArgs, config: KresConfig) -> SubprocessConfig:
+        cache_dir = str(config.cache.storage)
+        command_args: tuple[str, ...] = (str(CACHE_GC_EXECUTABLE), "-c", cache_dir)
 
-async def write_config_file(args: KresArgs, config: KresConfig) -> None:
-    if not supervisord_subprocess_log_dir(config).exists():
-        supervisord_subprocess_log_dir(config).mkdir(exist_ok=True)
+        cache_gc_config = config.cache.garbage_collector
+        command_args += (
+            f"-d {cache_gc_config.interval.millis()}",
+            f"-u {cache_gc_config.threshold}",
+            f"-f {cache_gc_config.release}",
+            f"-l {cache_gc_config.rw_deletes}",
+            f"-L {cache_gc_config.rw_reads}",
+            f"-t {cache_gc_config.temp_keys_space.mbytes()}",
+            f"-m {cache_gc_config.rw_duration.micros()}",
+            f"-w {cache_gc_config.rw_delay.micros()}",
+        )
+        if config.logging.level == "debug" or (config.logging.groups and "cache-gc" in config.logging.groups):
+            command_args += ("-v",)
+        if cache_gc_config.dry_run:
+            command_args += ("-n",)
 
-    template = await asyncio.to_thread((files(__package__) / "supervisord.conf.j2").read_text)
-    config_string = Template(template).render(
-        gc=ProcessTypeConfig.create_gc_config(config),
-        loader=ProcessTypeConfig.create_policy_loader_config(config),
-        kresd=ProcessTypeConfig.create_kresd_config(config),
-        manager=ProcessTypeConfig.create_manager_config(args, config),
-        config=SupervisordConfig.create(config),
-    )
-    await writefile(supervisord_config_file_tmp(config), config_string)
-    # atomically replace (we don't technically need this right now, but better safe then sorry)
-    os.rename(supervisord_config_file_tmp(config), supervisord_config_file(config))
+        return SubprocessConfig(
+            command=" ".join(command_args),
+            workdir=Path.cwd().absolute(),
+        )
